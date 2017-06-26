Fix inverse mm, inverse mv gradients

gpytorch/math/functions/invmm.py
@@ -17,11 +17,9 @@ class Invmm(Function):
 
         # input_1 gradient
         if self.needs_input_grad[0]:
-            grad_input_1 = input_1_t_input_2
+            grad_input_1 = torch.mm(grad_output, input_1_t_input_2.t())
             grad_input_1 = grad_input_1.potrs(chol_matrix, out=grad_input_1)
             grad_input_1 = grad_input_1.mul_(-1)
-            grad_input_1 = torch.mm(grad_output, grad_input_1.t())
-
 
         # input_2 gradient
         if self.needs_input_grad[1]:

gpytorch/math/functions/invmv.py
@@ -17,10 +17,9 @@ class Invmv(Function):
 
         # matrix gradient
         if self.needs_input_grad[0]:
-            grad_matrix = matrix_t_vector
-            grad_matrix = grad_matrix.view(-1, 1).potrs(chol_matrix, out=grad_matrix).view(-1)
+            grad_matrix = torch.ger(grad_output, matrix_t_vector)
+            grad_matrix = grad_matrix.potrs(chol_matrix, out=grad_matrix)
             grad_matrix = grad_matrix.mul_(-1)
-            grad_matrix = torch.ger(grad_output, grad_matrix)
 
         # vector gradient
         if self.needs_input_grad[1]:

test/math/functions/invmm_test.py
@@ -33,7 +33,10 @@ def test_backward():
         ])
         b = torch.ones(3, 3).fill_(2)
         c = torch.randn(3, n_cols)
-        actual_a_grad = torch.eye(3, n_cols).mm((-(a.inverse() / 2).mm(a.inverse() / 2).mm(c)).t()) * 2 * 2
+        actual_a_grad = -torch.mm(
+            a.inverse().mul_(0.5).mm(torch.eye(3, n_cols)),
+            a.inverse().mul_(0.5).mm(c).t()
+        ) * 2 * 2
         actual_c_grad = (a.inverse() / 2).t().mm(torch.eye(3, n_cols)) * 2
 
         a_var = Variable(a, requires_grad=True)

test/math/functions/invmv_test.py
@@ -29,7 +29,7 @@ def test_backward():
     ])
     b = torch.ones(3, 3).fill_(2)
     c = torch.randn(3)
-    actual_a_grad = torch.ones(3, 1).mm((-(a.inverse() / 2).mm(a.inverse() / 2).mv(c)).view(1, -1)) * 2 * 2
+    actual_a_grad = -torch.ger(a.inverse().mul_(0.5).mv(torch.ones(3)), a.inverse().mul_(0.5).mv(c)) * 2 * 2
     actual_c_grad = (a.inverse() / 2).t().mv(torch.ones(3)) * 2
 
     a_var = Variable(a, requires_grad=True)
@@ -40,8 +40,6 @@ def test_backward():
     out_var.backward()
     a_res = a_var.grad.data
     c_res = c_var.grad.data
-    print(actual_a_grad, a_var.grad)
-    print(actual_c_grad, c_var.grad)
 
     assert(torch.norm(actual_a_grad - a_res) < 1e-4)
     assert(torch.norm(actual_c_grad - c_res) < 1e-4)