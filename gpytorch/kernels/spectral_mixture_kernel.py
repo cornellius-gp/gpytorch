0c33df2bbb7a8ef05141f5d068da3bb1972282a9
Jake Gardner <jrg365@cornell.edu>
2017-06-30 11:07:38 -0400

    Implement spectral mixture kernel.

---
+++ gpytorch/kernels/spectral_mixture_kernel.py
@@ -0,0 +1,40 @@
+import torch
+import math
+from torch.nn import Parameter
+from torch.autograd import Function, Variable
+from .kernel import Kernel
+
+class SpectralMixtureKernel(Kernel):
+    def forward(self, x1, x2, log_mixture_weights, log_mixture_means, log_mixture_scales):
+        n, d = x1.size()
+        m, _ = x2.size()
+
+        mixture_weights = log_mixture_weights.exp()
+        mixture_means = log_mixture_means.exp()
+        mixture_scales = log_mixture_scales.mul(2).exp_()
+
+        sq_distance = torch.mm(x1, x2.transpose(0,1)).mul_(2)
+        #sq_distance.addmm_(1, 2, x1, x2.transpose(0, 1)) # sq_distance = 2 x1 x2^T
+
+        x1_squared = torch.bmm(x1.view(n, 1, d), x1.view(n, d, 1))
+        x1_squared = x1_squared.view(n, 1).expand(n, m)
+        x2_squared = torch.bmm(x2.view(m, 1, d), x2.view(m, d, 1))
+        x2_squared = x2_squared.view(1, m).expand(n, m)
+
+        sq_distance.add_(-x1_squared).add_(-x2_squared) # sq_distance = -(x - z)^2
+
+        distance = torch.sqrt(sq_distance.mul(-1)) # distance = (x-z)
+
+        sq_distance.mul_(2*math.pi**2) # sq_distance = -2*pi^2*(x-z)^2
+
+        res = torch.zeros(n,m)
+        for weight, mean, scale in zip(mixture_weights,mixture_means,mixture_scales):
+            weight = weight.expand(n,m)
+            mean = mean.expand(n,m)
+            scale = scale.expand(n,m)
+
+            sq_distance_factor = (scale * sq_distance).exp_()
+            cos_factor = torch.cos(2*math.pi*mean.mul(distance))
+            res = weight * sq_distance_factor * torch.cos(2*math.pi* mean * distance) # res += w_a^2*exp{-2\pi^2*\sigma_a^2*sq_distance}*cos(2\pi*\mu_a*distance)
+
+        return res
---
+++ test/examples/spectral_mixture_gp_regression_test.py
@@ -0,0 +1,57 @@
+import math
+import torch
+import gpytorch
+
+from torch.autograd import Variable
+from torch.nn import Parameter
+from gpytorch.parameters import MLEParameterGroup
+from gpytorch.kernels import RBFKernel, SpectralMixtureKernel
+from gpytorch.means import ConstantMean
+from gpytorch.likelihoods import GaussianLikelihood
+from gpytorch.inference import Inference
+from gpytorch import ObservationModel
+from gpytorch.random_variables import GaussianRandomVariable
+
+# Simple training data: let's try to learn a sine function
+train_x = Variable(torch.linspace(0, 0.75, 11))
+train_y = Variable(torch.sin(train_x.data * (2 * math.pi)))
+
+test_x = Variable(torch.linspace(0, 2, 51))
+test_y = Variable(torch.sin(test_x.data * (2 * math.pi)))
+
+class SpectralMixtureGPModel(gpytorch.ObservationModel):
+    def __init__(self):
+        super(SpectralMixtureGPModel,self).__init__(GaussianLikelihood())
+        self.mean_module = ConstantMean()
+        self.covar_module = SpectralMixtureKernel()
+        self.params = MLEParameterGroup(
+            log_noise=Parameter(torch.Tensor([-2])),
+            log_mixture_weights=Parameter(torch.zeros(3)),
+            log_mixture_means=Parameter(torch.zeros(3)),
+            log_mixture_scales=Parameter(torch.zeros(3))
+        )
+
+    def forward(self,x):
+        mean_x = self.mean_module(x, constant=Variable(torch.Tensor([0])))
+        covar_x = self.covar_module(x,
+                                    log_mixture_weights=self.params.log_mixture_weights,
+                                    log_mixture_means=self.params.log_mixture_means,
+                                    log_mixture_scales=self.params.log_mixture_scales)
+
+        latent_pred = GaussianRandomVariable(mean_x, covar_x)
+        return latent_pred, self.params.log_noise
+
+prior_observation_model = SpectralMixtureGPModel()
+
+def test_spectral_mixture_gp_mean_abs_error():
+    prior_observation_model = SpectralMixtureGPModel()
+
+    # Compute posterior distribution
+    infer = Inference(prior_observation_model)
+    posterior_observation_model = infer.run(train_x, train_y)
+
+    test_preds = posterior_observation_model(test_x).mean()
+    mean_abs_error = torch.mean(torch.abs(test_y - test_preds))
+
+    # The spectral mixture kernel should be trivially able to extrapolate the sine function.
+    assert(mean_abs_error.data.squeeze()[0] < 0.01)
