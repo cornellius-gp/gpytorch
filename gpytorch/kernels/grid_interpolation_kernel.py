b37be3fc65d3f0181d3b24dbc0b389f868d78316
Jake Gardner <jrg365@cornell.edu>
2017-07-20 12:37:28 -0400

    Better GridInterpolationKernel interface.

--- gpytorch/kernels/grid_interpolation_kernel.py
+++ gpytorch/kernels/grid_interpolation_kernel.py
@@ -4,13 +4,14 @@ from .kernel import Kernel
 from gpytorch.utils.interpolation import Interpolation
 
 class GridInterpolationKernel(Kernel):
-    def __init__(self, base_kernel_closure, grid_size):
-        self.base_kernel_closure = base_kernel_closure
+    def __init__(self, base_kernel_module, grid_size):
+        super(GridInterpolationKernel, self).__init__()
+        self.base_kernel_module = base_kernel_module
 
         self.grid_size = grid_size
-        self.grid = Variable(torch.linspace(0,1,grid_size))
+        self.grid = Variable(torch.linspace(0, 1, grid_size))
 
-    def forward(self, x1, x2):
+    def forward(self, x1, x2, **kwargs):
         n, d = x1.size()
         m, _ = x2.size()
 
@@ -26,7 +27,8 @@ class GridInterpolationKernel(Kernel):
         # Explicitly compute full, dense interpolated matrix at the moment just for testing.
         W1 = Variable(Interpolation().interpolate(self.grid.data,x1.data.squeeze()).to_dense()) # Will never need gradients
         W2 = Variable(Interpolation().interpolate(self.grid.data,x2.data.squeeze()).to_dense()) # same here.
-        K_UU = self.base_kernel_closure(self.grid)
+
+        K_UU = self.base_kernel_module(self.grid, **kwargs)
 
         K_XX = W1.mm(K_UU).mm(W2.t())
 
