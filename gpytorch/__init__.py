989a2c5719e8276271c5f8b1e81c23f18c6b1958
Jake Gardner <jrg365@cornell.edu>
2017-08-01 15:16:13 -0400

    Documentation

--- gpytorch/__init__.py
+++ gpytorch/__init__.py
@@ -27,7 +27,18 @@ __all__ = [
 
 
 def add_diag(input, diag):
-    # LazyVariables don't typecheck, functions do.
+    """
+    Adds a diagonal matrix s*I to the input matrix input.
+
+    Args:
+        - input (matrix nxn) - Variable or LazyVariable wrapping matrix to add diagonal \
+                               component to.
+        - diag (scalar) - Scalar s so that s*I is added to the input matrix.
+
+    Returns:
+        - matrix nxn - Variable or LazyVariable wrapping a new matrix with the diagonal \
+                       component added.
+    """
     if not isinstance(diag, Variable):
         raise RuntimeError('Expected a variable for the diagonal component.')
 
@@ -44,6 +55,18 @@ def add_diag(input, diag):
 
 
 def exact_gp_marginal_log_likelihood(covar, target):
+    """
+    Computes the log marginal likelihood of the data with a GP prior and Gaussian noise model
+    given a label vector and covariance matrix.
+
+    Args:
+        - covar (matrix nxn) - Variable or LazyVariable representing the covariance matrix of the observations.
+                               Usually, this is K + s*I, where s is the noise variance, and K is the prior covariance.
+        - target (vector n) - Training label vector.
+
+    Returns:
+        - scalar - The marginal log likelihood of the data.
+    """
     if isinstance(covar, ToeplitzLazyVariable):
         W_left = Variable(utils.index_coef_to_sparse(covar.J_left, covar.C_left, len(covar.c)))
         W_right = Variable(utils.index_coef_to_sparse(covar.J_right, covar.C_right, len(covar.c)))
@@ -54,26 +77,77 @@ def exact_gp_marginal_log_likelihood(covar, target):
 
 
 def invmm(mat1, mat2):
+    """
+    Computes a linear solve with several right hand sides.
+
+    Args:
+        - mat1 (matrix nxn) - Matrix to solve with
+        - mat2 (matrix nxk) - Matrix of k right hand side vectors.
+
+    Returns:
+        - matrix nxk - (mat1)^{-1}mat2
+    """
     return Invmm()(mat1, mat2)
 
 
 def invmv(mat, vec):
+    """
+    Computes a linear solve with a single right hand side
+
+    Args:
+        - mat1 (matrix nxn) - Matrix to solve with
+        - vec (vector n) - Right hand side vector
+
+    Returns:
+        - vector n - (mat1)^{-1}vec
+    """
     return Invmv()(mat, vec)
 
 
 def normal_cdf(x):
+    """
+    Computes the element-wise standard normal CDF of an input tensor x.
+    """
     return NormalCDF()(x)
 
 
 def log_normal_cdf(x):
+    """
+    Computes the element-wise log standard normal CDF of an input tensor x.
+
+    This function should always be preferred over calling normal_cdf and taking the log
+    manually, as it is more numerically stable.
+    """
     return LogNormalCDF()(x)
 
 
 def mvn_kl_divergence(mean_1, chol_covar_1, mean_2, covar_2):
+    """
+    Computes the KL divergence between two Gaussian distributions N1 and N2.
+
+    N1 is parameterized by a mean vector and the Cholesky decomposition of the covariance matrix.
+    N2 is parameterized by a mean vector and covariance matrix.
+
+    For more information, see the MVNKLDivergence function documentation.
+    """
     return MVNKLDivergence()(mean_1, chol_covar_1, mean_2, covar_2)
 
 
 def interpolated_toeplitz_mul(c, W_left, W_right, v, noise_diag=None):
+    """
+    Given a interpolated symmetric Toeplitz matrix W_left*T*W_right, plus possibly an additional
+    diagonal component s*I, compute a matrix-vector product with some vector v.
+
+    Args:
+        - c (vector m) - First column of the symmetric Toeplitz matrix T
+        - W_left (sparse matrix nxm) - Left interpolation matrix
+        - W_right (sparse matrix pxm) - Right interpolation matrix
+        - v (matrix pxk) - Vector (k=1) or matrix (k>1) to multiply WTW with
+        - noise_diag (vector p) - If not none, add (s*I)v to WTW at the end.
+
+    Returns:
+        - matrix nxk - The result of multiplying (WTW + sI)v if noise_diag exists, or (WTW)v otherwise.
+    """
     noise_term = None
     if v.ndimension() == 1:
         if noise_diag is not None:
@@ -105,6 +179,28 @@ def interpolated_toeplitz_mul(c, W_left, W_right, v, noise_diag=None):
 
 def _exact_predict(test_mean, test_test_covar, train_y, train_mean,
                    train_train_covar, train_test_covar, test_train_covar, alpha=None):
+    """
+    Computes the predictive mean and variance of a posterior Gaussian process. Additionally computes
+    certain posterior parameters that can be cached between rounds of predictions.
+
+        Args:
+            - test_mean (vector k) - prior mean values for the test points.
+            - test_test_covar (matrix kxk) - prior covariance matrix for the test points.
+            - train_y (vector n) - training labels
+            - train_mean (vector n) - prior mean values for the training points
+            - train_train_covar (matrix nxn) - Variable or LazyVariable representing the train-train covariance matrix.
+                                               Usually takes the form (K+sI), where K is the prior covariance matrix and
+                                               s is the noise variance.
+            - train_test_covar (matrix nxk) - prior covariance matrix between training and test points.
+            - test_train_covar (matrix nxk) - prior covariance matrix between test and training points.
+                                              Usually, this is simply the transpose of train_test_covar.
+            - alpha (vector n) - Coefficients for predictive mean that do not depend on the test points.
+                                 If not supplied, this function will compute them.
+
+        Returns:
+            - GaussianRandomVariable with the predictive mean and covariance matrix for the test points.
+            - alpha (vector n) - Coefficients that can be reused when this function is next called.
+    """
     if isinstance(train_train_covar, ToeplitzLazyVariable):
         W_test_left = utils.index_coef_to_sparse(test_train_covar.J_left,
                                                  test_train_covar.C_left,
