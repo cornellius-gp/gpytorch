055876d7a7f31640d9e95394a873d95f86e62502
Geoff Pleiss <gpleiss@gmail.com>
2017-08-11 18:13:39 +1000

    Remove params, lbfgs, pdcatcher

--- gpytorch/utils/__init__.py
+++ gpytorch/utils/__init__.py
@@ -2,48 +2,8 @@ import torch
 from torch.autograd import Variable
 import logging
 from .interpolation import Interpolation
-from .lbfgs import LBFGS
 from .lincg import LinearCG
 from .lanczos_quadrature import StochasticLQ
-
-
-__all__ = [
-    Interpolation,
-    LBFGS,
-    LinearCG,
-    StochasticLQ,
-]
-
-
-class pd_catcher(object):
-    '''
-    A decorator to deal with non-positive definite matrices (useful during optimization)
-    If an error due to non-psotiive definiteness occurs when calling the function, we
-    retry the function call a certain number of times.
-    After a certain number of trials, it fails.
-    '''
-    def __init__(self, catch_function=None, max_trials=20, log_interval=5):
-        self.catch_function = catch_function
-        self.n_trials = 0
-        self.max_trials = max_trials
-        self.log_interval = log_interval
-
-    def __call__(self, function):
-        def wrapped_function(*args, **kwargs):
-            try:
-                result = function(*args, **kwargs)
-                self.n_trials = 0
-            except (ZeroDivisionError, RuntimeError) as e:
-                if self.n_trials < self.max_trials:
-                    if self.catch_function:
-                        result = self.catch_function(*args, **kwargs)
-                    self.n_trials += 1
-                    if self.n_trials % self.log_interval == 0:
-                        logging.warning('Not PD matrix: %d more attempts' % (self.max_trials - self.n_trials))
-                else:
-                    raise e
-            return result
-        return wrapped_function
 
 
 def reverse(input, dim=0):
@@ -88,4 +48,11 @@ def approx_equal(self, other, epsilon=1e-4):
     return torch.max((self - other).abs()) <= epsilon
 
 
-__all__ = [LBFGS, pd_catcher]
+__all__ = [
+    Interpolation,
+    LinearCG,
+    StochasticLQ,
+    reverse,
+    rcumsum,
+    approx_equal,
+]
