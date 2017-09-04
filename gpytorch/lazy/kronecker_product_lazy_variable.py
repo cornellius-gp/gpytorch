Modify derivative_quadratic_form_factory. Refactor exact gp marginal log likelihood. Fix a bug in computing the gradient for noise.

gpytorch/lazy/kronecker_product_lazy_variable.py
@@ -5,7 +5,7 @@ from .lazy_variable import LazyVariable
 from .toeplitz_lazy_variable import ToeplitzLazyVariable
 from torch.autograd import Variable
 from ..posterior import InterpolatedPosteriorStrategy
-from ..utils import sparse_eye, LinearCG
+from ..utils import sparse_eye
 from ..utils.kronecker_product import sym_kronecker_product_toeplitz_matmul, kp_interpolated_toeplitz_matmul, \
     kp_sym_toeplitz_derivative_quadratic_form, list_of_indices_and_values_to_sparse, kronecker_product
 
@@ -52,57 +52,21 @@ class KroneckerProductLazyVariable(LazyVariable):
                 columns, = args
                 left_v_W = left_vector
                 W_right_v = right_vector
+                return kp_sym_toeplitz_derivative_quadratic_form(columns, left_v_W, W_right_v),
             elif len(args) == 4:
                 columns, W_left, W_right, added_diag, = args
+
+                if added_diag is not None:
+                    diag_grad = torch.zeros(len(added_diag))
+                    diag_grad[0] = left_vector.dot(right_vector)
+                else:
+                    diag_grad = None
+
                 left_v_W = torch.dsmm(W_left.t(), left_vector.unsqueeze(1)).squeeze()
                 W_right_v = torch.dsmm(W_right.t(), right_vector.unsqueeze(1)).squeeze()
-            return tuple([kp_sym_toeplitz_derivative_quadratic_form(columns,
-                                                                    left_v_W,
-                                                                    W_right_v)] + [None] * (len(args) - 1))
-        return closure
-
-    def _exact_gp_mll_grad_closure_factory(self, *args):
-        if len(args) == 1:
-            columns, = args
-            W_left = sparse_eye(len(columns))
-            W_right = sparse_eye(len(columns))
-            added_diag = None
-        elif len(args) == 4:
-            columns, W_left, W_right, added_diag, = args
-        else:
-            raise AttributeError('Invalid number of arguments')
-
-        def closure(mm_closure, tr_inv, mat_inv_labels, labels, num_samples):
-            # Gradient of c
-            labels_mat_inv_W_left = torch.dsmm(W_left.t(), mat_inv_labels.unsqueeze(1)).t()
-            W_right_mat_inv_labels = torch.dsmm(W_right.t(), mat_inv_labels.unsqueeze(1))
-            quad_form_part = kp_sym_toeplitz_derivative_quadratic_form(columns,
-                                                                       labels_mat_inv_W_left.squeeze(),
-                                                                       W_right_mat_inv_labels.squeeze())
-            log_det_part = torch.zeros(columns.size())
-            sample_matrix = torch.sign(torch.randn(len(labels), num_samples))
-
-            left_vectors = torch.dsmm(W_left.t(), LinearCG().solve(mm_closure, sample_matrix)).t()
-            right_vectors = torch.dsmm(W_right.t(), sample_matrix).t()
-
-            for left_vector, right_vector in zip(left_vectors, right_vectors):
-                log_det_part += kp_sym_toeplitz_derivative_quadratic_form(columns, left_vector, right_vector)
-
-            log_det_part.div_(num_samples)
-            columns_grad = quad_form_part - log_det_part
-
-            # Gradient of diagonal term
-            diag_grad = None
-            if added_diag is not None:
-                quad_form_part = mat_inv_labels.dot(mat_inv_labels)
-                diag_grad = columns.new().resize_(1).fill_(quad_form_part - tr_inv)
-
-            # Return grads for c, W_left (None), W_right (None), diag
-            if len(args) == 1:
-                return columns_grad,
-            elif len(args) == 4:
-                return columns_grad, None, None, diag_grad
-
+                return tuple([kp_sym_toeplitz_derivative_quadratic_form(columns,
+                                                                        left_v_W,
+                                                                        W_right_v)] + [None] * 2 + [diag_grad])
         return closure
 
     def add_diag(self, diag):

gpytorch/lazy/toeplitz_lazy_variable.py
@@ -4,7 +4,7 @@ from torch.autograd import Variable
 from gpytorch.utils import toeplitz
 from .lazy_variable import LazyVariable
 from ..posterior import InterpolatedPosteriorStrategy
-from ..utils import sparse_eye, LinearCG
+from ..utils import sparse_eye
 from ..utils.toeplitz import interpolated_sym_toeplitz_matmul, index_coef_to_sparse, sym_toeplitz_matmul, \
     sym_toeplitz_derivative_quadratic_form
 
@@ -46,55 +46,19 @@ class ToeplitzLazyVariable(LazyVariable):
                 toeplitz_column, = args
                 left_v_W = left_vector
                 W_right_v = right_vector
+                return sym_toeplitz_derivative_quadratic_form(left_v_W, W_right_v),
             elif len(args) == 4:
                 toeplitz_column, W_left, W_right, added_diag, = args
+                
+                if added_diag is not None:
+                    diag_grad = torch.zeros(len(added_diag))
+                    diag_grad[0] = left_vector.dot(right_vector)
+                else:
+                    diag_grad = None
+
                 left_v_W = torch.dsmm(W_left.t(), left_vector.unsqueeze(1)).squeeze()
                 W_right_v = torch.dsmm(W_right.t(), right_vector.unsqueeze(1)).squeeze()
-            return tuple([sym_toeplitz_derivative_quadratic_form(left_v_W, W_right_v)] + [None] * (len(args) - 1))
-        return closure
-
-    def _exact_gp_mll_grad_closure_factory(self, *args):
-        if len(args) == 1:
-            toeplitz_column, = args
-            W_left = sparse_eye(len(toeplitz_column))
-            W_right = sparse_eye(len(toeplitz_column))
-            added_diag = None
-        elif len(args) == 4:
-            toeplitz_column, W_left, W_right, added_diag, = args
-        else:
-            raise AttributeError('Invalid number of arguments')
-
-        def closure(mm_closure, tr_inv, mat_inv_labels, labels, num_samples):
-            # Gradient of c
-            labels_mat_inv_W_left = torch.dsmm(W_left.t(), mat_inv_labels.unsqueeze(1)).t()
-            W_right_mat_inv_labels = torch.dsmm(W_right.t(), mat_inv_labels.unsqueeze(1))
-            quad_form_part = sym_toeplitz_derivative_quadratic_form(labels_mat_inv_W_left.squeeze(),
-                                                                    W_right_mat_inv_labels.squeeze())
-            log_det_part = torch.zeros(len(toeplitz_column))
-            sample_matrix = torch.sign(torch.randn(len(labels), num_samples))
-
-            left_vectors = torch.dsmm(W_left.t(), LinearCG().solve(mm_closure, sample_matrix)).t()
-            right_vectors = torch.dsmm(W_right.t(), sample_matrix).t()
-
-            for left_vector, right_vector in zip(left_vectors, right_vectors):
-                log_det_part += sym_toeplitz_derivative_quadratic_form(left_vector,
-                                                                       right_vector)
-
-            log_det_part.div_(num_samples)
-            c_grad = quad_form_part - log_det_part
-
-            # Gradient of diagonal term
-            diag_grad = None
-            if added_diag is not None:
-                quad_form_part = mat_inv_labels.dot(mat_inv_labels)
-                diag_grad = toeplitz_column.new().resize_(1).fill_(quad_form_part - tr_inv)
-
-            # Return grads for c, W_left (None), W_right (None), diag
-            if len(args) == 1:
-                return c_grad,
-            elif len(args) == 4:
-                return c_grad, None, None, diag_grad
-
+                return tuple([sym_toeplitz_derivative_quadratic_form(left_v_W, W_right_v)] + [None] * 2 + [diag_grad])
         return closure
 
     def add_diag(self, diag):

gpytorch/utils/function_factory.py
@@ -17,15 +17,6 @@ def _default_derivative_quadratic_form_factory(mat):
     return lambda left_vector, right_vector: (left_vector.ger(right_vector),)
 
 
-def _default_exact_gp_mml_grad_closure_factory(*args):
-    def closure(matmul_closure, tr_inv, mat_inv_labels, labels, num_samples):
-        grad = torch.ger(labels.view(-1), mat_inv_labels.view(-1))
-        grad.add_(-torch.eye(*grad.size()))
-        grad = LinearCG().solve(matmul_closure, grad)
-        return grad,
-    return closure
-
-
 def inv_matmul_factory(matmul_closure_factory=_default_matmul_closure_factor,
                        derivative_quadratic_form_factory=_default_derivative_quadratic_form_factory):
     class InvMatmul(Function):
@@ -227,7 +218,7 @@ def trace_logdet_quad_form_factory(matmul_closure_factory=_default_matmul_closur
 
 
 def exact_gp_mll_factory(matmul_closure_factory=_default_matmul_closure_factor,
-                         exact_gp_mml_grad_closure_factory=_default_exact_gp_mml_grad_closure_factory):
+                         derivative_quadratic_form_factory=_default_derivative_quadratic_form_factory):
     class ExactGPMLL(Function):
         def __init__(self, num_samples=10):
             self.num_samples = num_samples
@@ -241,21 +232,19 @@ def exact_gp_mll_factory(matmul_closure_factory=_default_matmul_closure_factor,
             # Inverse quad form
             res = mat_inv_labels.dot(labels)
             # Log determinant
-            logdet, tr_inv = StochasticLQ(num_random_probes=10).evaluate(matmul_closure, len(labels),
-                                                                         [lambda x: x.log(), lambda x: x.pow(-1)])
+            logdet, = StochasticLQ(num_random_probes=10).evaluate(matmul_closure, len(labels), [lambda x: x.log()])
 
             res += logdet
             res += math.log(2 * math.pi) * len(labels)
             res *= -0.5
 
             self.mat_inv_labels = mat_inv_labels
-            self.tr_inv = tr_inv
             self.matmul_closure = matmul_closure
             self.save_for_backward(*args)
             return labels.new().resize_(1).fill_(res)
 
         def backward(self, grad_output):
-            if exact_gp_mml_grad_closure_factory is None:
+            if derivative_quadratic_form_factory is None:
                 raise NotImplementedError
 
             closure_args = self.saved_tensors[:-1]
@@ -264,17 +253,49 @@ def exact_gp_mll_factory(matmul_closure_factory=_default_matmul_closure_factor,
             grad_output_value = grad_output.squeeze()[0]
 
             matmul_closure = self.matmul_closure
-            tr_inv = self.tr_inv
             closure_arg_grads = [None] * len(closure_args)
             labels_grad = None
 
             # input_1 gradient
             if any(self.needs_input_grad[:-1]):
-                grad_closure = exact_gp_mml_grad_closure_factory(*(list(closure_args)))
-                closure_arg_grads = list(grad_closure(matmul_closure, tr_inv, mat_inv_labels, labels, self.num_samples))
-                for i, closure_arg_grad in enumerate(closure_arg_grads):
-                    if closure_arg_grad is not None:
-                        closure_arg_grad.mul_(0.5 * grad_output_value)
+                for i in range(len(closure_args)):
+                    if self.needs_input_grad[i]:
+                        closure_arg_grads[i] = torch.zeros(closure_args[i].size())
+                    else:
+                        closure_arg_grads[i] = None
+
+                if self.num_samples < len(labels):
+                    quad_form_part = derivative_quadratic_form_factory(*closure_args)(mat_inv_labels, mat_inv_labels)
+
+                    sample_matrix = torch.sign(torch.randn(len(labels), self.num_samples))
+                    left_vectors = LinearCG().solve(matmul_closure, sample_matrix).t()
+                    right_vectors = sample_matrix.t()
+
+                    for i in range(self.num_samples):
+                        quad_derivative = derivative_quadratic_form_factory(*closure_args)(left_vectors[i],
+                                                                                           right_vectors[i])
+                        for j in range(len(closure_args)):
+                            if self.needs_input_grad[j]:
+                                closure_arg_grads[j].add_(quad_derivative[j])
+
+                    for i in range(len(closure_args)):
+                        if self.needs_input_grad[i]:
+                            closure_arg_grads[i] = quad_form_part[i].add_(-closure_arg_grads[i].div_(self.num_samples))
+                            closure_arg_grads[i].mul_(0.5 * grad_output_value)
+                else:
+                    left_vectors = torch.ger(labels, mat_inv_labels) - torch.eye(len(labels))
+                    left_vectors = LinearCG().solve(matmul_closure, left_vectors)
+                    right_vectors = torch.eye(len(labels))
+                    for i in range(len(labels)):
+                        quad_derivative = derivative_quadratic_form_factory(*closure_args)(left_vectors[i],
+                                                                                           right_vectors[i])
+                        for j in range(len(closure_args)):
+                            if self.needs_input_grad[j]:
+                                closure_arg_grads[j] += quad_derivative[j]
+
+                    for i in range(len(closure_args)):
+                        if self.needs_input_grad[i]:
+                            closure_arg_grads[i].mul_(0.5 * grad_output_value)
 
             # input_2 gradient
             if self.needs_input_grad[-1]: