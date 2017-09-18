dbe671913a8bffa0da133a8852aa31ab68269f60
Ruihan Wu <ruihan.wu14@gmail.com>
2017-09-18 18:57:17 -0400

    Fix bugs in __getitem__()

--- gpytorch/lazy/kronecker_product_lazy_variable.py
+++ gpytorch/lazy/kronecker_product_lazy_variable.py
@@ -220,27 +220,23 @@ class KroneckerProductLazyVariable(LazyVariable):
 
     def __getitem__(self, i):
         if isinstance(i, tuple):
+            first_index = i[0]
+            if not isinstance(first_index, slice):
+                first_index = slice(first_index, first_index + 1, None)
+            second_index = i[1]
+            if not isinstance(second_index, slice):
+                second_index = slice(second_index, second_index + 1, None)
+
             if self.J_lefts is None:
-                # Pretend that the matrix is WTW, where W is an identity matrix, with appropriate slices
-                # # J[:, i[0], :], C[:, i[0], :]
-                # J_left_new = self.c.data.new(range(len(self.c))[i[0]]).unsqueeze(1)
-                # C_left_new = self.c.data.new().resize_as_(J_left_new).fill_(1)
-                # J_left_new = J_left_new.long()
-
-                # # J[:, i[1], :], C[:, i[1], :]
-                # J_right_new = self.c.data.new(range(len(self.c))[i[1]]).unsqueeze(1)
-                # C_right_new = self.c.data.new().resize_as_(J_right_new).fill_(1)
-                # J_right_new = J_right_new.long()
-
                 d, m0 = self.columns.size()
-                len_i0 = len(range(self.kronecker_product_size)[i[0]])
-                len_i1 = len(range(self.kronecker_product_size)[i[1]])
+                len_i0 = len(range(self.kronecker_product_size)[first_index])
+                len_i1 = len(range(self.kronecker_product_size)[second_index])
                 J_lefts_new = torch.zeros(d, len_i0)
                 J_rights_new = torch.zeros(d, len_i1)
                 for j in range(d):
-                    J_lefts_new_tensor = torch.arange(0, self.kronecker_product_size)[i[0]] / pow(m0, d - j - 1)
+                    J_lefts_new_tensor = torch.arange(0, self.kronecker_product_size)[first_index] / pow(m0, d - j - 1)
                     J_lefts_new[j] = self.columns.data.new(J_lefts_new_tensor)
-                    J_rights_new_tensor = torch.arange(0, self.kronecker_product_size)[i[1]] / pow(m0, d - j - 1)
+                    J_rights_new_tensor = torch.arange(0, self.kronecker_product_size)[second_index] / pow(m0, d - j - 1)
                     J_rights_new[j] = self.columns.data.new(J_rights_new_tensor)
                 C_lefts_new = self.columns.data.new().resize_as_(J_lefts_new).fill_(1).unsqueeze(2)
                 C_rights_new = self.columns.data.new().resize_as_(J_lefts_new).fill_(1).unsqueeze(2)
@@ -248,12 +244,12 @@ class KroneckerProductLazyVariable(LazyVariable):
                 J_rights_new = J_rights_new.long().unsqueeze(2)
             else:
                 # J[:, i[0], :], C[:, i[0], :]
-                J_lefts_new = self.J_lefts[:, i[0], :]
-                C_lefts_new = self.C_lefts[:, i[0], :]
+                J_lefts_new = self.J_lefts[:, first_index, :]
+                C_lefts_new = self.C_lefts[:, first_index, :]
 
                 # J[:, i[1], :], C[:, i[1], :]
-                J_rights_new = self.J_rights[:, i[1], :]
-                C_rights_new = self.C_rights[:, i[1], :]
+                J_rights_new = self.J_rights[:, second_index, :]
+                C_rights_new = self.C_rights[:, second_index, :]
 
             if self.added_diag is not None:
                 if len(J_lefts_new[0]) != len(J_rights_new[0]):
@@ -261,7 +257,7 @@ class KroneckerProductLazyVariable(LazyVariable):
                                         diagonal component to make it non-square is probably not intended.\
                                         It is ambiguous which diagonal elements to choose')
 
-                diag_new = self.added_diag[i[0]]
+                diag_new = self.added_diag[first_index]
             else:
                 diag_new = None
 
