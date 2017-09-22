e9522d7545abf0b3188b70e4628cb71b9a90e07f
Geoff Pleiss <gpleiss@gmail.com>
2017-09-22 14:01:28 -0400

    Non lazy variable

---
+++ gpytorch/lazy/non_lazy_variable.py
@@ -0,0 +1,38 @@
+import torch
+import gpytorch
+from gpytorch.lazy import LazyVariable
+from gpytorch.utils import function_factory
+
+
+class NonLazyVariable(LazyVariable):
+    def __init__(self, var):
+        """
+        Not a lazy variable
+
+        Args:
+        - var (Variable: matrix) a variable
+        """
+        self.var = var
+
+    def _matmul_closure_factory(self, tensor):
+        def closure(rhs_tensor):
+            return torch.matmul(tensor, rhs_tensor)
+        return closure
+
+    def _derivative_quadratic_form_factory(self, mat):
+        return function_factory._default_derivative_quadratic_form_factory(mat)
+
+    def add_diag(self, diag):
+        return NonLazyVariable(gpytorch.add_diag(self.var, diag))
+
+    def evaluate(self):
+        return self.var
+
+    def mul(self, constant):
+        return NonLazyVariable(self.var.mul(constant))
+
+    def representation(self):
+        return self.var,
+
+    def __getitem__(self, index):
+        return NonLazyVariable(self.var[index])
