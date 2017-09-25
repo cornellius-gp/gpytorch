70c05e3a55692603ac1fd20bacff04e22ff9a0d4
Geoff Pleiss <gpleiss@gmail.com>
2017-09-25 07:22:06 -0400

    Adding lazy vars to init

--- gpytorch/lazy/__init__.py
+++ gpytorch/lazy/__init__.py
@@ -1,12 +1,16 @@
 from .lazy_variable import LazyVariable
+from .kronecker_product_lazy_variable import KroneckerProductLazyVariable
+from .mul_lazy_variable import MulLazyVariable
+from .non_lazy_variable import NonLazyVariable
 from .sum_lazy_variable import SumLazyVariable
 from .toeplitz_lazy_variable import ToeplitzLazyVariable
-from .kronecker_product_lazy_variable import KroneckerProductLazyVariable
 
 
 __all__ = [
+    KroneckerProductLazyVariable,
     LazyVariable,
+    MulLazyVariable,
+    NonLazyVariable,
     SumLazyVariable,
     ToeplitzLazyVariable,
-    KroneckerProductLazyVariable
 ]
