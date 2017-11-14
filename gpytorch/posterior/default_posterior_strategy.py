bc802f109cb976ad90b076059ad5d568c294b3c3
Geoff Pleiss <gpleiss@gmail.com>
2017-11-14 09:27:57 -0500

    Remove old code from posterior strategies

--- gpytorch/posterior/default_posterior_strategy.py
+++ gpytorch/posterior/default_posterior_strategy.py
@@ -1,6 +1,5 @@
 import torch
 import gpytorch
-from torch.autograd import Variable
 from gpytorch.lazy import LazyVariable
 from .posterior_strategy import PosteriorStrategy
 
@@ -17,31 +16,3 @@ class DefaultPosteriorStrategy(PosteriorStrategy):
         if isinstance(self.var, LazyVariable):
             return self.var.matmul(alpha) + test_mean
         return torch.addmv(test_mean, self.var, alpha)
-
-    def monte_carlo_log_likelihood(self, log_probability_func, train_y, variational_mean, chol_var_covar):
-        epsilon = Variable(variational_mean.data.new(len(variational_mean),
-                           gpytorch.functions.num_trace_samples).normal_())
-        samples = chol_var_covar.t().mm(epsilon)
-        samples = samples + variational_mean.unsqueeze(1)
-        log_likelihood = log_probability_func(samples, train_y)
-        return log_likelihood
-
-    def variational_posterior_alpha(self, variational_mean):
-        return gpytorch.inv_matmul(self.var, variational_mean)
-
-    def variational_posterior_mean(self, alpha):
-        return self.var.matmul(alpha)
-
-    def variational_posterior_covar(self, induc_test_covar, chol_variational_covar,
-                                    test_test_covar, induc_induc_covar):
-        from ..lazy import NonLazyVariable, MatmulLazyVariable
-        # left_factor = K_{mn}K_{nn}^{-1}(S - K_{nn})
-        variational_covar = chol_variational_covar.t().matmul(chol_variational_covar)
-        left_factor = torch.mm(self.var, gpytorch.inv_matmul(induc_induc_covar,
-                                                             variational_covar - induc_induc_covar))
-        # right_factor = K_{nn}^{-1}K_{nm}
-        right_factor = gpytorch.inv_matmul(induc_induc_covar, induc_test_covar)
-        # test_test_covar = K_{mm} + K_{mn}K_{nn}^{-1}(S - K_{nn})K_{nn}^{-1}K_{nm}
-        if not isinstance(test_test_covar, LazyVariable):
-            test_test_covar = NonLazyVariable(test_test_covar)
-        return test_test_covar + MatmulLazyVariable(left_factor, right_factor)
--- gpytorch/posterior/interpolated_posterior_strategy.py
+++ gpytorch/posterior/interpolated_posterior_strategy.py
@@ -1,5 +1,4 @@
 import gpytorch
-from torch.autograd import Variable
 from .posterior_strategy import PosteriorStrategy
 
 
@@ -34,26 +33,3 @@ class InterpolatedPosteriorStrategy(PosteriorStrategy):
     def exact_posterior_mean(self, test_mean, alpha):
         alpha = alpha.unsqueeze(1)
         return test_mean.add(gpytorch.dsmm(self.interp_left, alpha).squeeze())
-
-    def monte_carlo_log_likelihood(self, log_probability_func, train_y, variational_mean, chol_var_covar):
-        epsilon = Variable(variational_mean.data.new(variational_mean.size(0),
-                           gpytorch.functions.num_trace_samples).normal_())
-        samples = chol_var_covar.mm(epsilon)
-        samples = samples + variational_mean.unsqueeze(1).expand_as(samples)
-        samples = gpytorch.dsmm(self.interp_left, samples)
-        log_likelihood = log_probability_func(samples, train_y)
-
-        return log_likelihood
-
-    def variational_posterior_alpha(self, variational_mean):
-        return variational_mean.add(0)  # Trick to ensure that we're not returning a Paremeter
-
-    def variational_posterior_mean(self, alpha):
-        return gpytorch.dsmm(self.interp_left, alpha.unsqueeze(1)).squeeze()
-
-    def variational_posterior_covar(self, induc_test_covar, chol_variational_covar,
-                                    test_test_covar, induc_induc_covar):
-        from ..lazy import MatmulLazyVariable
-        covar_right = gpytorch.dsmm(self.interp_left, chol_variational_covar.t()).t()
-        covar_left = gpytorch.dsmm(self.interp_left, chol_variational_covar.t())
-        return MatmulLazyVariable(covar_left, covar_right)
