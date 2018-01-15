26e390d68a4fad9c46b4f34e7ba330bf9a39acc6
Jake Gardner <jrg365@cornell.edu>
2018-01-15 18:54:36 -0500

    Revert "Fix #58 by importing SumVariationalStrategy in variational init"
    This reverts commit f0c1965794835eae0a52a6fd6f6906a7726722d5.

--- gpytorch/variational/__init__.py
+++ gpytorch/variational/__init__.py
@@ -1,10 +1,9 @@
 from .variational_strategy import VariationalStrategy
 from .mvn_variational_strategy import MVNVariationalStrategy
-from .sum_variational_strategy import SumVariationalStrategy
+
 
 
 __all__ = [
     VariationalStrategy,
     MVNVariationalStrategy,
-    SumVariationalStrategy,
 ]
