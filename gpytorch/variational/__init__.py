43879fc14a1fd460414d4c1ef383065ae855dcaf
Jake Gardner <jrg365@cornell.edu>
2018-01-15 18:14:19 -0500

    Fix lint

--- gpytorch/variational/__init__.py
+++ gpytorch/variational/__init__.py
@@ -2,6 +2,7 @@ from .variational_strategy import VariationalStrategy
 from .mvn_variational_strategy import MVNVariationalStrategy
 from .sum_variational_strategy import SumVariationalStrategy
 
+
 __all__ = [
     VariationalStrategy,
     MVNVariationalStrategy,
