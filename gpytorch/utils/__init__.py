220e5fec79fb7b966794018d1f393f55d05f72ce
Jake Gardner <jrg365@cornell.edu>
2018-01-25 15:23:00 -0500

    Working on stable Lanczos bidiagonalization

--- gpytorch/utils/__init__.py
+++ gpytorch/utils/__init__.py
@@ -7,6 +7,7 @@ from .linear_cg import linear_cg
 from . import lanczos
 from . import sparse
 from .stochastic_lq import StochasticLQ
+from .lanczos_bidiagonalize import LanczosBidiagonalize
 
 
 def reverse(input, dim=0):
@@ -405,7 +406,12 @@ def tridiag_batch_potrs(tensor, chol_trid, upper=True):
 
 __all__ = [
     Interpolation,
+<<<<<<< 96b0dc0fb6d4867790ce4ff5d76888e8ce8aae4a
     linear_cg,
+=======
+    LinearCG,
+    LanczosBidiagonalize,
+>>>>>>> Working on stable Lanczos bidiagonalization
     StochasticLQ,
     left_interp,
     reverse,
