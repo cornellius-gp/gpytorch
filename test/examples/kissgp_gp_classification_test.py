858872ecf4a19c0ffe77916f2e6e2ae64663c6cb
Jake Gardner <jrg365@cornell.edu>
2018-02-27 12:22:02 -0500

    Improve test stability

--- test/examples/kissgp_gp_classification_test.py
+++ test/examples/kissgp_gp_classification_test.py
@@ -10,12 +10,12 @@ from gpytorch.random_variables import GaussianRandomVariable
 
 
 train_x = Variable(torch.linspace(0, 1, 10))
-train_y = Variable(torch.sign(torch.cos(train_x.data * (4 * math.pi))))
+train_y = Variable(torch.sign(torch.cos(train_x.data * (8 * math.pi))))
 
 
 class GPClassificationModel(gpytorch.models.GridInducingVariationalGP):
     def __init__(self):
-        super(GPClassificationModel, self).__init__(grid_size=30, grid_bounds=[(0, 1)])
+        super(GPClassificationModel, self).__init__(grid_size=32, grid_bounds=[(0, 1)])
         self.mean_module = ConstantMean(constant_bounds=[-1e-5, 1e-5])
         self.covar_module = RBFKernel(log_lengthscale_bounds=(-5, 6))
         self.register_parameter('log_outputscale', nn.Parameter(torch.Tensor([0])), bounds=(-5, 6))
@@ -37,7 +37,7 @@ def test_kissgp_classification_error():
     model.train()
     likelihood.train()
 
-    optimizer = optim.Adam(model.parameters(), lr=0.15)
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
     optimizer.n_iter = 0
     for i in range(200):
         optimizer.zero_grad()
