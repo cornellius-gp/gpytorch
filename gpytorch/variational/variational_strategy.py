cbbb76805eb2634ef1d1db2bd25a2dc5d5e4a5aa
Geoff Pleiss <gpleiss@gmail.com>
2018-03-21 10:01:54 -0400

    Fix variational_strategy.py lint

--- gpytorch/variational/variational_strategy.py
+++ gpytorch/variational/variational_strategy.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+
 class VariationalStrategy(object):
     def __init__(self, variational_dist, prior_dist):
         self.variational_dist = variational_dist
