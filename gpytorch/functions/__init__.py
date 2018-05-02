febcc972fbbc5624a053ba8cd8d6369edb029567
Max Balandat <balandat@fb.com>
2018-05-02 06:11:24 -0700

    Add py2 compatibility imports to functions/__init__.py

--- gpytorch/functions/__init__.py
+++ gpytorch/functions/__init__.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from torch.autograd import Variable
 from ._add_diag import AddDiag
 from ._dsmm import DSMM
