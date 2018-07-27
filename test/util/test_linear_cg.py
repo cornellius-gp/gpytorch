271287f5e9c3ffb6469fc6e6b653a6254de2ac47
Jake <jrg365@cornell.edu>
2018-07-27 19:42:58 -0400

    lock down seed

--- test/util/test_linear_cg.py
+++ test/util/test_linear_cg.py
@@ -3,6 +3,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os
+import random
 import torch
 import unittest
 from gpytorch.utils import approx_equal, batch_potrf, batch_potrs
@@ -10,6 +12,18 @@ from gpytorch.utils.linear_cg import linear_cg
 
 
 class TestLinearCG(unittest.TestCase):
+    def setUp(self):
+        if os.getenv("UNLOCK_SEED") is None or os.getenv("UNLOCK_SEED").lower() == "false":
+                self.rng_state = torch.get_rng_state()
+                torch.manual_seed(0)
+                if torch.cuda.is_available():
+                    torch.cuda.manual_seed_all(0)
+                random.seed(0)
+
+    def tearDown(self):
+        if hasattr(self, "rng_state"):
+            torch.set_rng_state(self.rng_state)
+
     def test_cg(self):
         size = 100
         matrix = torch.DoubleTensor(size, size).normal_()
