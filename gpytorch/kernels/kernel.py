d1e5e70687cf9b5868a9f23ad51e35c5647711f1
Jake Gardner <gardner.jake@gmail.com>
2018-07-31 14:01:28 -0400

    Add docstrings to Kernel

--- gpytorch/kernels/kernel.py
+++ gpytorch/kernels/kernel.py
@@ -13,6 +13,30 @@ from gpytorch.utils import prod
 
 
 class Kernel(Module):
+    """
+    Kernels in GPyTorch are implemented as a :class:`gpytorch.Module` that, when called on two :obj:`torch.tensor`
+    objects `x1` and `x2` returns either a :obj:`torch.tensor` or a :obj:`gpytorch.lazy.LazyVariable` that represents
+    the covariance matrix between `x1` and `x2`.
+
+    In the typical use case, to extend this class means to implement the :func:`~gpytorch.kernels.Kernel.forward`
+    method.
+
+    ..note::
+        The `:func:`~gpytorch.kernels.Kernel.__call__` does some additional internal work. In particular,
+        all kernels are lazily evaluated so that, in some cases, we can index in to the kernel matrix before actually
+        computing it. Furthermore, many built in kernel modules return LazyVariables that allow for more efficient
+        inference than if we explicitly computed the kernel matrixself.
+
+        As a result, if you want to use a :obj:`gpytorch.kernels.Kernel` object just to get an actual
+        :obj:`torch.tensor` representing the covariance matrix, you may need to call the
+        :func:`gpytorch.lazy.LazyVariable.evaluate` method on the output.
+
+    Example:
+        >>> covar_module = gpytorch.kernels.LinearKernel()
+        >>> x1 = torch.randn(50, 3)
+        >>> lazy_covar_matrix = covar_module(x1) # Returns a RootLazyVariable
+        >>> tensor_covar_matrix = lazy_covar_matrix.evaluate() # Gets the actual tensor for this kernel matrix
+    """
     def __init__(
         self,
         has_lengthscale=False,
@@ -22,6 +46,19 @@ class Kernel(Module):
         batch_size=1,
         log_lengthscale_bounds=None,
     ):
+        """
+        The base Kernel class handles both lengthscales and ARD.
+
+        Args:
+            has_lengthscale (bool): If True, we will register a :obj:`torch.nn.Parameter` named `log_lengthscale`
+            ard_num_dims (int): If not None, the `log_lengthscale` parameter will have this many entries.
+            log_lengthscale_prior (:obj:`gpytorch.priors.Prior`): Prior over the log lengthscale
+            active_dims (list): List of data dimensions to evaluate this Kernel on.
+            batch_size (int): If training or testing multiple GPs simultaneously, this is how many lengthscales to
+                              register.
+            log_lengthscale_bounds (tuple): Deprecated min and max values for the lengthscales. If supplied, this
+                                            now registers a :obj:`gpytorch.priors.SmoothedBoxPrior`
+        """
         super(Kernel, self).__init__()
         if active_dims is not None and not torch.is_tensor(active_dims):
             active_dims = torch.tensor(active_dims, dtype=torch.long)
@@ -77,6 +114,14 @@ class Kernel(Module):
 
 
 class AdditiveKernel(Kernel):
+    """
+    A Kernel that supports summing over multiple component kernels.
+
+    Example:
+        >>> covar_module = RBFKernel(active_dims=torch.tensor([1])) + RBFKernel(active_dims=torch.tensor([2]))
+        >>> x1 = torch.randn(50, 2)
+        >>> additive_kernel_matrix = covar_module(x1)
+    """
     def __init__(self, *kernels):
         super(AdditiveKernel, self).__init__()
         self.kernels = ModuleList(kernels)
@@ -90,6 +135,14 @@ class AdditiveKernel(Kernel):
 
 
 class ProductKernel(Kernel):
+    """
+    A Kernel that supports elementwise multiplying multiple component kernels together.
+
+    Example:
+        >>> covar_module = RBFKernel(active_dims=torch.tensor([1])) * RBFKernel(active_dims=torch.tensor([2]))
+        >>> x1 = torch.randn(50, 2)
+        >>> kernel_matrix = covar_module(x1) # The RBF Kernel already decomposes multiplicatively, so this is foolish!
+    """
     def __init__(self, *kernels):
         super(ProductKernel, self).__init__()
         self.kernels = ModuleList(kernels)
