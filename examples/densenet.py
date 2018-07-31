98cf018df0def6d79f42c91519a4b14f82deea74
Max Balandat <balandat@fb.com>
2018-07-31 16:58:56 -0700

    Add py2 compatibility imports to densenet example

--- examples/densenet.py
+++ examples/densenet.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 # This implementation is based on the DenseNet-BC implementation in torchvision
 # https://github.com/pytorch/vision/blob/master/torchvision/models/densenet.py
 
