f37b06d145306c579601e1817bf115ca0fd000fe
Geoff Pleiss <gpleiss@gmail.com>
2018-08-22 16:25:15 -0700

    Update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,9 +13,8 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-import sys
+import gpytorch
 import sphinx_rtd_theme
-sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
