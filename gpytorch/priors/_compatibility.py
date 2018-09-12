e945d461832cd28d1b0d26bce7389df6b2c23b3b
Geoff Pleiss <gpleiss@gmail.com>
2018-09-12 18:01:05 -0400

    Include deprecation warnings

--- gpytorch/priors/_compatibility.py
+++ gpytorch/priors/_compatibility.py
@@ -3,20 +3,18 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import logging
+import warnings
 import torch
 from gpytorch.priors import SmoothedBoxPrior
-
-logger = logging.getLogger()
 
 
 def _bounds_to_prior(prior, bounds, batch_size=None, log_transform=True):
     if prior is not None:
         return prior
     elif bounds is not None:
-        logger.warning(
+        warnings.warn(
             "Parameter bounds have been deprecated and will be removed in a future release. "
-            "Please either remove them or use a SmoothedBoxPrior instead!"
+            "Please either remove them or use a SmoothedBoxPrior instead!", DeprecationWarning
         )
         a = torch.full((batch_size or 1,), float(bounds[0]))
         b = torch.full((batch_size or 1,), float(bounds[1]))
