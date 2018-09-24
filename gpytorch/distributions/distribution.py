b92e3a750b4b381a463057c7be493bd7fe44465c
Geoff Pleiss <gpleiss@gmail.com>
2018-09-24 19:03:59 -0400

    Move confidence region to MVN distribution

--- gpytorch/distributions/distribution.py
+++ gpytorch/distributions/distribution.py
@@ -4,21 +4,6 @@ from torch.distributions import Distribution as TDistribution
 
 
 class Distribution(TDistribution):
-    def confidence_region(self):
-        """
-        Returns 2 standard deviations above and below the mean.
-
-        Returns:
-            Tuple[Tensor, Tensor]: pair of tensors of size (b x d) or (d), where
-                b is the batch size and d is the dimensionality of the random
-                variable. The first (second) Tensor is the lower (upper) end of
-                the confidence region.
-
-        """
-        std2 = self.stddev.mul_(2)
-        mean = self.mean
-        return mean.sub(std2), mean.add(std2)
-
     @property
     def islazy(self):
         return self._islazy
--- gpytorch/distributions/multivariate_normal.py
+++ gpytorch/distributions/multivariate_normal.py
@@ -114,6 +114,21 @@ class MultivariateNormal(TMultivariateNormal, Distribution):
         of the covariance matrix"""
         return self.loc + _batch_mv(self._unbroadcasted_scale_tril, base_samples)
 
+    def confidence_region(self):
+        """
+        Returns 2 standard deviations above and below the mean.
+
+        Returns:
+            Tuple[Tensor, Tensor]: pair of tensors of size (b x d) or (d), where
+                b is the batch size and d is the dimensionality of the random
+                variable. The first (second) Tensor is the lower (upper) end of
+                the confidence region.
+
+        """
+        std2 = self.stddev.mul_(2)
+        mean = self.mean
+        return mean.sub(std2), mean.add(std2)
+
     @property
     def variance(self):
         if self.islazy:
