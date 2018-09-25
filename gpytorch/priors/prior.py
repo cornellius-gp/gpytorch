b7503a3f26a8579f38da69bbb339dbad8b6fb848
Geoff Pleiss <gpleiss@gmail.com>
2018-09-25 11:48:50 -0400

    Update docstring for prior

--- gpytorch/priors/prior.py
+++ gpytorch/priors/prior.py
@@ -7,7 +7,13 @@ from torch.nn import Module
 
 
 class Prior(Distribution, Module, ABC):
-    """Priors ar imposed on hyperparameters"""
+    """
+    Base class for Priors in GPyTorch.
+    In GPyTorch, a parameter can be assigned a prior by passing it as the `prior` argument to
+    :func:`~gpytorch.module.register_parameter`. GPyTorch performs internal bookkeeping of priors,
+    and for each parameter with a registered prior includes the log probability of the parameter under its
+    respective prior in computing the Marginal Log-Likelihood.
+    """
 
     @property
     def log_transform(self):
