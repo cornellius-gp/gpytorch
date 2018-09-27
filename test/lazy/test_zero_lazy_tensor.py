97b9b2e89545d755b822ba444be98a99194d3720
Geoff Pleiss <gpleiss@gmail.com>
2018-09-27 12:07:44 -0400

    Improve add_diag test coverage and arg checking

--- test/lazy/test_zero_lazy_tensor.py
+++ test/lazy/test_zero_lazy_tensor.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 
 import torch
 import unittest
+from gpytorch.utils import approx_equal
 from gpytorch.lazy import ZeroLazyTensor
 
 
@@ -36,6 +37,42 @@ class TestZeroLazyTensor(unittest.TestCase):
         self.assertLess(torch.norm(res_one - torch.zeros(2, 4, 3)), 1e-4)
         self.assertLess(torch.norm(res_two - torch.zeros(5, 2, 3)), 1e-4)
         self.assertLess(torch.norm(res_three - torch.zeros(5, 4, 2)), 1e-4)
+
+    def test_add_diag(self):
+        diag = torch.tensor(1.5)
+        res = ZeroLazyTensor(5, 5).add_diag(diag).evaluate()
+        actual = torch.eye(5).mul(1.5)
+        self.assertTrue(approx_equal(res, actual))
+
+        diag = torch.tensor([1.5])
+        res = ZeroLazyTensor(5, 5).add_diag(diag).evaluate()
+        actual = torch.eye(5).mul(1.5)
+        self.assertTrue(approx_equal(res, actual))
+
+        diag = torch.tensor([1.5, 1.3, 1.2, 1.1, 2.])
+        res = ZeroLazyTensor(5, 5).add_diag(diag).evaluate()
+        actual = diag.diag()
+        self.assertTrue(approx_equal(res, actual))
+
+        diag = torch.tensor(1.5)
+        res = ZeroLazyTensor(2, 5, 5).add_diag(diag).evaluate()
+        actual = torch.eye(5).unsqueeze(0).repeat(2, 1, 1).mul(1.5)
+        self.assertTrue(approx_equal(res, actual))
+
+        diag = torch.tensor([1.5])
+        res = ZeroLazyTensor(2, 5, 5).add_diag(diag).evaluate()
+        actual = torch.eye(5).unsqueeze(0).repeat(2, 1, 1).mul(1.5)
+        self.assertTrue(approx_equal(res, actual))
+
+        diag = torch.tensor([1.5, 1.3, 1.2, 1.1, 2.])
+        res = ZeroLazyTensor(2, 5, 5).add_diag(diag).evaluate()
+        actual = diag.diag().unsqueeze(0).repeat(2, 1, 1)
+        self.assertTrue(approx_equal(res, actual))
+
+        diag = torch.tensor([[1.5, 1.3, 1.2, 1.1, 2.], [0, 1, 2, 1, 1]])
+        res = ZeroLazyTensor(2, 5, 5).add_diag(diag).evaluate()
+        actual = torch.cat([diag[0].diag().unsqueeze(0), diag[1].diag().unsqueeze(0)])
+        self.assertTrue(approx_equal(res, actual))
 
 
 if __name__ == "__main__":
