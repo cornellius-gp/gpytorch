0e9369d9a05970c27f798d37390bc7766ce7f9c8
Geoff Pleiss <gpleiss@gmail.com>
2018-10-02 10:25:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,21 @@ this_file = os.path.dirname(__file__)
 
 setup(
     name="gpytorch",
-    version="0.1",
+    version="0.1.0.rc1",
     description="An implementation of Gaussian Processes in Pytorch",
-    url="https://github.com/cornellius-gp/gpytorch",
+    url="https://gpytorch.ai",
     author="Jake Gardner, Geoff Pleiss",
-    author_email="jrg365@cornell.edu",
+    author_email="jrg365@cornell.edu, gpleiss@gmail.com",
     install_requires=[],
     setup_requires=[],
     packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+    ],
+    project_urls={
+        'Documentation': 'https://gpytorch.readthedocs.io',
+        'Source': 'https://github.com/cornellius-gp/gpytorch/',
+    },
     ext_package="",
 )
