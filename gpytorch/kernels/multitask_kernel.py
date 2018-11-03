51e90c152f390d757e0e6ee6ee755e06d2ec521d
Jake Gardner <gardner.jake@gmail.com>
2018-11-03 09:55:18 -0400

    Fix lazy evaluation bug in MultitaskKernel

--- gpytorch/kernels/multitask_kernel.py
+++ gpytorch/kernels/multitask_kernel.py
@@ -49,7 +49,7 @@ class MultitaskKernel(Kernel):
 
         covar_i = self.task_covar_module.covar_matrix
         covar_i = covar_i.repeat(x1.size(0), 1, 1)
-        covar_x = self.data_covar_module(x1, x2, **params)
+        covar_x = self.data_covar_module.forward(x1, x2, **params)
         if not isinstance(covar_x, LazyTensor):
             covar_x = NonLazyTensor(covar_x)
         res = KroneckerProductLazyTensor(covar_i, covar_x)
