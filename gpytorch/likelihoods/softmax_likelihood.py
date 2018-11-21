cc20c53a06e11083c6dd91d195d68af5ae3931d6
Jake Gardner <gardner.jake@gmail.com>
2018-11-21 12:16:53 -0500

    SoftmaxLikelihood handles single feature case

--- gpytorch/likelihoods/softmax_likelihood.py
+++ gpytorch/likelihoods/softmax_likelihood.py
@@ -46,6 +46,8 @@ class SoftmaxLikelihood(Likelihood):
     def variational_log_probability(self, latent_func, target):
         n_samples = settings.num_likelihood_samples.value()
         samples = latent_func.rsample(sample_shape=torch.Size((n_samples,)))
+        if samples.dim() == 2:
+            samples = samples.unsqueeze(-1).transpose(-2, -1)
         samples = samples.permute(1, 2, 0).contiguous()  # Now n_featuers, n_data, n_samples
         if samples.ndimension() != 3:
             raise RuntimeError("f should have 3 dimensions: features x data x samples")
