27abca583c61fa6b67ff51ebedbc232649c138d6
Geoff Pleiss <gpleiss@gmail.com>
2019-01-02 07:52:01 -0500

    v0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ this_file = os.path.dirname(__file__)
 
 setup(
     name="gpytorch",
-    version="0.1.0",
+    version="0.1.1",
     description="An implementation of Gaussian Processes in Pytorch",
     url="https://gpytorch.ai",
     author="Jake Gardner, Geoff Pleiss",
