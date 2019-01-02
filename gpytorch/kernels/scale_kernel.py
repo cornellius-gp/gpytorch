eca84104b741177b571b28901b03e7ef5e277b2e
Jake Gardner <gardner.jake@gmail.com>
2019-01-01 22:12:34 -0500

    Fix outputscale shaping for diag mode

--- gpytorch/kernels/scale_kernel.py
+++ gpytorch/kernels/scale_kernel.py
@@ -94,8 +94,11 @@ class ScaleKernel(Kernel):
             outputscales = outputscales.unsqueeze(1).repeat(1, x1.size(-1)).view(-1)
 
         orig_output = self.base_kernel.forward(x1, x2, diag=diag, batch_dims=batch_dims, **params)
-        if diag:
-            return delazify(orig_output) * outputscales
+
         if torch.is_tensor(orig_output):
             outputscales = outputscales.view(-1, *([1] * (orig_output.dim() - 1)))
+
+        if diag:
+            return delazify(orig_output) * outputscales
+
         return orig_output.mul(outputscales)
