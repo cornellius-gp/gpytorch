7d54fcfc8ff83e24ec496986d8178d35643583f3
dme65 <dme65@cornell.edu>
2019-01-03 23:29:24 -0500

    Working gpderiv in 1d

--- gpytorch/kernels/rbf_kernel_grad.py
+++ gpytorch/kernels/rbf_kernel_grad.py
@@ -34,56 +34,65 @@ class RBFKernelGrad(RBFKernel):
         )
 
     def forward(self, x1, x2, diag=False, **params):
-        _, n, d = x1.size()
+        if len(x1.size()) == 1:
+            d = 1
+            n1, n2 = x1.size()[0], x2.size()[0]
+        elif len(x1.size()) == 2:
+            n1, d = x1.size()
+            n2, _ = x2.size()
+        else:
+            _, n1, d = x1.size()
+            _, n2, _ = x2.size()
+
         if not diag:
-            K = torch.zeros(*x1.shape[:-2], n * (d + 1), n * (d + 1))
-            x1_ = x1.div(self.lengthscale)
-            x2_ = x2.div(self.lengthscale)
+            K = torch.zeros(*x1.shape[:-2], n1 * (d + 1), n2 * (d + 1))
+            x1_ = x1 / self.lengthscale
+            x2_ = x2 / self.lengthscale
             x1_, x2_ = self._create_input_grid(x1_, x2_, **params)
 
             all_diff = (x1_ - x2_)
             diff = all_diff.norm(2, dim=-1)
+            all_diff = all_diff.squeeze(len(all_diff.size()) - 1)
 
             # 1) Kernel block
             K_11 = diff.pow(2).div(-2).exp()
-            K[..., :n, :n] = K_11
+            K[..., :n1, :n2] = K_11
 
             shape_list = [1] * len(K_11.shape[:-2])
-            left_shape_list = shape_list + [d, 1]
-            right_shape_list = shape_list + [1, d]
 
-            # 2) Gradient block
-            K_rep = K_11.unsqueeze(-1).repeat(*(shape_list + [1, 1, d]))
+            # 2) First gradient block
+            K_rep = K_11.repeat(*(shape_list + [1, d]))
             all_K = (all_diff / self.lengthscale) * K_rep
-            deriv_K = all_K.transpose(-3, -1).transpose(-2, -1).contiguous().view(d * n, n)
+            deriv_K = all_K.contiguous().view(d * n1, n2)
+            K[..., :n1, n2:] = deriv_K
 
-            K[..., :n, n:] = deriv_K.t()
-            K[..., n:, :n] = deriv_K
+            # 3) Second gradient block
+            K_rep = K_11.repeat(*(shape_list + [d, 1]))
+            all_K = (all_diff / self.lengthscale) * K_rep
+            deriv_K = all_K.permute(0, 2, 1).contiguous().view(d * n2, n1)
+            K[..., n1:, :n2] = -deriv_K.transpose(0, 1)
 
-            # 3) Hessian block
+            # 4) Hessian block
+            outer_prod = all_diff.contiguous().view(d * n1, n2)
+            outer_prod = - (outer_prod * outer_prod) / self.lengthscale.pow(2)
+            I = torch.eye(d) / self.lengthscale.pow(2)
+            outer_prod += I.repeat(shape_list + [n1, n2])
+            repeated_K = K_11.repeat(shape_list + [d, d])
+            K[..., n1:, n2:] = outer_prod * repeated_K
 
-            outer_prod = all_diff.transpose(-3, -1).transpose(-2, -1).contiguous().view(d * n, n)
-            outer_prod = outer_prod.repeat(*right_shape_list)
-
-            repeated_K = K_11.repeat(*left_shape_list).repeat(*right_shape_list)
-
-            diag_part = DiagLazyTensor(NonLazyTensor(repeated_K).diag()).evaluate()
-            diag_part = diag_part / self.lengthscale.pow(2)
-
-            K[..., n:, n:] = diag_part - (outer_prod / self.lengthscale.pow(2)) * repeated_K
-
-            pi = torch.arange(n * (d + 1)).view(d + 1, n).t().contiguous().view((n * (d + 1)))
-            K = K[..., pi, :][..., :, pi]
-            print(K.shape)
+            pi1 = torch.arange(n1 * (d + 1)).view(d + 1, n1).t().contiguous().view((n1 * (d + 1)))
+            pi2 = torch.arange(n2 * (d + 1)).view(d + 1, n2).t().contiguous().view((n2 * (d + 1)))
+            K = K[..., pi1, :][..., :, pi2]
 
             return K
         else:
             kernel_diag = super(RBFKernelGrad, self).forward(x1, x2, diag=True)
+            print(kernel_diag.size())
             # TODO: This will change when ARD is supported
             grad_diags = (1 / self.lengthscale.squeeze().pow(2)).expand_as(kernel_diag).repeat(1, d)
 
             k_diag = torch.cat((kernel_diag, grad_diags), dim=-1)
-            pi = torch.arange(n * (d + 1)).view(d + 1, n).t().contiguous().view((n * (d + 1)))
+            pi = torch.arange(n1 * (d + 1)).view(d + 1, n2).t().contiguous().view((n1 * (d + 1)))
             return k_diag[..., pi]
 
     def size(self, x1, x2):
