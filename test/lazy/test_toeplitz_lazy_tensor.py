22377ca85729baaaa201539f76746e5f70be0de0
Geoff Pleiss <gpleiss@gmail.com>
2019-02-01 15:30:38 -0500

    Multi-batch NonLazyTensor, DiagLazyTensor, ToeplitzLazyTensor

--- test/lazy/test_toeplitz_lazy_tensor.py
+++ test/lazy/test_toeplitz_lazy_tensor.py
@@ -34,5 +34,27 @@ class TestToeplitzLazyTensorBatch(LazyTensorTestCase, unittest.TestCase):
         )
 
 
+class TestToeplitzLazyTensorMultiBatch(LazyTensorTestCase, unittest.TestCase):
+    seed = 0
+
+    def create_lazy_tensor(self):
+        toeplitz_column = torch.tensor([[2, -1, 0.5, 0.25], [4, 0.5, 0, 1]], dtype=torch.float)
+        toeplitz_column = toeplitz_column.repeat(3, 1, 1)
+        toeplitz_column.requires_grad_(True)
+        return ToeplitzLazyTensor(toeplitz_column)
+
+    def evaluate_lazy_tensor(self, lazy_tensor):
+        return torch.cat(
+            [
+                toeplitz.sym_toeplitz(lazy_tensor.column[0, 0]).unsqueeze(0),
+                toeplitz.sym_toeplitz(lazy_tensor.column[0, 1]).unsqueeze(0),
+                toeplitz.sym_toeplitz(lazy_tensor.column[1, 0]).unsqueeze(0),
+                toeplitz.sym_toeplitz(lazy_tensor.column[1, 1]).unsqueeze(0),
+                toeplitz.sym_toeplitz(lazy_tensor.column[2, 0]).unsqueeze(0),
+                toeplitz.sym_toeplitz(lazy_tensor.column[2, 1]).unsqueeze(0),
+            ]
+        ).view(3, 2, 4, 4)
+
+
 if __name__ == "__main__":
     unittest.main()
