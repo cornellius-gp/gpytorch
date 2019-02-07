31fc10b5876c8afdfe9929550a88cb93966b1ed1
Geoff Pleiss <gpleiss@gmail.com>
2019-02-07 15:10:52 -0500

    Remove unused (and slow) tridiag potrf/potrs

--- test/utils/test_cholesky.py
+++ test/utils/test_cholesky.py
@@ -2,26 +2,9 @@
 
 import unittest
 import warnings
-from test._utils import approx_equal
 
 import torch
-from gpytorch.utils.cholesky import psd_safe_cholesky, tridiag_batch_potrf, tridiag_batch_potrs
-
-
-class TestTriDiag(unittest.TestCase):
-    def test_potrf(self):
-        chol = torch.tensor([[1, 0, 0, 0], [2, 1, 0, 0], [0, 1, 2, 0], [0, 0, 2, 3]], dtype=torch.float).unsqueeze(0)
-        trid = chol.matmul(chol.transpose(-1, -2))
-
-        self.assertTrue(torch.equal(chol, tridiag_batch_potrf(trid, upper=False)))
-
-    def test_potrs(self):
-        chol = torch.tensor([[1, 0, 0, 0], [2, 1, 0, 0], [0, 1, 2, 0], [0, 0, 2, 3]], dtype=torch.float).unsqueeze(0)
-
-        mat = torch.randn(1, 4, 3)
-        self.assertTrue(
-            approx_equal(torch.potrs(mat[0], chol[0], upper=False), tridiag_batch_potrs(mat, chol, upper=False)[0])
-        )
+from gpytorch.utils.cholesky import psd_safe_cholesky
 
 
 class TestPSDSafeCholesky(unittest.TestCase):
