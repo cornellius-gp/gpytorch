a8d51ee1915355c21b32f992db95bc2a12c8ad15
Geoff Pleiss <gpleiss@gmail.com>
2019-02-10 13:49:34 -0500

    InterpolatedLazyTensor is multi-batch

--- gpytorch/utils/sparse.py
+++ gpytorch/utils/sparse.py
@@ -23,32 +23,22 @@ def make_sparse_from_indices_and_values(interp_indices, interp_values, num_rows)
         raise RuntimeError("interp_indices and interp_values should be tensors")
 
     # Is it batch mode?
-    is_batch = interp_indices.ndimension() > 2
-    if is_batch:
-        batch_size, n_target_points, n_coefficients = interp_values.size()
-    else:
-        n_target_points, n_coefficients = interp_values.size()
+    batch_shape = interp_values.shape[:-2]
+    n_target_points, n_coefficients = interp_values.shape[-2:]
 
     # Index tensor
-    row_tensor = torch.arange(0, n_target_points, dtype=torch.long, device=interp_values.device)
-    row_tensor.unsqueeze_(1)
-    if is_batch:
+    batch_tensors = []
+    for i, batch_size in enumerate(batch_shape):
         batch_tensor = torch.arange(0, batch_size, dtype=torch.long, device=interp_values.device)
-        batch_tensor.unsqueeze_(1).unsqueeze_(2)
+        batch_tensor = batch_tensor.unsqueeze_(1).repeat(
+            batch_shape[:i].numel(),
+            batch_shape[i + 1:].numel() * n_target_points * n_coefficients
+        ).view(-1)
+        batch_tensors.append(batch_tensor)
 
-        row_tensor = row_tensor.repeat(batch_size, 1, n_coefficients)
-        batch_tensor = batch_tensor.repeat(1, n_target_points, n_coefficients)
-        index_tensor = torch.stack(
-            [
-                batch_tensor.contiguous().view(-1),
-                interp_indices.contiguous().view(-1),
-                row_tensor.contiguous().view(-1),
-            ],
-            0,
-        )
-    else:
-        row_tensor = row_tensor.repeat(1, n_coefficients)
-        index_tensor = torch.cat([interp_indices.contiguous().view(1, -1), row_tensor.contiguous().view(1, -1)], 0)
+    row_tensor = torch.arange(0, n_target_points, dtype=torch.long, device=interp_values.device)
+    row_tensor = row_tensor.unsqueeze_(1).repeat(batch_shape.numel(), n_coefficients).view(-1)
+    index_tensor = torch.stack([*batch_tensors, interp_indices.contiguous().view(-1), row_tensor], 0)
 
     # Value tensor
     value_tensor = interp_values.contiguous().view(-1)
@@ -58,17 +48,12 @@ def make_sparse_from_indices_and_values(interp_indices, interp_values, num_rows)
         index_tensor = index_tensor.index_select(1, nonzero_indices)
         value_tensor = value_tensor.index_select(0, nonzero_indices)
     else:
-        index_tensor = index_tensor.resize_(3 if is_batch else 2, 1).zero_()
+        index_tensor = index_tensor.resize_(interp_indices.dim(), 1).zero_()
         value_tensor = value_tensor.resize_(1).zero_()
-
-    # Size
-    if is_batch:
-        interp_size = torch.Size([batch_size, num_rows, n_target_points])
-    else:
-        interp_size = torch.Size([num_rows, n_target_points])
 
     # Make the sparse tensor
     type_name = value_tensor.type().split(".")[-1]  # e.g. FloatTensor
+    interp_size = torch.Size((*batch_shape, num_rows, n_target_points))
     if index_tensor.is_cuda:
         cls = getattr(torch.cuda.sparse, type_name)
     else:
