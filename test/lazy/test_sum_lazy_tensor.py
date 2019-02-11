0c07e79919065087b48cc788f2c5b2788b13d2b3
Geoff Pleiss <gpleiss@gmail.com>
2019-02-11 09:09:16 -0500

    Remainder of LTs are multi-batch friendly

--- test/lazy/test_sum_lazy_tensor.py
+++ test/lazy/test_sum_lazy_tensor.py
@@ -2,7 +2,7 @@
 
 import torch
 import unittest
-from gpytorch.lazy import ToeplitzLazyTensor
+from gpytorch.lazy import ToeplitzLazyTensor, NonLazyTensor
 from test.lazy._lazy_tensor_test_case import LazyTensorTestCase
 
 
@@ -30,6 +30,23 @@ class TestSumLazyTensorBatch(LazyTensorTestCase, unittest.TestCase):
         c2 = torch.tensor([[2, 0.5, 0, 0], [6, 0, 1, -1]], dtype=torch.float, requires_grad=True)
         t2 = ToeplitzLazyTensor(c2)
         return t1 + t2
+
+    def evaluate_lazy_tensor(self, lazy_tensor):
+        tensors = [lt.evaluate() for lt in lazy_tensor.lazy_tensors]
+        return sum(tensors)
+
+
+class TestSumLazyTensorMultiBatch(LazyTensorTestCase, unittest.TestCase):
+    seed = 0
+    # Because these LTs are large, we'll skil the big tests
+    skip_slq_tests = True
+
+    def create_lazy_tensor(self):
+        mat1 = torch.randn(2, 3, 4, 4)
+        lt1 = NonLazyTensor(mat1 @ mat1.transpose(-1, -2))
+        mat2 = torch.randn(2, 3, 4, 4)
+        lt2 = NonLazyTensor(mat2 @ mat2.transpose(-1, -2))
+        return lt1 + lt2
 
     def evaluate_lazy_tensor(self, lazy_tensor):
         tensors = [lt.evaluate() for lt in lazy_tensor.lazy_tensors]
