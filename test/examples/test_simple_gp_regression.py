a4816c2680a7da8426dbcd9b8d228ad7a7272c0e
balandat <balandat@fb.com>
2019-02-12 12:50:47 -0800

    Fix missing import

--- test/examples/test_simple_gp_regression.py
+++ test/examples/test_simple_gp_regression.py
@@ -4,7 +4,7 @@ import os
 import random
 import unittest
 from math import exp, pi
-from test._utils import approx_equal
+from test._utils import approx_equal, least_used_cuda_device
 
 import gpytorch
 import torch
