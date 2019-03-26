815a30d40188c1d39005de990cf323679a780d2d
Jake Gardner <jake.gardner@uber.com>
2019-03-26 13:21:41 -0700

    Fix half matern kernel name

--- gpytorch/kernels/half_matern_kernel.py
+++ gpytorch/kernels/half_matern_kernel.py
@@ -2,7 +2,7 @@ from .matern_kernel import MaternKernel
 from ..lazy import HalfNonLazyTensor
 
 
-class HalfRBFKernel(MaternKernel):
+class HalfMaternKernel(MaternKernel):
     def forward(self, x1, x2, **params):
         base_out = super().forward(x1, x2, **params)
         return HalfNonLazyTensor(base_out.half())
