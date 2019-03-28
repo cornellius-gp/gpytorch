36c8384d61a5403fda11e5d06454cf47f8de3acd
Stephen Tyree <styree@nvidia.com>
2019-03-28 09:04:17 -0500

    more checks

--- gpytorch/kernels/half_matern_kernel.py
+++ gpytorch/kernels/half_matern_kernel.py
@@ -4,6 +4,8 @@ from ..lazy import HalfNonLazyTensor
 
 class HalfMaternKernel(MaternKernel):
     def forward(self, x1, x2, **params):
+        print(x1.dtype, x2.dtype)
+        print(params)
         base_out = super().forward(x1, x2, **params)
-        print(x1.dtype, x2.dtype, base_out.dtype)
+        print(base_out.dtype)
         return HalfNonLazyTensor(base_out.half())
