9309a8333b9b1ed44bca3c3a1bde21d618275a53
Jake <jrg365@cornell.edu>
2019-04-26 17:59:19 -0700

    0.3.2

--- gpytorch/__init__.py
+++ gpytorch/__init__.py
@@ -35,7 +35,7 @@ from .mlls import ExactMarginalLogLikelihood, VariationalMarginalLogLikelihood
 from .lazy import lazify, delazify, cat
 
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 __all__ = [
     # Submodules
