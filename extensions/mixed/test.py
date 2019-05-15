202a73bc13cbc4c9f0bdb6bff605017d9122ac7a
Alex Wang <kaw293@cornell.edu>
2019-05-14 22:59:44 -0700

    add fp32 comparisons to mixed tests

--- extensions/mixed/test.py
+++ extensions/mixed/test.py
@@ -51,9 +51,10 @@ if torch.allclose(true_C, C) and torch.allclose(true_C, C_):
 
 
 
-m = 2**15
-k = 2**9
-n = 2**12
+# tensor cores need multiples of 8
+m = 8**2
+k = 8**5
+n = 8**2
 
 print("Testing large matrices...")
 num_trials = 10
@@ -62,7 +63,7 @@ A = torch.randn(m, k, dtype=torch.float16, device="cuda")
 B = torch.randn(k, n, dtype=torch.float16, device="cuda")
 
 print(f"A has size {A.size()}")
-print(f"B has size {A.size()}")
+print(f"B has size {B.size()}")
 
 times = []
 for i in range(num_trials):
@@ -81,6 +82,19 @@ for i in range(num_trials):
     times.append(time.time() - start)
 times = np.array(times)
 print(f"Non-inplace torch.mm time ({num_trials} trials): {times.mean():.2e}")
+
+A = A.float()
+B = B.float()
+times = []
+for i in range(num_trials):
+    start = time.time()
+    res = torch.mm(A, B)
+    torch.cuda.synchronize()
+    times.append(time.time() - start)
+times = np.array(times)
+print(f"Non-inplace torch.mm time fp32 ({num_trials} trials): {times.mean():.2e}")
+A = A.half()
+B = B.half()
 
 times = []
 for i in range(num_trials):
@@ -104,15 +118,31 @@ for i in range(num_trials):
 times = np.array(times)
 print(f"Inplace torch.mm time ({num_trials} trials): {times.mean():.2e}")
 
+A = A.float()
+B = B.float()
+times = []
+for i in range(num_trials):
+    C_ = torch.empty(m, n, dtype=torch.float32, device="cuda")
+    torch.cuda.synchronize()
+    start = time.time()
+    res = torch.mm(A, B, out=C_)
+    torch.cuda.synchronize()
+    times.append(time.time() - start)
+times = np.array(times)
+print(f"Inplace torch.mm time fp32 ({num_trials} trials): {times.mean():.2e}")
+A = A.half()
+B = B.half()
+
 del A; del B; del C; del C_; del true_C;
 
 b = 2**3
+
 # batched
 A = torch.randn(b, m, k, dtype=torch.float16, device="cuda")
 B = torch.randn(b, k, n, dtype=torch.float16, device="cuda")
 
 print(f"A has size {A.size()}")
-print(f"B has size {A.size()}")
+print(f"B has size {B.size()}")
 
 times = []
 for i in range(num_trials):
@@ -121,7 +151,7 @@ for i in range(num_trials):
     torch.cuda.synchronize()
     times.append(time.time() - start)
 times = np.array(times)
-print(f"Batched non-inplace mixed.mm time ({num_trials} trials): {times.mean():.2e}")
+print(f"Batched non-inplace mixed.bmm time ({num_trials} trials): {times.mean():.2e}")
 
 times = []
 for i in range(num_trials):
@@ -134,6 +164,41 @@ print(f"Batched non-inplace torch.bmm time ({num_trials} trials): {times.mean():
 
 times = []
 for i in range(num_trials):
+    start = time.time()
+    res = torch.matmul(A, B)
+    torch.cuda.synchronize()
+    times.append(time.time() - start)
+times = np.array(times)
+print(f"Batched non-inplace torch.matmul time ({num_trials} trials): {times.mean():.2e}")
+
+A = A.float()
+B = B.float()
+times = []
+for i in range(num_trials):
+    start = time.time()
+    res = torch.bmm(A, B)
+    torch.cuda.synchronize()
+    times.append(time.time() - start)
+times = np.array(times)
+print(f"Batched non-inplace torch.bmm time fp32 ({num_trials} trials): {times.mean():.2e}")
+A = A.half()
+B = B.half()
+
+A = A.float()
+B = B.float()
+times = []
+for i in range(num_trials):
+    start = time.time()
+    res = torch.matmul(A, B)
+    torch.cuda.synchronize()
+    times.append(time.time() - start)
+times = np.array(times)
+print(f"Batched non-inplace torch.matmul time fp32 ({num_trials} trials): {times.mean():.2e}")
+A = A.half()
+B = B.half()
+
+times = []
+for i in range(num_trials):
     C_ = torch.empty(b, m, n, dtype=torch.float32, device="cuda")
     torch.cuda.synchronize()
     start = time.time()
@@ -141,7 +206,7 @@ for i in range(num_trials):
     torch.cuda.synchronize()
     times.append(time.time() - start)
 times = np.array(times)
-print(f"Batched inplace mixed.mm time ({num_trials} trials): {times.mean():.2e}")
+print(f"Batched inplace mixed.bmm time ({num_trials} trials): {times.mean():.2e}")
 
 times = []
 for i in range(num_trials):
@@ -153,3 +218,18 @@ for i in range(num_trials):
     times.append(time.time() - start)
 times = np.array(times)
 print(f"Batched inplace torch.bmm time ({num_trials} trials): {times.mean():.2e}")
+
+A = A.float()
+B = B.float()
+times = []
+for i in range(num_trials):
+    C_ = torch.empty(b, m, n, dtype=torch.float32, device="cuda")
+    torch.cuda.synchronize()
+    start = time.time()
+    res = torch.bmm(A, B, out=C_)
+    torch.cuda.synchronize()
+    times.append(time.time() - start)
+times = np.array(times)
+print(f"Batched inplace torch.bmm time fp32 ({num_trials} trials): {times.mean():.2e}")
+A = A.half()
+B = B.half()
