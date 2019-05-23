535c7c0090c5456c90c9a4f66f4a339477a020b2
Alex Wang <kaw293@cornell.edu>
2019-05-23 10:50:58 -0700

    fix mixed backward variable saving bug

--- extensions/mixed/mixed.py
+++ extensions/mixed/mixed.py
@@ -11,35 +11,24 @@ class AddMatmul(torch.autograd.Function):
             print(f"Size of A: {B.size()} is not a multiple of 8 in last two dims")
         if C is not None and (C.size(-1) % 8 != 0 or C.size(-2) % 8 != 0):
             print(f"Size of C: {C.size()} is not a multiple of 8 in last two dims")
-        if torch.any(torch.isnan(A)) or torch.any(torch.isnan(B)):
-            raise RuntimeError("Got nans as A and B inputs")
-        if C is not None and torch.any(torch.isnan(C)):
-            raise RuntimeError("Got nans in C")
         addmatmul = mixed_cpp.baddbmm if A.dim() > 2 else mixed_cpp.addmm
         C_ = addmatmul(alpha, A.contiguous(), B.contiguous(), beta, C.contiguous() if torch.is_tensor(C) else C)
-        #if torch.any(torch.isnan(C_)):
-        #    if C is None:
-        #        C_ = torch.matmul(A.double(), B.double()) * alpha
-        #    else:
-        #        C_ = torch.addmm(C.double(), A.double(), B.double(), alpha=alpha, beta=beta).float()
-        #    from IPython.core.debugger import set_trace; set_trace()
         if any(ctx.needs_input_grad):
-            ctx.save_for_backward(A, B, alpha, beta, C)
+            ctx.save_for_backward(A, B, C)
+            ctx.alpha = alpha
+            ctx.beta = beta
         return C_
 
     @staticmethod
     def backward(ctx, gradC_):
-        A, B, alpha, beta, C = ctx.saved_variables
+        A, B, C = ctx.saved_variables
+        alpha, beta = ctx.alpha, ctx.beta
         addmatmul = mixed_cpp.baddbmm if gradC_.dim() > 2 else mixed_cpp.addmm
-        gradA = addmatmul(alpha, gradC_, B.transpose(-1, -2), 0., None)
-        gradB = addmatmul(alpha, A.transpose(-1, -2), gradC_, 0., None)
-        if torch.any(torch.isnan(gradA)) or torch.any(torch.isnan(gradB)):
-            from IPython.core.debugger import set_trace; set_trace()
-        gradC = beta * gradC_ if C is not None else None
-        if gradC is not None and torch.any(torch.isnan(gradC)):
-            from IPython.core.debugger import set_trace; set_trace()
+        gradA = addmatmul(alpha, gradC_, B.transpose(-1, -2), 0., None).half()
+        gradB = addmatmul(alpha, A.transpose(-1, -2), gradC_, 0., None).half()
+        gradC = (beta * gradC_).half() if C is not None else None
 
-        return gradA.half(), gradB.half(), None, None, gradC.half()
+        return gradA, gradB, None, None, gradC
 
 
 def mm(A, B, C=None):
