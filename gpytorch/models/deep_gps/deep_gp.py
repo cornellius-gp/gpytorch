fix kl for warping_layer

gpytorch/models/deep_gps/deep_gp.py
@@ -97,6 +97,10 @@ class AbstractDeepGPHiddenLayer(AbstractVariationalGP):
         variational_dist_f = super(AbstractDeepGPHiddenLayer, self).__call__(inputs)
         #mean_qf = variational_dist_f.mean
         #std_qf = variational_dist_f.variance.sqrt()
+
+        loss_term = NegativeKLDivergence(self.variational_strategy)
+        self.update_added_loss_term("hidden_kl_divergence", loss_term)
+
         if hasattr(self, 'warping_layer') and self.warping_layer:
             return variational_dist_f
 
@@ -108,9 +112,6 @@ class AbstractDeepGPHiddenLayer(AbstractVariationalGP):
             samples = variational_dist_f.rsample(torch.Size([num_samples]))
             #samples = torch.distributions.Normal(mean_qf, std_qf).rsample(torch.Size([num_samples]))
             samples = samples.transpose(-2, -1)
-
-        loss_term = NegativeKLDivergence(self.variational_strategy)
-        self.update_added_loss_term("hidden_kl_divergence", loss_term)
 
         if hasattr(self, 'householder'):
             samples = self.householder(samples)