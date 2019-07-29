148645501f0929a55095ff2fc9f1a013ffb78235
Jake Gardner <jake.gardner@uber.com>
2019-07-29 14:44:39 -0700

    KeOps is an optional dependency

--- gpytorch/kernels/keops/keops_kernel.py
+++ gpytorch/kernels/keops/keops_kernel.py
@@ -1,13 +1,19 @@
 import torch
-from pykeops.torch import LazyTensor as KEOLazyTensor
 from ..kernel import Kernel
 from abc import abstractmethod
 
+try:
+    from pykeops.torch import LazyTensor as KEOLazyTensor
 
-class KeOpsKernel(Kernel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    class KeOpsKernel(Kernel):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
 
-    @abstractmethod
-    def covar_func(self, x1: torch.Tensor, x2: torch.Tensor) -> KEOLazyTensor:
-        raise NotImplementedError("KeOpsKernels must define a covar_func method")
+        @abstractmethod
+        def covar_func(self, x1: torch.Tensor, x2: torch.Tensor) -> KEOLazyTensor:
+            raise NotImplementedError("KeOpsKernels must define a covar_func method")
+
+except ImportError:
+    class KeOpsKernel(Kernel):
+        def __init__(self, *args, **kwargs):
+            raise RuntimeError("You must have KeOps installed to use a KeOpsKernel")
--- gpytorch/kernels/keops/rbf_kernel.py
+++ gpytorch/kernels/keops/rbf_kernel.py
@@ -1,38 +1,45 @@
 import torch
-from pykeops.torch import LazyTensor as KEOLazyTensor
-from ..kernel import Kernel
+
+from .keops_kernel import KeOpsKernel
 from ..rbf_kernel import postprocess_rbf
 from gpytorch.lazy import KeOpsLazyTensor
 
+try:
+    from pykeops.torch import LazyTensor as KEOLazyTensor
 
-class RBFKernel(Kernel):
-    def __init__(self, **kwargs):
-        super(RBFKernel, self).__init__(has_lengthscale=True, **kwargs)
+    class RBFKernel(KeOpsKernel):
+        def __init__(self, **kwargs):
+            super(RBFKernel, self).__init__(has_lengthscale=True, **kwargs)
 
-    def covar_func(self, x1, x2, diag=False):
-        # TODO: x1 / x2 size checks are a work around for a very minor bug in KeOps.
-        # This bug is fixed on KeOps master, and we'll remove that part of the check
-        # when they cut a new release.
-        if diag or x1.size(-2) == 1 or x2.size(-2) == 1:
-            return self.covar_dist(
-                x1, x2, square_dist=True, diag=diag,
-                dist_postprocess_func=postprocess_rbf,
-                postprocess=True
-            )
-        else:
-            with torch.autograd.enable_grad():
-                x1_ = KEOLazyTensor(x1[:, None, :])
-                x2_ = KEOLazyTensor(x2[None, :, :])
+        def covar_func(self, x1, x2, diag=False):
+            # TODO: x1 / x2 size checks are a work around for a very minor bug in KeOps.
+            # This bug is fixed on KeOps master, and we'll remove that part of the check
+            # when they cut a new release.
+            if diag or x1.size(-2) == 1 or x2.size(-2) == 1:
+                return self.covar_dist(
+                    x1, x2, square_dist=True, diag=diag,
+                    dist_postprocess_func=postprocess_rbf,
+                    postprocess=True
+                )
+            else:
+                with torch.autograd.enable_grad():
+                    x1_ = KEOLazyTensor(x1[:, None, :])
+                    x2_ = KEOLazyTensor(x2[None, :, :])
 
-                K = (-((x1_ - x2_) ** 2).sum(-1) / 2).exp()
+                    K = (-((x1_ - x2_) ** 2).sum(-1) / 2).exp()
 
-                return K
+                    return K
 
-    def forward(self, x1, x2, diag=False, **params):
-        x1_ = x1.div(self.lengthscale)
-        x2_ = x2.div(self.lengthscale)
-        if diag:
-            return self.covar_func(x1_, x2_, diag=True)
+        def forward(self, x1, x2, diag=False, **params):
+            x1_ = x1.div(self.lengthscale)
+            x2_ = x2.div(self.lengthscale)
+            if diag:
+                return self.covar_func(x1_, x2_, diag=True)
 
-        covar_func = lambda x1, x2, diag=False: self.covar_func(x1, x2, diag)
-        return KeOpsLazyTensor(x1_, x2_, covar_func)
+            covar_func = lambda x1, x2, diag=False: self.covar_func(x1, x2, diag)
+            return KeOpsLazyTensor(x1_, x2_, covar_func)
+
+except ImportError:
+    class RBFKernel(KeOpsKernel):
+        def __init__(self, *args, **kwargs):
+            super().__init__()
