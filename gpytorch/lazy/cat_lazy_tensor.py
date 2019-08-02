Fix addcmul broadcasting
PyTorch does not allow (any more, for addcmul operator) to resize the input tensor.
To allow the output shaped differently from the input, we have to save it in the temporary tensor.
We cannot do `res = torch.addcmul(res, -1, a, c)` because functions are supposed to modify res in-place.

Also makes sure the dtype for masks is dynamically chosen where needed, in order to avoid compatibility issues between pytorch stable and master (shoudl be removed after next pytorch release)

gpytorch/lazy/cat_lazy_tensor.py
@@ -9,6 +9,10 @@ from ..utils.getitem import _noop_index
 from .. import settings
 
 
+# TODO: Use bool instead of uint8 dtype once pytorch #21113 is in stable release
+bool_compat = (torch.ones(1) > 0).dtype
+
+
 def cat(inputs, dim=0, output_device=None):
     if all(torch.is_tensor(i) for i in inputs):
         return torch.cat(inputs, dim=dim)
@@ -137,8 +141,7 @@ class CatLazyTensor(LazyTensor):
 
         # Find out for which indices we switch to different tensors
         target_tensors = self.idx_to_tensor_idx[cat_dim_indices]
-        # TODO: Use bool instead of uint8 dtype once pytorch #21113 is in stable release
-        does_switch_tensor = torch.ones(target_tensors.numel() + 1, dtype=torch.uint8, device=self.device)
+        does_switch_tensor = torch.ones(target_tensors.numel() + 1, dtype=bool_compat, device=self.device)
         torch.ne(target_tensors[:-1], target_tensors[1:], out=does_switch_tensor[1:-1])
 
         # Get the LazyTensors that will comprise the new LazyTensor
@@ -190,8 +193,7 @@ class CatLazyTensor(LazyTensor):
         elif torch.is_tensor(cat_dim_indices):
             # Find out for which indices we switch to different tensors
             target_tensors = self.idx_to_tensor_idx[cat_dim_indices]
-            # TODO: Use bool instead of uint8 dtype once pytorch #21113 is in stable release
-            does_switch_tensor = torch.ones(target_tensors.numel() + 1, dtype=torch.uint8, device=self.device)
+            does_switch_tensor = torch.ones(target_tensors.numel() + 1, dtype=bool_compat, device=self.device)
             torch.ne(target_tensors[:-1], target_tensors[1:], out=does_switch_tensor[1:-1])
 
             # Get the LazyTensors that will comprise the new LazyTensor

gpytorch/utils/linear_cg.py
@@ -5,6 +5,10 @@ import warnings
 from .. import settings
 
 
+# TODO: Use bool instead of uint8 dtype once pytorch #21113 is in stable release
+bool_compat = (torch.ones(1) > 0).dtype
+
+
 def _default_preconditioner(x):
     return x.clone()
 
@@ -16,7 +20,8 @@ def _jit_linear_cg_updates(
 ):
     # # Update result
     # # result_{k} = result_{k-1} + alpha_{k} p_vec_{k-1}
-    torch.addcmul(result, alpha, curr_conjugate_vec, out=result)
+    result_ = torch.addcmul(result, alpha, curr_conjugate_vec)
+    result.set_(result_)
 
     # beta_{k} = (precon_residual{k}^T r_vec_{k}) / (precon_residual{k-1}^T r_vec_{k-1})
     beta.resize_as_(residual_inner_prod).copy_(residual_inner_prod)
@@ -179,16 +184,14 @@ def linear_cg(
         mul_storage = torch.empty_like(residual)
         alpha = torch.empty(*batch_shape, 1, rhs.size(-1), dtype=residual.dtype, device=residual.device)
         beta = torch.empty_like(alpha)
-        # TODO: Use bool instead of uint8 dtype once pytorch #21113 is in stable release
-        is_zero = torch.empty(*batch_shape, 1, rhs.size(-1), dtype=torch.uint8, device=residual.device)
+        is_zero = torch.empty(*batch_shape, 1, rhs.size(-1), dtype=bool_compat, device=residual.device)
 
     # Define tridiagonal matrices, if applicable
     if n_tridiag:
         t_mat = torch.zeros(
             n_tridiag_iter, n_tridiag_iter, *batch_shape, n_tridiag, dtype=alpha.dtype, device=alpha.device
         )
-        # TODO: Use bool instead of uint8 dtype once pytorch #21113 is in stable release
-        alpha_tridiag_is_zero = torch.empty(*batch_shape, n_tridiag, dtype=torch.uint8, device=t_mat.device)
+        alpha_tridiag_is_zero = torch.empty(*batch_shape, n_tridiag, dtype=bool_compat, device=t_mat.device)
         alpha_reciprocal = torch.empty(*batch_shape, n_tridiag, dtype=t_mat.dtype, device=t_mat.device)
         prev_alpha_reciprocal = torch.empty_like(alpha_reciprocal)
         prev_beta = torch.empty_like(alpha_reciprocal)
@@ -219,7 +222,8 @@ def linear_cg(
 
             # Update residual
             # residual_{k} = residual_{k-1} - alpha_{k} mat p_vec_{k-1}
-            torch.addcmul(residual, -1, alpha, mvms, out=residual)
+            residual_ = torch.addcmul(residual, -1, alpha, mvms)
+            residual.set_(residual_)
 
             # Update precond_residual
             # precon_residual{k} = M^-1 residual_{k}