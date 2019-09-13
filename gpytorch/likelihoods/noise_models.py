Add NoiseModelAddedLossTerm, fix computation if noise in HeterodskedasticNoise
Previously, when using a heteroskedastic noise model with a separate model providing the noise, joint training would not work properly because
1. the noise model would be evaluated in prior mode (wheras we need the noise model's posterior predictions for the noise in the actual model)
2. the MLL did not account for the MLL of the noise model.

For 1. the fix is to simply ensure that the noise model is in eval mode when called inside `HeteroskedasticNoise`'s `forward` method. Note that this also requires ensuring to not detach the test caches of the main model during evaluation, since o/w some of the noise model's parameters are chopped off from the compute graph.
For 2. we can use the the `NoiseModelAddedLossTerm`.

gpytorch/likelihoods/noise_models.py
@@ -11,6 +11,7 @@ from ..distributions import MultivariateNormal
 from ..lazy import DiagLazyTensor, ZeroLazyTensor
 from ..module import Module
 from ..utils.broadcasting import _mul_broadcast_shape
+from .. import settings
 
 
 class Noise(Module):
@@ -132,10 +133,14 @@ class HeteroskedasticNoise(Noise):
         batch_shape: Optional[torch.Size] = None,
         shape: Optional[torch.Size] = None
     ) -> DiagLazyTensor:
-        if len(params) == 1 and not torch.is_tensor(params[0]):
-            output = self.noise_model(*params[0])
-        else:
-            output = self.noise_model(*params)
+        training = self.noise_model.training  # keep track of mode
+        self.noise_model.eval()  # we want the posterior prediction of the noise model
+        with settings.detach_test_caches(False):
+            if len(params) == 1 and not torch.is_tensor(params[0]):
+                output = self.noise_model(*params[0])
+            else:
+                output = self.noise_model(*params)
+        self.noise_model.train(training)
         if not isinstance(output, MultivariateNormal):
             raise NotImplementedError(
                 "Currently only noise models that return a MultivariateNormal are supported"