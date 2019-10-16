4002a3edf048c1a8afbd17fcfa1011623b5cdb90
Wesley Maddox <wm326@cornell.edu>
2019-10-16 12:26:05 -0400

    wip: refactor how latents are done

--- gpytorch/kernels/spectral_gp_kernel.py
+++ gpytorch/kernels/spectral_gp_kernel.py
@@ -1,208 +1,205 @@
 #!/usr/bin/env python3
 
-#TODO: this needs to be cleaned up to gpytorch standards.
+# TODO: this needs to be cleaned up to gpytorch standards.
 # it probably won't load as is
 
 import math
 import torch
-import copy
 
-from scipy.signal import periodogram, welch
-from scipy.interpolate import interp1d
+from . import Kernel
 
-from torch.nn import ModuleList
-
-from . import Kernel, ScaleKernel, MaternKernel, GridKernel
-from ..likelihoods import GaussianLikelihood
-from ..priors import SmoothedBoxPrior, NormalPrior
-from ..models import ExactGP
-from ..distributions import MultivariateNormal
-from ..means import LogRBFMean, ZeroMean
-#from ..utils import spectral_init
-
-
-class ExactGPModel(ExactGP):
-    def __init__(self, train_x, train_y, likelihood, mean=ZeroMean, grid = True):
-        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
-        self.mean_module = mean()
-
-        self.covar_module = ScaleKernel(
-                MaternKernel(nu=1.5,
-                    lengthscale_prior = NormalPrior(torch.zeros(1), torch.ones(1),
-                                            transform=torch.exp)
-                    ),
-                    outputscale_prior = NormalPrior(torch.zeros(1), torch.ones(1),
-                                            transform=torch.exp)
-                )
-
-        self.grid = grid
-        if self.grid:
-            self.grid_module = GridKernel(self.covar_module, grid = train_x.unsqueeze(1))
-
-    def forward(self, x):
-        mean_x = self.mean_module(x)
-        if self.grid:
-            covar_x = self.grid_module(x)
-        else:
-            covar_x = self.covar_module(x)
-
-        return MultivariateNormal(mean_x, covar_x)
-
-def spectral_init(x, y, spacing, num_freq=500, omega_lim=None,
-                    rtn_oneside=True):
-    if spacing == 'even':
-        f, den = periodogram(y.cpu().numpy(), fs=1/(x[1] - x[0]).item(),
-                            return_onesided=rtn_oneside)
-    elif spacing == 'random':
-        interp_x = torch.linspace(x.min(), x.max()-1e-6, num_freq)
-        interp_y = interp1d(x.cpu().numpy(), y.cpu().numpy())(interp_x.cpu().numpy())
-
-        cand = 0.0
-        idx = 0
-        while cand < 1.e-23:
-            cand = (interp_x[idx+1] - interp_x[idx])
-            idx += 1
-        f, den = periodogram(interp_y, fs=1/(cand).item(), return_onesided=rtn_oneside)
-
-    ## get omegas ##
-    print(omega_lim)
-    if omega_lim is not None:
-        lw_bd = max(min(f), omega_lim[0])
-        up_bd = min(max(f), omega_lim[1])
-        omega = torch.linspace(lw_bd + 1e-7, up_bd - 1e-7, num_freq)
-    else:
-        omega = torch.linspace(min(f)+1e-6, 0.5 * (max(f)-1e-6), num_freq)
-
-    interp_den = interp1d(f, den)(omega.cpu().numpy())
-    interp_den += 1e-10
-    log_periodogram = torch.Tensor(interp_den).log()
-
-    return omega, log_periodogram
 
 class SpectralGPKernel(Kernel):
-    def __init__(self, integration='U', omega = None, num_locs = 50, omega_max = 0.2,
-                    normalize = False, transform = torch.exp, symmetrize = False,
-                    register_latent_params = True,
-                    **kwargs):
+    def __init__(
+        self,
+        train_inputs,
+        latent_lh=None,
+        latent_model=None,
+        num_locs=50,
+        period_factor=8.0,
+        normalize=False,
+        transform=torch.exp,
+        register_latent_params=True,
+        use_latent_model=False,
+        **kwargs
+    ):
         r"""
         integration: {U, MC} U is trapezoidal rule, MC is Mone Carlo w/ w \sim U(-pi, pi)
         omega (default None)
         num_locs: number of omegas
         normalize: enforce that integral = 1
         transform: S(\omega) := transform(latent_params(\omega)), default: torch.exp
-        symmetrize: use g^*(\omega) := 1/2 * (g(-\omega) + g(\omega))
         """
         super(SpectralGPKernel, self).__init__(**kwargs)
 
         self.normalize = normalize
-        self.integration = integration
         self.transform = transform
-        self.symmetrize = symmetrize
+        self.num_locs = num_locs
+        self.period_factor = period_factor
+        self.use_latent_model = use_latent_model
 
-        if omega is None:
-            if integration == 'U' and omega_max is not None:
-                omega = torch.linspace(0., omega_max, num_locs)
-            else:
-                omega = torch.randn(num_locs) * math.pi
-           
-        self.register_parameter('omega', torch.nn.Parameter(omega))
-        self.num_locs = len(omega)
-        self.omega.requires_grad = False
-        
         if register_latent_params:
-            self.register_parameter('latent_params', torch.nn.Parameter(torch.zeros(self.num_locs)))
+            self.register_parameter("latent_params", torch.nn.Parameter(torch.zeros(self.num_locs)))
         else:
             self.latent_params = None
 
-    def compute_kernel_values(self, tau, density, integration='U', normalize=True):
+        # begin initialization by choosing omega
+        omega = self._choose_frequencies(train_inputs, period_factor)
 
-        # expand tau \in \mathbb{batch x n x n x M}
-        expanded_tau = tau.unsqueeze(-1)
-        if len(tau.size()) == 3:
-            dims = [1,1,1,self.num_locs]
-        else:
-            dims = [1,1,self.num_locs]
-        expanded_tau = expanded_tau.repeat(*dims)#.float()
+        # TODO: check if omega actually needs to be a parameter
+        self.register_parameter("omega", torch.nn.Parameter(omega))
+        self.omega.requires_grad = False
 
-        # numerically compute integral in expanded space
-        integrand = density * torch.cos(2.0 * math.pi * self.omega * expanded_tau)
+        if use_latent_model:
+            # now initialize from data if we want to store a latent model
+            self._initialize_from_data(train_inputs, latent_lh, latent_model, period_factor, **kwargs)
 
-        if integration == 'MC':
-            # take mean
-            integral = integrand.mean(-1)
-        elif integration == 'U':
-            # compute trapezoidal rule for now
-            diff = self.omega[1:] - self.omega[:-1]
-            integral = (diff * (integrand[...,1:] + integrand[...,:-1]) / 2.0).sum(-1,keepdim=False)
+    def _choose_frequencies(self, train_inputs, period_factor=8.0):
+        x1 = train_inputs.unsqueeze(-1)
 
-            # divide by integral of density
-            if normalize:
-                norm_constant = (diff * (density[1:] + density[:-1]) / 2.0).sum()
+        max_tau = x1.max() - x1.min()
+        max_tau = period_factor * max_tau
+        omega = math.pi * 2.0 * torch.arange(self.num_locs, dtype=x1.dtype).div(max_tau)
 
-                integral = integral / norm_constant
+        return omega
 
-        return integral
-
-    def initialize_from_data(self, train_x, train_y, num_locs=100, omega_max=None, spacing='random',
-            latent_lh = None, latent_mod = None, pretrain = False, nonstat = False, **kwargs):
-        #from ..models import ExactGPModel
+    def _initialize_from_data(
+        self, train_inputs, latent_lh=None, latent_model=None, period_factor=8.0, latent_params_grad=True, **kwargs
+    ):
+        """
+        Spectral methods require a bit of hand-initialization - this is analogous to the SM
+        intialize_from_data method.
+        train_inputs: train locations
+        latent_lh: latent model's likliehood function, if non-standard
+        latent_model: latent model
+        period_factor: constant to multiply on period
+        """
         from ..priors import GaussianProcessPrior
-        #from ..trainer import trainer
+        device = train_inputs.device
 
-        # get omega and periodogram
-        if omega_max is not None:
-            omega_lim = (1.e-10, omega_max)
-        else:
-            omega_lim = None
 
-        self.omega.data, log_periodogram = spectral_init(train_x, train_y, spacing, num_freq=num_locs, omega_lim=omega_lim)
-
-        if nonstat:
-            self.omega.data = torch.linspace(1.e-10, omega_max, num_locs)
-            log_periodogram = torch.ones_like(self.omega)
-
+        log_periodogram = torch.ones_like(self.omega)
 
         self.num_locs = len(self.omega)
 
         # if latent model is passed in, use that
         if latent_lh is None:
-            self.latent_lh = GaussianLikelihood(noise_prior=SmoothedBoxPrior(1e-8, 1e-3))
+            from ..likelihoods import GaussianLikelihood
+            from ..priors import SmoothedBoxPrior
+
+            self.latent_lh = GaussianLikelihood(noise_prior=SmoothedBoxPrior(1e-8, 1e-3)).to(device)
         else:
             self.latent_lh = latent_lh
 
-        if latent_mod is None:
-            self.latent_mod = ExactGPModel(self.omega, log_periodogram, self.latent_lh, mean=LogRBFMean)
+        if latent_model is None:
+            from ..means import QuadraticMean
+            from ..priors import NormalPrior
+            from ..constraints import LessThan
+            from ..kernels import ScaleKernel, MaternKernel, GridKernel
+            from ..models import PyroVariationalGP
+            from ..distributions import MultivariateNormal
+            from ..variational import CholeskyVariationalDistribution, VariationalStrategy
+
+            # we construct a Scale(Matern 3/2) kernel with a quadratic mean by default
+            class PyroGPModel(PyroVariationalGP):
+                def __init__(self, train_x, train_y, likelihood, mean_module, covar_module):
+                    # Define all the variational stuff
+                    variational_distribution = CholeskyVariationalDistribution(
+                        num_inducing_points=int(train_x.numel())
+                    )
+                    variational_strategy = VariationalStrategy(
+                        self, train_x, variational_distribution
+                    )
+
+                    super(PyroGPModel, self).__init__(variational_strategy, likelihood, num_data=train_x.numel())
+                    self.mean_module = mean_module
+                    self.covar_module = covar_module
+
+                def forward(self, x):
+                    mean = self.mean_module(x)
+                    covar = self.covar_module(x)
+                    return MultivariateNormal(mean, covar)
+
+            # construct by default a LogRBF prior on the latent spectral GP
+            latent_mean = QuadraticMean().to(device)
+            latent_mean.register_prior(
+                "bias_prior",
+                prior=NormalPrior(torch.zeros(1, device=device), 100.0 * torch.ones(1, device=device), transform=None),
+                param_or_closure="bias"
+            )
+            latent_mean.register_constraint("quadratic_weights", constraint=LessThan(upper_bound=0.0),
+                                            )
+            latent_mean.register_prior(
+                "quadratic_weights_prior",
+                prior=NormalPrior(
+                    torch.zeros(1, device=device),
+                    100.0 * torch.ones(1, device=device),
+                    transform=torch.nn.functional.softplus,
+                ),
+                param_or_closure="quadratic_weights"
+            )
+
+            latent_covar = ScaleKernel(
+                MaternKernel(nu=1.5, lengthscale_prior=NormalPrior(torch.zeros(1), torch.ones(1), transform=torch.exp)),
+                outputscale_prior=NormalPrior(torch.zeros(1), torch.ones(1), transform=torch.exp),
+            )
+            # latent_covar = GridKernel(latent_covar, grid=self.omega.unsqueeze(-1))
+
+            self.latent_model = PyroGPModel(
+                self.omega, log_periodogram, self.latent_lh, mean_module=latent_mean, covar_module=latent_covar
+            )
         else:
-            self.latent_mod = latent_mod
-            #update the training data to include this set of omega and log_periodogram
-            self.latent_mod.set_train_data(self.omega, log_periodogram, strict=False)
+            self.latent_model = latent_model
+            # update the training data to include this set of omega and log_periodogram
+            self.latent_model.set_train_data(self.omega, log_periodogram, strict=False)
 
-        self.latent_mod.train()
-        self.latent_lh.train()
-
-        # now fit the latent model to the log periodogram
-        if pretrain:
-            trainer(self.omega, log_periodogram, self.latent_mod, self.latent_lh)
-
-        self.latent_mod.train()
+        self.latent_model.train()
         self.latent_lh.train()
 
         # set the latent g to be the demeaned periodogram
         # and make it not require a gradient (we're using ESS for it)
-        self.latent_params.data = log_periodogram
-        self.latent_params.requires_grad = False
+        if self.latent_params is None:
+            self.latent_params = log_periodogram
+        else:
+            self.latent_params.data = log_periodogram
+        if not latent_params_grad:
+            self.latent_params.requires_grad = False
 
         # clear cache and reset training data
-        self.latent_mod.set_train_data(inputs = self.omega, targets=self.latent_params.data, strict=False)
+        #self.latent_model.set_train_data(inputs=self.omega, targets=self.latent_params.data, strict=False)
 
         # register prior for latent_params as latent mod
-        latent_prior = GaussianProcessPrior(self.latent_mod, self.latent_lh)
-        self.register_prior('latent_prior', latent_prior, lambda: self.latent_params)
-        # print('max of self.omega', self.omega.max())
-        return self.latent_lh, self.latent_mod
+        latent_prior = GaussianProcessPrior(self.latent_model, self.latent_lh)
+        self.register_prior("latent_prior", latent_prior, lambda: self.latent_params)
 
-    def forward(self, x1, x2=None, diag=False, last_dim_is_batch=False,
-                **kwargs):
+        return self.latent_lh, self.latent_model
+
+    def compute_kernel_values(self, tau, density, normalize=False):
+
+        # expand tau \in \mathbb{R}^{batch x n x n x M}
+        expanded_tau = tau.unsqueeze(-1)
+        if len(tau.size()) == 3:
+            dims = [1, 1, 1, self.num_locs]
+        else:
+            dims = [1, 1, self.num_locs]
+        expanded_tau = expanded_tau.repeat(*dims)  # .float()
+
+        # numerically compute integral in expanded space
+        integrand = density * torch.cos(2.0 * math.pi * self.omega * expanded_tau)
+
+        # compute trapezoidal rule for now
+        diff = self.omega[1:] - self.omega[:-1]
+        integral = (diff * (integrand[..., 1:] + integrand[..., :-1]) / 2.0).sum(-1, keepdim=False)
+
+        # divide by integral of density
+        if normalize:
+            norm_constant = (diff * (density[1:] + density[:-1]) / 2.0).sum()
+
+            integral = integral / norm_constant
+
+        return integral
+
+    def forward(self, x1, x2=None, diag=False, last_dim_is_batch=False, **kwargs):
         x1_ = x1
         x2_ = x1 if x2 is None else x2
         if last_dim_is_batch:
@@ -214,27 +211,16 @@ class SpectralGPKernel(Kernel):
         # transform to enforce positivity
         density = self.transform(self.latent_params)
 
-        if self.symmetrize:
-            density = 0.5 * (density + torch.flip(density, [0]))
-
-        output = self.compute_kernel_values(tau, density=density,
-                                            integration=self.integration,
-                                            normalize=self.normalize)
+        # TODO: more efficient diagonal
+        output = self.compute_kernel_values(
+            tau, density=density, normalize=self.normalize
+        )
         if diag:
             output = output.diag()
         return output
 
-    def get_latent_mod(self, idx=None):
-        return self.latent_mod
+    def set_latent_params(self, new_latent_params):
+        # updates latent parameters and sets prediction strategy to be none
 
-    def get_latent_lh(self, idx=None):
-        return self.latent_lh
-
-    def get_omega(self, idx=None):
-        return self.omega
-
-    def get_latent_params(self, idx=None):
-        return self.latent_params
-
-    def set_latent_params(self, g, idx=None):
-        self.latent_params.data = g
+        self.latent_params = new_latent_params
+        self.prediction_strategy = None
--- gpytorch/means/quadratic_mean.py
+++ gpytorch/means/quadratic_mean.py
@@ -3,17 +3,18 @@
 import torch
 
 from .mean import Mean
-from ..priors import NormalPrior
-from ..constraints import LessThan
+#from ..priors import NormalPrior
+#from ..constraints import LessThan
+
 
 class QuadraticMean(Mean):
-    def __init__(self, input_size, batch_shape=torch.Size(), bias=True, weights=True):
+    def __init__(self, input_size=1, batch_shape=torch.Size(), bias=True, weights=True):
         r"""
         Implements a mean function that is quadratic_weights x^2 + weights x + bias
         """
         super().__init__()
         self.register_parameter(name='quadratic_weights',
-                                    parameter=torch.nn.Parameter(torch.randn(*batch_shape, input_size, 1)))
+                                parameter=torch.nn.Parameter(torch.randn(*batch_shape, input_size, 1)))
         if weights:
             self.register_parameter(name='weights',
                                     parameter=torch.nn.Parameter(torch.randn(*batch_shape, input_size, 1)))
@@ -34,21 +35,21 @@ class QuadraticMean(Mean):
             res = res + self.bias
         return res
 
-class LogRBFMean(QuadraticMean):
-    def __init__(self, input_size = 1):
-        r"""
-        implements the logarithm of a RBF spectral density with required constraints
-        output = c - t^2 / 2l
-        """
-        #TODO: see how unclean it would be to make this device independent
+# class LogRBFMean(QuadraticMean):
+#     def __init__(self, input_size = 1):
+#         r"""
+#         implements the logarithm of a RBF spectral density with required constraints
+#         output = c - t^2 / 2l
+#         """
+#         #TODO: see how unclean it would be to make this device independent
         
-        super(LogRBFMean, self).__init__(input_size, bias=True, weights=False)
+#         super(LogRBFMean, self).__init__(input_size, bias=True, weights=False)
         
-        self.register_prior('bias_prior', prior=NormalPrior(torch.zeros(1), 100.*torch.ones(1), transform=None), 
-            param_or_closure = 'bias')
+#         self.register_prior('bias_prior', prior=NormalPrior(torch.zeros(1), 100.*torch.ones(1), transform=None), 
+#             param_or_closure = 'bias')
         
-        self.register_constraint('quadratic_weights', LessThan(upper_bound=0.0))
-        self.register_prior('quadratic_weight_prior', param_or_closure='quadratic_weights', 
-            prior=NormalPrior(torch.zeros(1), 100.*torch.ones(1), transform=torch.nn.functional.softplus))
+#         self.register_constraint('quadratic_weights', LessThan(upper_bound=0.0))
+#         self.register_prior('quadratic_weight_prior', param_or_closure='quadratic_weights', 
+#             prior=NormalPrior(torch.zeros(1), 100.*torch.ones(1), transform=torch.nn.functional.softplus))
 
 
