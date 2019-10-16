82951ba3ffc3977a3310261e91be14166e5f428e
Geoff Pleiss <gpleiss@gmail.com>
2019-10-16 13:54:24 -0400

    Deprecate VariationalMarginalLogLikelihood

--- gpytorch/mlls/__init__.py
+++ gpytorch/mlls/__init__.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
+import warnings
 from .added_loss_term import AddedLossTerm
 from .exact_marginal_log_likelihood import ExactMarginalLogLikelihood
 from .inducing_point_kernel_added_loss_term import InducingPointKernelAddedLossTerm
 from .marginal_log_likelihood import MarginalLogLikelihood
 from .sum_marginal_log_likelihood import SumMarginalLogLikelihood
 from .variational_elbo import VariationalELBO, VariationalELBOEmpirical
-from .variational_marginal_log_likelihood import VariationalMarginalLogLikelihood
 from .noise_model_added_loss_term import NoiseModelAddedLossTerm
+
+
+# Deprecated for 0.4 release
+class VariationalMarginalLogLikelihood(VariationalELBO):
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            "VariationalMarginalLogLikelihood is deprecated. Please use VariationalELBO instead.",
+            DeprecationWarning
+        )
+        super().__init__(*args, **kwargs)
+
 
 __all__ = [
     "AddedLossTerm",
@@ -18,5 +29,4 @@ __all__ = [
     "SumMarginalLogLikelihood",
     "VariationalELBO",
     "VariationalELBOEmpirical",
-    "VariationalMarginalLogLikelihood",
 ]
