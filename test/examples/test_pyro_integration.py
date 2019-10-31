f73f721fda3b3d812fb5bcd491138fc03448a8ff
Geoff Pleiss <gpleiss@gmail.com>
2019-10-31 10:10:29 -0400

    Alternate low-level pyro interface

--- test/examples/test_pyro_integration.py
+++ test/examples/test_pyro_integration.py
@@ -82,6 +82,80 @@ try:
             return res
 
 
+    class LowLevelInterfaceClusterMultitaskGPModel(gpytorch.models.ApproximateGP):
+        def __init__(self, train_x, train_y, num_functions=2):
+            # Define all the variational stuff
+            inducing_points = torch.linspace(0, 1, 32).unsqueeze(-1).repeat(num_functions, 1, 1)
+            variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(
+                num_inducing_points=inducing_points.size(-2),
+                batch_shape=torch.Size([num_functions])
+            )
+            variational_strategy = gpytorch.variational.MultitaskVariationalStrategy(
+                gpytorch.variational.VariationalStrategy(self, inducing_points, variational_distribution)
+            )
+            super().__init__(variational_strategy)
+
+            # Mean, covar
+            self.mean_module = gpytorch.means.ZeroMean()
+            self.covar_module = gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.RBFKernel(batch_shape=torch.Size([num_functions])),
+                batch_shape=torch.Size([num_functions]),
+            )
+
+            # Values to store
+            self.name_prefix = "llcmgp"
+            self.num_data, self.num_tasks = train_y.shape
+            self.num_functions = num_functions
+
+            # Define likelihood stuff
+            self.register_parameter(
+                "variational_logits",
+                torch.nn.Parameter(torch.randn(self.num_tasks, self.num_functions))
+            )
+            self.register_parameter("raw_noise", torch.nn.Parameter(torch.tensor(0.)))
+
+        @property
+        def noise(self):
+            return torch.nn.functional.softplus(self.raw_noise)
+
+        def forward(self, x):
+            mean_x = self.mean_module(x)
+            covar_x = self.covar_module(x)
+            res = gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+            return res
+
+        def guide(self, x, y):
+            name_prefix = "llcmgp"
+            self.pyro_guide(x, name_prefix=name_prefix)
+            pyro.sample(
+                name_prefix + ".cluster_logits",
+                pyro.distributions.OneHotCategorical(logits=self.variational_logits).to_event(1)
+            )
+
+        def model(self, x, y):
+            pyro.module(self.name_prefix + ".gp", self)
+
+            # Draw sample from q(f)
+            function_samples = self.pyro_model(x, name_prefix=self.name_prefix)
+
+            # Draw samples of cluster assignments
+            cluster_assignment_samples = pyro.sample(
+                self.name_prefix + ".cluster_logits",
+                pyro.distributions.OneHotCategorical(
+                    logits=torch.zeros(self.num_tasks, self.num_functions)
+                ).to_event(1)
+            )
+
+            # Sample from observation distribution
+            obs_dist = pyro.distributions.Normal(
+                loc=(function_samples.unsqueeze(-2) * cluster_assignment_samples).sum(-1),
+                scale=self.noise.sqrt(),
+            ).to_event(1)
+            with pyro.plate(self.name_prefix + ".output_values_plate", obs_dist.batch_shape[-1], dim=-1):
+                with pyro.poutine.scale(scale=(self.num_data / y.size(-2))):
+                    return pyro.sample(self.name_prefix + ".y", obs_dist, obs=y)
+
+
     class TestPyroIntegration(BaseTestCase, unittest.TestCase):
         seed = 1
 
@@ -129,6 +203,40 @@ try:
             self.assertLess(mean_abs_error_task_2.squeeze().item(), 0.1)
             self.assertLess(mean_abs_error_task_3.squeeze().item(), 0.1)
             self.assertLess(mean_abs_error_task_4.squeeze().item(), 0.1)
+
+        def test_low_level_interface(self):
+            # Simple training data: let's try to learn sine and cosine functions
+            train_x = torch.linspace(0, 1, 100)
+
+            # y1 and y4 functions are sin(2*pi*x) with noise N(0, 0.05)
+            train_y1 = torch.sin(train_x * (2 * pi)) + torch.randn(train_x.size()) * 0.03
+            train_y4 = torch.sin(train_x * (2 * pi)) + torch.randn(train_x.size()) * 0.03
+            # y2 and y3 functions are -sin(2*pi*x) with noise N(0, 0.05)
+            train_y2 = -torch.sin(train_x * (2 * pi)) + torch.randn(train_x.size()) * 0.03
+            train_y3 = -torch.sin(train_x * (2 * pi)) + torch.randn(train_x.size()) * 0.03
+            # Create a train_y which interleaves the four
+            train_y = torch.stack([train_y1, train_y2, train_y3, train_y4], -1)
+            model = LowLevelInterfaceClusterMultitaskGPModel(train_x, train_y)
+            # Find optimal model hyperparameters
+            model.train()
+
+            # Use the adam optimizer
+            optimizer = pyro.optim.Adam({"lr": 0.03})
+            elbo = pyro.infer.Trace_ELBO(num_particles=16, vectorize_particles=True, retain_graph=True)
+            svi = pyro.infer.SVI(model.model, model.guide, optimizer, elbo)
+
+            n_iter = 150
+            for _ in range(n_iter):
+                loss = svi.step(train_x, train_y)
+
+            # Test the model inference
+            cluster_1_idx = model.variational_logits[0].max(dim=-1)[1].item()
+            cluster_2_idx = 1 - cluster_1_idx
+            cluster_probs = torch.softmax(model.variational_logits, dim=-1)
+            self.assertGreater(cluster_probs[0, cluster_1_idx].squeeze().item(), 0.9)
+            self.assertGreater(cluster_probs[1, cluster_2_idx].squeeze().item(), 0.9)
+            self.assertGreater(cluster_probs[2, cluster_2_idx].squeeze().item(), 0.9)
+            self.assertGreater(cluster_probs[3, cluster_1_idx].squeeze().item(), 0.9)
 
 except ImportError:
     pass
