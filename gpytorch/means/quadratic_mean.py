aabfc71882072a187274bcb89875d350f7fbdcc3
Wesley Maddox <wm326@cornell.edu>
2019-11-01 17:12:46 -0400

    add ability to constrain linear + quadratic mean

--- gpytorch/means/quadratic_mean.py
+++ gpytorch/means/quadratic_mean.py
@@ -6,28 +6,42 @@ from .mean import Mean
 
 
 class QuadraticMean(Mean):
-    def __init__(self, input_size=1, batch_shape=torch.Size(), weights_constraint = None,
-                bias_constraint = None, quadratic_weights_constraint = None, weights_prior=None, bias_prior=None,
-                quadratic_weights_prior=None):
+    def __init__(self, input_size=1, batch_shape=torch.Size(), raw_weights_constraint = None,
+                raw_bias_constraint = None, raw_quadratic_weights_constraint = None, weights_prior=None, bias_prior=None,
+                quadratic_weights_prior=None, use_weights=True, use_bias=True):
         r"""
         Implements a mean function that is quadratic_weights x^2 + weights x + bias
         """
         super().__init__()
         
-        self.register_parameter(name='quadratic_weights',
+        self.register_parameter(name='raw_quadratic_weights',
                                 parameter=torch.nn.Parameter(torch.zeros(*batch_shape, input_size, 1)))
         
-        self.register_parameter(name='weights',
-                                parameter=torch.nn.Parameter(torch.randn(*batch_shape, input_size, 1)))
+        if raw_quadratic_weights_constraint:
+            self.register_constraint('raw_quadratic_weights', raw_quadratic_weights_constraint)
+        else:
+            self.raw_quadratic_weights_constraint = None
 
-        self.register_parameter(name='bias', parameter=torch.nn.Parameter(torch.randn(*batch_shape, 1)))
+        if use_weights:
+            self.register_parameter(name='weights',
+                                    parameter=torch.nn.Parameter(torch.randn(*batch_shape, input_size, 1)))
 
-        if quadratic_weights_constraint is not None:
-            self.register_constraint('quadratic_weights', quadratic_weights_constraint)
-        if weights_constraint is not None:
-            self.register_constraint('weights', weights_constraint)        
-        if bias_constraint is not None:
-            self.register_constraint('bias', bias_constraint)
+            if raw_weights_constraint:
+                self.register_constraint('raw_weights', raw_weights_constraint)
+            else:
+                self.raw_weights_constraint = None
+        
+        self.use_weights = use_weights
+
+        if use_bias:
+            self.register_parameter(name='raw_bias', parameter=torch.nn.Parameter(torch.randn(*batch_shape, 1)))
+            
+            if raw_bias_constraint:
+                self.register_constraint('raw_bias', raw_bias_constraint)
+            else:
+                self.raw_bias_constraint = None
+        
+        self.use_bias = use_bias
 
         if quadratic_weights_prior is not None:
             self.register_prior('quadratic_weights_prior', quadratic_weights_prior, 'quadratic_weights')
@@ -39,7 +53,69 @@ class QuadraticMean(Mean):
     def forward(self, x):
         res = x.pow(2.0).matmul(self.quadratic_weights).squeeze(-1)
 
-        res = res + x.matmul(self.weights).squeeze(-1)
+        if self.use_weights:
+            res = res + x.matmul(self.weights).squeeze(-1)
 
-        res = res + self.bias
+        if self.use_bias:
+            res = res + self.bias
         return res
+
+    @property
+    def quadratic_weights(self):
+        if self.raw_quadratic_weights_constraint is not None:
+            return self.raw_quadratic_weights_constraint.transform(self.raw_quadratic_weights)
+        else:
+            return self.raw_quadratic_weights
+
+    @quadratic_weights.setter
+    def quadratic_weights(self, value):
+        self._set_quadratic_weights(value)
+    
+    def _set_quadratic_weights(self, value):
+        if not torch.is_tensor(value):
+            value = torch.as_tensor(value).to(self.raw_quadratic_weights)
+
+        if self.raw_quadratic_weights_constraint is not None:
+            self.initialize(raw_quadratic_weights=self.raw_quadratic_weights_constraint.inverse_transform(value))
+        else:
+            self.initialize(raw_quadratic_weights=value)
+
+    @property
+    def weights(self):
+        if self.raw_weights_constraint is not None:
+            return self.raw_weights_constraint.transform(self.raw_weights)
+        else:
+            return self.raw_weights
+
+    @weights.setter
+    def weights(self, value):
+        self._set_weights(value)
+    
+    def _set_weights(self, value):
+        if not torch.is_tensor(value):
+            value = torch.as_tensor(value).to(self.raw_weights)
+
+        if self.raw_weights_constraint is not None:
+            self.initialize(raw_weights=self.raw_weights_constraint.inverse_transform(value))
+        else:
+            self.initialize(raw_weights=value)
+
+    @property
+    def bias(self):
+        if self.raw_bias_constraint is not None:
+            return self.raw_bias_constraint.transform(self.raw_bias)
+        else:
+            return self.raw_bias
+
+    @bias.setter
+    def bias(self, value):
+        self._set_bias(value)
+
+    def _set_bias(self, value):
+        if not torch.is_tensor(value):
+            value = torch.as_tensor(value).to(self.raw_bias)
+
+        if self.raw_bias_constraint is not None:
+            self.initialize(raw_bias=self.raw_bias_constraint.inverse_transform(value))
+        else:
+            self.initialize(raw_bias=value)
