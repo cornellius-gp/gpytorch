97f6b4d441ef9633c0a6e05a7e4c2a4d310585fe
Jake Gardner <jake.gardner@uber.com>
2019-11-12 18:03:35 -0800

    WIP

--- gpytorch/kernels/spectral_autoregressive_flow_kernel.py
+++ gpytorch/kernels/spectral_autoregressive_flow_kernel.py
@@ -10,9 +10,12 @@ from .kernel import Kernel
 
 
 class SpectralAutoregressiveFlowKernel(Kernel):
-    def __init__(self, num_dims, hidden_factors=[8, 8], **kwargs):
+    def __init__(self, num_dims, stack_size=1, **kwargs):
         super(SpectralAutoregressiveFlowKernel, self).__init__(has_lengthscale=True, **kwargs)
-        self.dsf = BlockAutoregressive(num_dims, hidden_factors=hidden_factors)
+        if stack_size > 1:
+            self.dsf = torch.nn.ModuleList([BlockAutoregressive(num_dims, **kwargs) for _ in range(stack_size)])
+        else:
+            self.dsf = BlockAutoregressive(num_dims, **kwargs)
         self.num_dims = num_dims
 
     def _create_input_grid(self, x1, x2, diag=False, last_dim_is_batch=False, **params):
@@ -64,9 +67,9 @@ class SpectralAutoregressiveFlowKernel(Kernel):
             dsf = self.dsf
         dsf_dist = dist.TransformedDistribution(base_dist, dsf)
         if self.training:
-            Z = dsf_dist.rsample(torch.Size([256]))
+            Z = dsf_dist.rsample(torch.Size([2000]))
         else:
-            Z = dsf_dist.rsample(torch.Size([1000]))
+            Z = dsf_dist.rsample(torch.Size([2000]))
 
         if diag:
             diffs_times_Z = (Z.unsqueeze(-2) * diffs.unsqueeze(-3)).sum(-1)
@@ -74,4 +77,49 @@ class SpectralAutoregressiveFlowKernel(Kernel):
         else:
             diffs_times_Z = (Z.unsqueeze(-2).unsqueeze(-2) * diffs.unsqueeze(-4)).sum(-1)
             K = diffs_times_Z.mul(2 * math.pi).cos().mean(dim=-3)
+        return K
+
+
+class TrapezoidalSpectralAutoregressiveFlowKernel(SpectralAutoregressiveFlowKernel):
+    def __init__(self, num_dims, hidden_factors=[8, 8], **kwargs):
+        super().__init__(num_dims, hidden_factors, **kwargs)
+
+    def forward(self, x1, x2, diag=False, **params):
+        """
+        TODO: Update to not be a product kernel in D>1
+        """
+        x1_ = x1.div(self.lengthscale)
+        x2_ = x2.div(self.lengthscale)
+        x1_, x2_ = self._create_input_grid(x1_, x2_, diag=diag)
+
+        diffs = x1_ - x2_
+        base_dist = dist.Normal(
+            torch.zeros(self.num_dims, device=x1.device, dtype=x1.dtype),
+            torch.ones(self.num_dims, device=x1.device, dtype=x1.dtype),
+        )
+        if isinstance(self.dsf, torch.nn.ModuleList):
+            dsf = list(self.dsf)
+        else:
+            dsf = self.dsf
+        dsf_dist = dist.TransformedDistribution(base_dist, dsf)
+
+        if diag:
+            samples = dsf_dist.rsample(torch.Size([1000]))
+            diffs_times_Z = (samples.unsqueeze(-2) * diffs.unsqueeze(-3)).sum(-1)
+            log_probs = dsf_dist.log_prob(samples)
+            norm_log_probs = log_probs - torch.logsumexp(log_probs, -1)
+            norm_probs = norm_log_probs.exp().view(1000, 1)
+
+            values = diffs_times_Z.mul(2 * math.pi).cos().mul(norm_probs)  # I x n x n x d
+            K = values.mean(-2)
+        else:
+            samples = dsf_dist.rsample(torch.Size([1000]))
+            diffs_times_Z = (samples.unsqueeze(-2).unsqueeze(-2) * diffs.unsqueeze(-4)).sum(-1)
+            log_probs = dsf_dist.log_prob(samples)
+            norm_log_probs = log_probs - torch.logsumexp(log_probs, -1)
+            norm_probs = norm_log_probs.exp().view(1000, 1, 1)
+
+            values = diffs_times_Z.mul_(2 * math.pi).cos_().mul_(norm_probs)  # I x n x n x d
+            K = values.mean(-3)
+
         return K
