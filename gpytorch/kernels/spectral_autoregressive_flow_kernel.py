806980249d3d9d3032aa869c75954fd325b1c861
Jake Gardner <jake.gardner@uber.com>
2019-11-18 16:40:12 -0800

    WIP

--- gpytorch/kernels/spectral_autoregressive_flow_kernel.py
+++ gpytorch/kernels/spectral_autoregressive_flow_kernel.py
@@ -80,46 +80,117 @@ class SpectralAutoregressiveFlowKernel(Kernel):
         return K
 
 
-class TrapezoidalSpectralAutoregressiveFlowKernel(SpectralAutoregressiveFlowKernel):
-    def __init__(self, num_dims, hidden_factors=[8, 8], **kwargs):
-        super().__init__(num_dims, hidden_factors, **kwargs)
+class NonStationarySpectralAutoregressiveFlowKernel(SpectralAutoregressiveFlowKernel):
+    def __init__(self, num_dims, stack_size=1, **kwargs):
+        Kernel.__init__(self, has_lengthscale=True, **kwargs)
+        if stack_size > 1:
+            self.dsf = torch.nn.ModuleList([BlockAutoregressive(num_dims, **kwargs) for _ in range(stack_size)])
+            self.dsf2 = torch.nn.ModuleList([BlockAutoregressive(num_dims, **kwargs) for _ in range(stack_size)])
+        else:
+            self.dsf = BlockAutoregressive(num_dims, **kwargs)
+            self.dsf2 = BlockAutoregressive(num_dims, **kwargs)
+        self.num_dims = num_dims
 
     def forward(self, x1, x2, diag=False, **params):
-        """
-        TODO: Update to not be a product kernel in D>1
-        """
         x1_ = x1.div(self.lengthscale)
         x2_ = x2.div(self.lengthscale)
         x1_, x2_ = self._create_input_grid(x1_, x2_, diag=diag)
 
-        diffs = x1_ - x2_
         base_dist = dist.Normal(
             torch.zeros(self.num_dims, device=x1.device, dtype=x1.dtype),
             torch.ones(self.num_dims, device=x1.device, dtype=x1.dtype),
         )
+
+        base_dist2 = dist.Normal(
+            torch.zeros(self.num_dims, device=x1.device, dtype=x1.dtype),
+            torch.ones(self.num_dims, device=x1.device, dtype=x1.dtype),
+        )
+
+        if isinstance(self.dsf, torch.nn.ModuleList):
+            dsf = list(self.dsf)
+            dsf2 = list(self.dsf2)
+        else:
+            dsf = self.dsf
+            dsf2 = self.dsf2
+
+        dsf_dist = dist.TransformedDistribution(base_dist, dsf)
+        dsf_dist2 = dist.TransformedDistribution(base_dist2, dsf2)
+
+        if self.training:
+            Z = dsf_dist.rsample(torch.Size([2000]))
+            Z2 = dsf_dist2.rsample(torch.Size([2000]))
+        else:
+            Z = dsf_dist.rsample(torch.Size([2000]))
+            Z2 = dsf_dist2.rsample(torch.Size([2000]))
+
+        Z1_ = Z.unsqueeze(-1)
+        Z2_ = Z2.unsqueeze(-1)
+        if not diag:
+            Z1_ = Z1_.unsqueeze(-1)
+            Z2_ = Z2_.unsqueeze(-1)
+
+        first = (x1_ * Z1_).sum(-1).mul(2 * math.pi).cos()
+        second = (x2_ * Z2_).sum(-1).mul(2 * math.pi).cos()
+        third = (x1_ * Z1_).sum(-1).mul(2 * math.pi).sin()
+        fourth = (x2_ * Z2_).sum(-1).mul(2 * math.pi).sin()
+
+        kernel_mat = first * second + third * fourth
+        kernel_mat = kernel_mat.mean(0)
+
+        from IPython.core.debugger import set_trace
+
+        set_trace()
+
+        return kernel_mat
+
+
+class NSSAFKernel2(SpectralAutoregressiveFlowKernel):
+    def __init__(self, num_dims, stack_size=1, **kwargs):
+        Kernel.__init__(self, has_lengthscale=True, **kwargs)
+        if stack_size > 1:
+            self.dsf = torch.nn.ModuleList([BlockAutoregressive(2 * num_dims, **kwargs) for _ in range(stack_size)])
+        else:
+            self.dsf = BlockAutoregressive(2 * num_dims, **kwargs)
+
+        self.num_dims = num_dims
+
+    def forward(self, x1, x2, diag=False, **params):
+        x1_ = x1.div(self.lengthscale)
+        x2_ = x2.div(self.lengthscale)
+        x1_, x2_ = self._create_input_grid(x1_, x2_, diag=diag)
+
+        base_dist = dist.Normal(
+            torch.zeros(self.num_dims * 2, device=x1.device, dtype=x1.dtype),
+            torch.ones(self.num_dims * 2, device=x1.device, dtype=x1.dtype),
+        )
+
         if isinstance(self.dsf, torch.nn.ModuleList):
             dsf = list(self.dsf)
         else:
             dsf = self.dsf
+
         dsf_dist = dist.TransformedDistribution(base_dist, dsf)
 
-        if diag:
-            samples = dsf_dist.rsample(torch.Size([1000]))
-            diffs_times_Z = (samples.unsqueeze(-2) * diffs.unsqueeze(-3)).sum(-1)
-            log_probs = dsf_dist.log_prob(samples)
-            norm_log_probs = log_probs - torch.logsumexp(log_probs, -1)
-            norm_probs = norm_log_probs.exp().view(1000, 1)
+        Z = dsf_dist.rsample(torch.Size([128]))
+        Z1 = Z[:, : self.num_dims]
+        Z2 = Z[:, self.num_dims :]
 
-            values = diffs_times_Z.mul(2 * math.pi).cos().mul(norm_probs)  # I x n x n x d
-            K = values.mean(-2)
-        else:
-            samples = dsf_dist.rsample(torch.Size([1000]))
-            diffs_times_Z = (samples.unsqueeze(-2).unsqueeze(-2) * diffs.unsqueeze(-4)).sum(-1)
-            log_probs = dsf_dist.log_prob(samples)
-            norm_log_probs = log_probs - torch.logsumexp(log_probs, -1)
-            norm_probs = norm_log_probs.exp().view(1000, 1, 1)
+        Z1_ = torch.cat(([Z1, Z2, -Z1, -Z2]))
+        Z2_ = torch.cat(([Z1, Z2, -Z1, -Z2]))
 
-            values = diffs_times_Z.mul_(2 * math.pi).cos_().mul_(norm_probs)  # I x n x n x d
-            K = values.mean(-3)
+        Z1_ = Z1_.unsqueeze(1)
+        Z2_ = Z2_.unsqueeze(1)
+        if not diag:
+            Z1_ = Z1_.unsqueeze(1)
+            Z2_ = Z2_.unsqueeze(1)
+
+        x1z1 = x1_ * Z1_  # s x n x 1 x d
+        x2z2 = x2_ * Z2_  # s x 1 x n x d
+
+        x1z1 = x1z1.sum(-1)  # s x n x 1
+        x2z2 = x2z2.sum(-1)  # s x 1 x n
+
+        diff = x1z1 - x2z2  # s x n x n
+        K = diff.mul(2 * math.pi).cos().mean(0)  # n x n
 
         return K
