361a2490229705a1d62d01e3e889af1197579d57
Jake Gardner <gardner.jake@gmail.com>
2020-02-12 09:24:21 -0800

    LCM fixes

--- gpytorch/variational/batch_lcm_variational_strategy.py
+++ gpytorch/variational/batch_lcm_variational_strategy.py
@@ -22,7 +22,7 @@ class BatchLCMVariationalStrategy(_VariationalStrategy):
     """
 
     def __init__(
-        self, base_variational_strategy, num_tasks, num_functions=0, num_groups=1, function_dim=0, group_dim=1
+        self, base_variational_strategy, num_tasks, num_functions=0, num_groups=None, function_dim=0, group_dim=1
     ):
         Module.__init__(self)
         self.base_variational_strategy = base_variational_strategy
@@ -33,17 +33,22 @@ class BatchLCMVariationalStrategy(_VariationalStrategy):
         assert function_dim < group_dim
         self.batch_shape = self.base_variational_strategy._variational_distribution.batch_shape
         self.function_dim = function_dim if function_dim < 0 else (function_dim - len(self.batch_shape))
-        self.group_dim = group_dim if group_dim < 0 else (group_dim - len(self.batch_shape))
+        if num_groups is None:
+            self.group_dim = None
+        else:
+            self.group_dim = group_dim if group_dim < 0 else (group_dim - len(self.batch_shape))
+
+            if not (self.batch_shape[self.group_dim] == num_groups or self.batch_shape[self.group_dim] == 1):
+                raise RuntimeError(
+                    f"Mismatch in num_groups: got a variational distribution of batch shape {self.batch_shape}, "
+                    f"expected the gruop dim {self.group_dim} to be {self.num_groups}."
+                )
+
         # Ensure the number of tasks/groups is equal to what we have in the variational distribution
         if not (self.batch_shape[self.function_dim] == num_functions or self.batch_shape[self.function_dim] == 1):
             raise RuntimeError(
                 f"Mismatch in num_functions: got a variational distribution of batch shape {self.batch_shape}, "
                 f"expected the function dim {self.function_dim} to be {self.num_functions}."
-            )
-        if not (self.batch_shape[self.group_dim] == num_groups or self.batch_shape[self.group_dim] == 1):
-            raise RuntimeError(
-                f"Mismatch in num_groups: got a variational distribution of batch shape {self.batch_shape}, "
-                f"expected the gruop dim {self.group_dim} to be {self.num_groups}."
             )
         self.batch_shape = list(self.batch_shape)
         del self.batch_shape[self.function_dim]
@@ -74,7 +79,10 @@ class BatchLCMVariationalStrategy(_VariationalStrategy):
         return self.base_variational_strategy.variational_params_initialized
 
     def kl_divergence(self):
-        return super().kl_divergence().sum(dim=[self.function_dim, self.group_dim])
+        if self.group_dim is not None:
+            return super().kl_divergence().sum(dim=[self.function_dim, self.group_dim])
+        else:
+            return super().kl_divergence().sum(dim=[self.function_dim])
 
     def __call__(self, x, prior=False):
         function_dist = self.base_variational_strategy(x, prior=prior)
@@ -82,12 +90,14 @@ class BatchLCMVariationalStrategy(_VariationalStrategy):
         num_batch = len(function_dist.batch_shape)
         num_dim = num_batch + len(function_dist.event_shape)
         function_dim = num_batch + self.function_dim
-        group_dim = num_batch + self.group_dim
 
         # Mean
         mean = function_dist.mean.permute(*range(0, function_dim), *range(function_dim + 1, num_dim), function_dim)
         mean = mean @ lcm_coefficients.transpose(-1, -2)
-        # mean = mean.sum(group_dim - 1)
+
+        if self.group_dim is not None:
+            group_dim = num_batch + self.group_dim
+            mean = mean.sum(group_dim - 1)
 
         # Covar
         covar = function_dist.lazy_covariance_matrix
@@ -95,7 +105,8 @@ class BatchLCMVariationalStrategy(_VariationalStrategy):
         lcm_factor = lcm_coefficients @ lcm_coefficients.transpose(-1, -2)
         lcm_factor = lcm_factor.expand(*covar.batch_shape, *lcm_factor.shape[-2:])
         covar = KroneckerProductLazyTensor(covar, lcm_factor)
-        # covar = covar.sum(group_dim - 1)  # - 1 because we summed over the function_dim
+        if self.group_dim is not None:
+            covar = covar.sum(group_dim - 1)  # - 1 because we summed over the function_dim
 
         # Done!
         function_dist = MultitaskMultivariateNormal(mean, covar)
