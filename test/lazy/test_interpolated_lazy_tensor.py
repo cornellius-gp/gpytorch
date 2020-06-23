98b8015a2af7a2e3dd3b3778219bc7fa0d578796
Jake Gardner <gardner.jake@gmail.com>
2020-06-23 12:22:39 -0400

    don't test (inv) sqrt matmul on rectangular cases

--- test/lazy/test_interpolated_lazy_tensor.py
+++ test/lazy/test_interpolated_lazy_tensor.py
@@ -170,6 +170,8 @@ class TestInterpolatedLazyTensorRectangular(LazyTensorTestCase, unittest.TestCas
     test_root_decomposition = empty_method
     test_root_decomposition_cholesky = empty_method
     test_root_inv_decomposition = empty_method
+    test_sqrt_inv_matmul = empty_method
+    test_sqrt_inv_matmul_no_lhs = empty_method
 
 
 if __name__ == "__main__":
