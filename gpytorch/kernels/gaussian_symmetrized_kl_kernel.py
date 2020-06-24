464614e3ee9a876a2401a4da3cdd9cdeaaac12a0
Wesley Maddox <wjm363@nyu.edu>
2020-06-24 09:54:46 -0400

    cleanup documentation & log var ratio

--- gpytorch/kernels/gaussian_symmetrized_kl_kernel.py
+++ gpytorch/kernels/gaussian_symmetrized_kl_kernel.py
@@ -11,9 +11,9 @@ def _symmetrized_kl(dist1, dist2, eps=1e-8):
     are the log variances.
     Args:
         :attr:`dist1` (torch.Tensor) has shapes batch x n x dimensions. The first half
-        of the last dimensions are the means, while the second half are the log-variances.
+            of the last dimensions are the means, while the second half are the log-variances.
         :attr:`dist2` (torch.Tensor) has shapes batch x n x dimensions. The first half
-        of the last dimensions are the means, while the second half are the log-variances.
+            of the last dimensions are the means, while the second half are the log-variances.
         :attr:`eps` (float) jitter term for the noise variance
     """
 
@@ -28,10 +28,11 @@ def _symmetrized_kl(dist1, dist2, eps=1e-8):
     dist2_var = eps + dist2_logvar.exp()
 
     var_ratio12 = dist1_var / dist2_var
-    log_var_ratio12 = var_ratio12.log()
+    # log_var_ratio12 = var_ratio12.log()
+    # note that the log variance ratio cancels because of the summed KL.
     loc_sqdiffs = (dist1_mean - dist2_mean).pow(2)
-    kl1 = 0.5 * (var_ratio12 + loc_sqdiffs / dist2_var - 1 - log_var_ratio12)
-    kl2 = 0.5 * (var_ratio12.reciprocal() + loc_sqdiffs / dist1_var - 1 + log_var_ratio12)
+    kl1 = 0.5 * (var_ratio12 + loc_sqdiffs / dist2_var - 1)
+    kl2 = 0.5 * (var_ratio12.reciprocal() + loc_sqdiffs / dist1_var - 1)
     symmetrized_kl = kl1 + kl2
     return symmetrized_kl.sum(-1).transpose(-1, -2)
 
