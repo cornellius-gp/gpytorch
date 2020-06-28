378c19e1422c00f91f7ce0e87de5b8f7d89799ed
Wesley Maddox <wjm363@nyu.edu>
2020-06-28 16:23:05 -0400

    backwards works but gradients are off

--- gpytorch/lazy/kronecker_product_added_diag_lazy_tensor.py
+++ gpytorch/lazy/kronecker_product_added_diag_lazy_tensor.py
@@ -8,12 +8,14 @@ from .diag_lazy_tensor import DiagLazyTensor
 from .kronecker_product_lazy_tensor import KroneckerProductLazyTensor
 from .non_lazy_tensor import lazify
 
-# TODO: check the speed of the linear algebra - svd vs eig etc.
+# TODO: probably want to implement sqrt_inv_matmul as well given the eigendecomp makes it free
 
 
 class KroneckerProductAddedDiagLazyTensor(AddedDiagLazyTensor):
-    def __init__(self, *lazy_tensors):
-        super(KroneckerProductAddedDiagLazyTensor, self).__init__(*lazy_tensors)
+    def __init__(self, *lazy_tensors, preconditioner_override=None):
+        super(KroneckerProductAddedDiagLazyTensor, self).__init__(
+            *lazy_tensors, preconditioner_override=preconditioner_override
+        )
         if len(lazy_tensors) > 2:
             raise RuntimeError("An AddedDiagLazyTensor can only have two components")
         elif isinstance(lazy_tensors[0], DiagLazyTensor):
@@ -56,10 +58,8 @@ class KroneckerProductAddedDiagLazyTensor(AddedDiagLazyTensor):
         # TODO: check stability of numerics here
 
         q_matrix = KroneckerProductLazyTensor(*[lazify(eig_decomp[1]) for eig_decomp in self._eig_cache])
-        inv_mat = DiagLazyTensor(1.0 / (self._kron_eigenvalues.diag() + self._diag_tensor.diag()))
+        inv_mat_sqrt = DiagLazyTensor(1.0 / (self._kron_eigenvalues.diag() + self._diag_tensor.diag()) ** 0.5)
 
-        res = q_matrix.t().matmul(tensor)
-        res = inv_mat.matmul(res)
-        res = q_matrix.matmul(res)
-
-        return tensor.t().matmul(res).squeeze()
+        res = q_matrix.transpose(-2, -1).matmul(tensor)
+        res2 = inv_mat_sqrt.matmul(res)
+        return res2.transpose(-2, -1).matmul(res2).sum((-1, -2))
--- test/lazy/test_kronecker_product_added_diag_lazy_tensor.py
+++ test/lazy/test_kronecker_product_added_diag_lazy_tensor.py
@@ -19,7 +19,7 @@ from gpytorch.test.lazy_tensor_test_case import LazyTensorTestCase
 class TestKroneckerProductAddedDiagLazyTensor(unittest.TestCase, LazyTensorTestCase):
     def create_lazy_tensor(self):
         dims = (3, 5, 10)
-        latent_spaces = [torch.randn(d) for d in dims]
+        latent_spaces = [torch.arange(d).float() for d in dims]
         kernels = [MaternKernel()(ll) for ll in latent_spaces]
         lazy_kernel_matrices = KroneckerProductLazyTensor(*kernels)
 
@@ -34,12 +34,33 @@ class TestKroneckerProductAddedDiagLazyTensor(unittest.TestCase, LazyTensorTestC
 
     def test_log_likelihood_computation(self):
         covariance = self.create_lazy_tensor()
+        cloned_covariance = covariance.clone()
+
         mean_value = torch.zeros(covariance.shape[-1], device=covariance.device, dtype=covariance.dtype)
         dist = MultivariateNormal(mean_value, covariance)
 
         target = torch.randn_like(mean_value)
         value = dist.log_prob(target)
         value.backward()
+
+        lengthscale_grads = torch.tensor(
+            [lt.kernel.raw_lengthscale.grad.data for lt in covariance._lazy_tensor.lazy_tensors]
+        )
+
+        exact_dist = MultivariateNormal(mean_value, cloned_covariance.evaluate())
+        exact_value = exact_dist.log_prob(target)
+        exact_value.backward()
+        exact_lengthscale_grads = torch.tensor(
+            [lt.kernel.raw_lengthscale.grad.data for lt in cloned_covariance._lazy_tensor.lazy_tensors]
+        )
+
+        ll_relative_error = (value.data - exact_value.data).norm() / exact_value.data.norm()
+
+        ll_grad_relative_error = (lengthscale_grads - exact_lengthscale_grads).norm() / exact_lengthscale_grads.norm()
+
+        print(ll_grad_relative_error, lengthscale_grads, exact_lengthscale_grads)
+        self.assertLessEqual(ll_relative_error, 1e-3)
+        self.assertLessEqual(ll_grad_relative_error, 1e-2)
 
 
 if __name__ == "__main__":
