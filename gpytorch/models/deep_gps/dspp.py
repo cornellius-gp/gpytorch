7a17bdd4bf3a18edef6a285a4054ce1d96b15999
Jake Gardner <gardner.jake@gmail.com>
2020-07-29 16:13:22 -0400

    Remove custom likelihoods in favor of custom loss, move quad grid to DSPP

--- gpytorch/models/deep_gps/dspp.py
+++ gpytorch/models/deep_gps/dspp.py
@@ -3,23 +3,23 @@ import torch
 from gpytorch import settings
 from gpytorch.distributions import MultitaskMultivariateNormal
 from gpytorch.lazy import BlockDiagLazyTensor
-from gpytorch.likelihoods import GaussianLikelihood, MultitaskGaussianLikelihood
 
 from ..approximate_gp import ApproximateGP
+from .deep_gp import DeepGP
 from .deep_gp import DeepGPLayer as AbstractDeepGPLayer
 
 
 class AbstractDSPPLayer(AbstractDeepGPLayer):
-    def __init__(self, variational_strategy, input_dims, output_dims, num_sample_sites=3, quad_grid=None):
+    def __init__(self, variational_strategy, input_dims, output_dims, num_sample_sites=3, quad_sites=None):
         super().__init__(variational_strategy, input_dims, output_dims)
 
         self.num_sample_sites = num_sample_sites
 
-        # Pass in previous_layer.quad_grid if you want to share quad_grid across layers.
-        if quad_grid is not None:
-            self.quad_grid = quad_grid
+        # Pass in previous_layer.quad_sites if you want to share quad_sites across layers.
+        if quad_sites is not None:
+            self.quad_sites = quad_sites
         else:
-            self.quad_grid = torch.nn.Parameter(torch.randn(num_sample_sites, input_dims))
+            self.quad_sites = torch.nn.Parameter(torch.randn(num_sample_sites, input_dims))
 
     def __call__(self, inputs, are_samples=False, expand_for_quadgrid=True, **kwargs):
         if isinstance(inputs, MultitaskMultivariateNormal):
@@ -35,7 +35,7 @@ class AbstractDSPPLayer(AbstractDeepGPLayer):
 
             # unsqueeze sigmas to 1 x n x t, locations from [q] to Q^T x 1 x T.
             # Broadcasted result will be Q^T x N x T
-            qg = self.quad_grid.view([self.num_sample_sites] + [1] * (xi_mus.dim() - 2) + [self.input_dims])
+            qg = self.quad_sites.view([self.num_sample_sites] + [1] * (xi_mus.dim() - 2) + [self.input_dims])
             # qg = qg + torch.randn_like(qg) * 1e-2
             xi_sigmas = xi_sigmas * qg
 
@@ -71,49 +71,13 @@ class AbstractDSPPLayer(AbstractDeepGPLayer):
         return output
 
 
-class DeepPredictiveGaussianLikelihood(GaussianLikelihood):
-    def __init__(self, dims, num_sample_sites=3, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class DSPP(DeepGP):
+    def __init__(self, num_sample_sites):
+        super().__init__()
         self.num_sample_sites = num_sample_sites
-        self.register_parameter("raw_quad_weight_grid", torch.nn.Parameter(torch.randn(self.num_sample_sites)))
+        self.register_parameter("raw_quad_weights", torch.nn.Parameter(torch.randn(self.num_sample_sites)))
 
     @property
-    def quad_weight_grid(self):
-        qwd = self.raw_quad_weight_grid
+    def quad_weights(self):
+        qwd = self.raw_quad_weights
         return qwd - qwd.logsumexp(dim=-1)
-
-    def log_marginal(self, observations, function_dist, *params, **kwargs):
-        # Q^T x N
-        base_log_marginal = super().log_marginal(observations, function_dist)
-        deep_log_marginal = self.quad_weight_grid.unsqueeze(-1) + base_log_marginal
-
-        deep_log_prob = deep_log_marginal.logsumexp(dim=-2)
-
-        return deep_log_prob
-
-    def forward(self, *args, **kwargs):
-        pass
-
-
-class MultitaskDeepPredictiveGaussianLikelihood(MultitaskGaussianLikelihood):
-    def __init__(self, dims, num_sample_sites=3, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.num_sample_sites = num_sample_sites
-        self.register_parameter("raw_quad_weight_grid", torch.nn.Parameter(torch.randn(self.num_sample_sites)))
-
-    @property
-    def quad_weight_grid(self):
-        qwd = self.raw_quad_weight_grid
-        return qwd - qwd.logsumexp(dim=-1)
-
-    def log_marginal(self, observations, function_dist, *params, **kwargs):
-        # Q^T x N
-        base_log_marginal = super().log_marginal(observations, function_dist)
-        deep_log_marginal = self.quad_weight_grid.unsqueeze(-1) + base_log_marginal
-
-        deep_log_prob = deep_log_marginal.logsumexp(dim=-2)
-
-        return deep_log_prob
-
-    def forward(self, *args, **kwargs):
-        pass
