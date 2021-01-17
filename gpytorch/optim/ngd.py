a7610e452ce74b899c2c0100c8d8009f570eb851
Joost van Amersfoort <github@joo.st>
2021-01-17 23:33:33 +0000

    Fix incorrect type hint

--- gpytorch/optim/ngd.py
+++ gpytorch/optim/ngd.py
@@ -28,7 +28,7 @@ class NGD(torch.optim.Optimizer):
         super().__init__(params, defaults=dict(lr=lr))
 
     @torch.no_grad()
-    def step(self) -> torch.Tensor:
+    def step(self) -> None:
         """Performs a single optimization step.
         """
         for group in self.param_groups:
