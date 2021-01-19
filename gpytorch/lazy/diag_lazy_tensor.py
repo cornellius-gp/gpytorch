04ce4b7af907cba56175107a20bc38b2ae67dd94
Geoff Pleiss <gpleiss@gmail.com>
2021-01-19 23:22:26 +0000

    Fix unit test

--- gpytorch/lazy/diag_lazy_tensor.py
+++ gpytorch/lazy/diag_lazy_tensor.py
@@ -216,7 +216,10 @@ class ConstantDiagLazyTensor(DiagLazyTensor):
         super(TriangularLazyTensor, self).__init__(diag_values, diag_shape=diag_shape)
         self.diag_values = diag_values
         self.diag_shape = diag_shape
-        self._diag = diag_values.expand(*diag_values.shape[:-1], diag_shape)
+
+    @property
+    def _diag(self):
+        return self.diag_values.expand(*self.diag_values.shape[:-1], self.diag_shape)
 
     def _expand_batch(self, batch_shape):
         return self.__class__(self._diag.expand(*batch_shape, self._diag.size(-1)), diag_shape=self.diag_shape)
@@ -236,6 +239,16 @@ class ConstantDiagLazyTensor(DiagLazyTensor):
 
     def _prod_batch(self, dim):
         return self.__class__(self.diag_values.prod(dim), diag_shape=self.diag_shape)
+
+    def _quad_form_derivative(self, left_vecs, right_vecs):
+        # TODO: Use proper batching for input vectors (prepand to shape rathern than append)
+        if not self._diag.requires_grad:
+            return (None,)
+
+        res = left_vecs * right_vecs
+        if res.ndimension() > self._diag.ndimension():
+            res = res.sum(-1).sum(dim=-1, keepdim=True)
+        return (res,)
 
     def _sum_batch(self, dim):
         return self.__class__(self.diag_values.sum(dim), diag_shape=self.diag_shape)
