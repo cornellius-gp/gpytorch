de754b519358d531dbd6503a76cb21a46347eafc
AndPotap <apotapczynski@gmail.com>
2021-01-20 09:57:43 -0600

    Uses RootLazyTensor to avoid bad shaped in loss

--- gpytorch/kernels/rr_rff.py
+++ gpytorch/kernels/rr_rff.py
@@ -14,8 +14,8 @@ from typing import Optional
 import torch
 from torch import Tensor
 
-from ..lazy import MatmulLazyTensor, LowRankRootLazyTensor
-# from ..lazy import MatmulLazyTensor, RootLazyTensor
+# from ..lazy import MatmulLazyTensor, LowRankRootLazyTensor
+from ..lazy import MatmulLazyTensor, RootLazyTensor
 from ..models.exact_prediction_strategies import RFFPredictionStrategy
 from .kernel import Kernel
 
@@ -128,8 +128,8 @@ class RR_RFF_Kernel(Kernel):
         if diag:
             return (z1 * z2).sum(-1) / D
         if x1_eq_x2:
-            return LowRankRootLazyTensor(z1)
-            # return RootLazyTensor(z1)
+            # return LowRankRootLazyTensor(z1)
+            return RootLazyTensor(z1)
         else:
             print('Warning: x1!=x2 case is not supported for RR.')
             return MatmulLazyTensor(z1, z2.transpose(-1, -2))
