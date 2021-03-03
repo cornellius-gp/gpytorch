09f975c459d3d158cda7e0785c95e252401a8100
Michael Shvartsman <mshvartsman@users.noreply.github.com>
2021-03-03 09:37:28 -0800

    apparently registered priors can be `None`, is this intentional?

--- gpytorch/module.py
+++ gpytorch/module.py
@@ -262,18 +262,19 @@ class Module(nn.Module):
                 )
             closure = param_or_closure
 
-        hyperparameter_shape = closure().shape
-        prior_shape = prior.shape()
+        if prior is not None:
+            hyperparameter_shape = closure(self).shape
+            prior_shape = prior.shape()
 
-        if prior_shape != hyperparameter_shape:
-            try:
-                prior = prior.expand(hyperparameter_shape)
-            except RuntimeError:
-                raise RuntimeError(
-                    "Attempting to broadcast a prior that is not broadcastable! "
-                    + f"Got parameter shape {hyperparameter_shape} "
-                    + f"but prior shape {prior_shape}!"
-                )
+            if prior_shape != hyperparameter_shape:
+                try:
+                    prior = prior.expand(hyperparameter_shape)
+                except RuntimeError:
+                    raise RuntimeError(
+                        "Attempting to broadcast a prior that is not broadcastable! "
+                        + f"Got parameter shape {hyperparameter_shape} "
+                        + f"but prior shape {prior_shape}!"
+                    )
 
         self.add_module(name, prior)
         self._priors[name] = (prior, closure, setting_closure)
