520e9a2f87dca5ab849aa86c4aed011ae5d5745a
Vidhi Lalchand <vr308@cam.ac.uk>
2021-05-02 20:53:20 +0100

    Bayesian GPLVM using SVI

--- gpytorch/models/bayesian_gplvm.py
+++ gpytorch/models/bayesian_gplvm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import ApproximateGP
+from gpytorch.models import ApproximateGP
 
 class BayesianGPLVM(ApproximateGP):
     def __init__(self, X, variational_strategy):
