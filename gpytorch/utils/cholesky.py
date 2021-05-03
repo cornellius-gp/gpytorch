38e1bc7b37835429488d0a9e0817770dcf9419f0
Max Balandat <balandat@fb.com>
2021-05-03 09:35:07 -0700

    Fix tests to properly count cholesky mocks

--- gpytorch/utils/cholesky.py
+++ gpytorch/utils/cholesky.py
@@ -9,7 +9,9 @@ from .errors import NanError, NotPSDError
 from .warnings import NumericalWarning
 
 try:
-    from torch.linalg import cholesky_ex as cholesky_ex
+    from torch.linalg import cholesky_ex  # noqa: F401
+
+    CHOLESKY_METHOD = "torch.linalg.cholesky_ex"  # used for counting mock calls
 
     def _psd_safe_cholesky(A, out=None, jitter=None, max_tries=3):
         # Maybe log
@@ -17,11 +19,17 @@ try:
             settings.verbose_linalg.logger.debug(f"Running Cholesky on a matrix of size {A.shape}.")
 
         if out is not None:
-            out = (out, torch.empty(*A.shape[:-2], dtype=torch.int32))
+            out = (out, torch.empty(A.shape[:-2], dtype=torch.int32))
 
-        L, info = cholesky_ex(A, out=out)
+        L, info = torch.linalg.cholesky_ex(A, out=out)
         if not torch.any(info):
             return L
+
+        isnan = torch.isnan(A)
+        if isnan.any():
+            raise NanError(
+                f"cholesky_cpu: {isnan.sum().item()} of {A.numel()} elements of the {A.shape} tensor are NaN."
+            )
 
         if jitter is None:
             jitter = settings.cholesky_jitter.value(A.dtype)
@@ -34,7 +42,7 @@ try:
             Aprime.diagonal(dim1=-1, dim2=-2).add_(diag_add)
             jitter_prev = jitter_new
             warnings.warn(f"A not p.d., added jitter of {jitter_new:.1e} to the diagonal", NumericalWarning)
-            L, info = cholesky_ex(Aprime, out=out)
+            L, info = torch.linalg.cholesky_ex(Aprime, out=out)
             if not torch.any(info):
                 return L
         raise NotPSDError(f"Matrix not positive definite after repeatedly adding jitter up to {jitter_new:.1e}.")
@@ -44,6 +52,8 @@ except ImportError:
 
     # Fall back to torch.cholesky - this can be more than 3 orders of magnitude slower!
     # TODO: Remove once PyTorch req. is >= 1.9
+
+    CHOLESKY_METHOD = "torch.cholesky"  # used for counting mock calls
 
     def _psd_safe_cholesky(A, out=None, jitter=None, max_tries=3):
         # Maybe log
