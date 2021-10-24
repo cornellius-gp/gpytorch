967644bc9951c56c1b8578cc4dee68dd5aa36e42
Jake Gardner <jacobrg@seas.upenn.edu>
2021-10-23 23:20:28 -0400

    batch shape handling

--- gpytorch/kernels/experimental/derivative_kernel.py
+++ gpytorch/kernels/experimental/derivative_kernel.py
@@ -32,6 +32,7 @@ try:
             self.shuffle = shuffle
 
         def forward(self, x1, x2, diag=False, x1_eq_x2=None, **params):
+            batch_shape = x1.shape[:-2]
             n1, d = x1.shape[-2:]
             n2 = x2.shape[-2]
 
@@ -45,26 +46,36 @@ try:
                 K_x1_x2 = kernf(x1, x2)
 
                 # Compute K_{dx1, x2} block.
-                K_dx1_x2_func = vmap(lambda _x1: jacrev(kernf)(_x1, x2))
+                K_dx1_x2_func = vmap(lambda _x1: jacrev(kernf)(_x1, x2), in_dims=-3)
                 K_dx1_x2 = K_dx1_x2_func(x1.unsqueeze(-2))
-                K_dx1_x2 = K_dx1_x2.squeeze(-2).squeeze(-3).permute(-1, -3, -2).reshape(n1 * d, n2)
+                batch_dims = torch.flipud(-(torch.arange(len(batch_shape)) + 4))
+                K_dx1_x2 = (
+                    K_dx1_x2.squeeze(-2).squeeze(-3).permute(*batch_dims, -1, -3, -2).reshape(*batch_shape, n1 * d, n2)
+                )
 
                 if x1_eq_x2:
                     K_x1_dx2 = K_dx1_x2.transpose(-2, -1)
                 else:
                     # Compute K_{x1, dx2} block the same way (then we'll transpose).
-                    K_dx2_x1_func = vmap(lambda _x2: jacrev(kernf)(_x2, x1))
+                    K_dx2_x1_func = vmap(lambda _x2: jacrev(kernf)(_x2, x1), in_dims=-3)
                     K_dx2_x1 = K_dx2_x1_func(x2.unsqueeze(-2))
-                    K_dx2_x1 = K_dx2_x1.squeeze(-2).squeeze(-3).permute(-1, -3, -2).reshape(n2 * d, n1)
+                    batch_dims = torch.flipud(-(torch.arange(len(batch_shape)) + 4))
+                    K_dx2_x1 = (
+                        K_dx2_x1.squeeze(-2)
+                        .squeeze(-3)
+                        .permute(*batch_dims, -1, -3, -2)
+                        .reshape(*batch_shape, n2 * d, n1)
+                    )
                     K_x1_dx2 = K_dx2_x1.transpose(-2, -1)
 
                 # Compute K_{dx1, dx2} block.
-                K_dx1_dx2_func = vmap(vmap(jacrev(jacrev(kernf, argnums=0), argnums=1)))
-                x1_expand = x1.unsqueeze(-2).unsqueeze(-2).expand(n1, n2, 1, d)
-                x2_expand = x2.unsqueeze(-3).unsqueeze(-2).expand(n1, n2, 1, d)
+                K_dx1_dx2_func = vmap(vmap(jacrev(jacrev(kernf, argnums=0), argnums=1), in_dims=-3), in_dims=-4)
+                x1_expand = x1.unsqueeze(-2).unsqueeze(-2).expand(*batch_shape, n1, n2, 1, d)
+                x2_expand = x2.unsqueeze(-3).unsqueeze(-2).expand(*batch_shape, n1, n2, 1, d)
                 K_dx1_dx2 = K_dx1_dx2_func(x1_expand, x2_expand)
                 K_dx1_dx2 = K_dx1_dx2.squeeze(-2).squeeze(-3).squeeze(-3).squeeze(-3)
-                K_dx1_dx2 = K_dx1_dx2.permute(-2, -4, -1, -3).reshape(n1 * d, n2 * d)
+                batch_dims = torch.flipud(-(torch.arange(len(batch_shape)) + 5))
+                K_dx1_dx2 = K_dx1_dx2.permute(*batch_dims, -2, -4, -1, -3).reshape(*batch_shape, n1 * d, n2 * d)
 
                 R1 = torch.cat((K_x1_x2, K_x1_dx2), dim=-1)
                 R2 = torch.cat((K_dx1_x2, K_dx1_dx2), dim=-1)
@@ -95,7 +106,8 @@ try:
                 #
                 # Maybe we could vmap a vjp against columns of I or something?
                 k_grad_diag_f = vmap(jacrev(jacrev(k_diag_f, argnums=0), argnums=1))
-                k_grad_diag = k_grad_diag_f(x1, x2).diagonal(dim1=-2, dim2=-1).transpose(-3, -1).reshape(-1)
+                k_grad_diag = k_grad_diag_f(x1, x2)
+                k_grad_diag = k_grad_diag.diagonal(dim1=-2, dim2=-1).transpose(-3, -1).reshape(*batch_shape, -1)
 
                 K_diag = torch.cat((k_diag, k_grad_diag), dim=-1)
 
