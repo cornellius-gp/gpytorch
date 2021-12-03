8d11dd59b41dbba83cfd631f0ab2ceeb73ff7196
valtron <valtron2000@gmail.com>
2021-12-03 14:56:50 -0700

    Remove `_psd_safe_cholesky` that uses `torch.linalg.cholesky` (#1850)
    * remove `_psd_safe_cholesky` that uses `torch.linalg.cholesky`
    
    * black

--- gpytorch/test/variational_test_case.py
+++ gpytorch/test/variational_test_case.py
@@ -6,7 +6,6 @@ from unittest.mock import MagicMock, patch
 import torch
 
 import gpytorch
-from gpytorch.utils.cholesky import CHOLESKY_METHOD
 
 from .base_test_case import BaseTestCase
 
@@ -25,7 +24,10 @@ class VariationalTestCase(BaseTestCase):
             def __init__(self, inducing_points):
                 variational_distribution = distribution_cls(num_inducing, batch_shape=batch_shape)
                 variational_strategy = strategy_cls(
-                    self, inducing_points, variational_distribution, learn_inducing_locations=True
+                    self,
+                    inducing_points,
+                    variational_distribution,
+                    learn_inducing_locations=True,
                 )
                 super().__init__(variational_strategy)
                 if constant_mean:
@@ -45,7 +47,12 @@ class VariationalTestCase(BaseTestCase):
         return _SVGPRegressionModel(inducing_points), self.likelihood_cls()
 
     def _training_iter(
-        self, model, likelihood, batch_shape=torch.Size([]), mll_cls=gpytorch.mlls.VariationalELBO, cuda=False
+        self,
+        model,
+        likelihood,
+        batch_shape=torch.Size([]),
+        mll_cls=gpytorch.mlls.VariationalELBO,
+        cuda=False,
     ):
         train_x = torch.randn(*batch_shape, 32, 2).clamp(-2.5, 2.5)
         train_y = torch.linspace(-1, 1, self.event_shape[0])
@@ -132,12 +139,10 @@ class VariationalTestCase(BaseTestCase):
         eval_data_batch_shape = eval_data_batch_shape if eval_data_batch_shape is not None else self.batch_shape
 
         # Mocks
-        _wrapped_cholesky = MagicMock(
-            wraps=torch.linalg.cholesky if CHOLESKY_METHOD == "torch.linalg.cholesky" else torch.linalg.cholesky_ex
-        )
+        _wrapped_cholesky = MagicMock(wraps=torch.linalg.cholesky_ex)
         _wrapped_cg = MagicMock(wraps=gpytorch.utils.linear_cg)
         _wrapped_ciq = MagicMock(wraps=gpytorch.utils.contour_integral_quad)
-        _cholesky_mock = patch(CHOLESKY_METHOD, new=_wrapped_cholesky)
+        _cholesky_mock = patch("torch.linalg.cholesky_ex", new=_wrapped_cholesky)
         _cg_mock = patch("gpytorch.utils.linear_cg", new=_wrapped_cg)
         _ciq_mock = patch("gpytorch.utils.contour_integral_quad", new=_wrapped_ciq)
 
@@ -194,12 +199,10 @@ class VariationalTestCase(BaseTestCase):
         expected_batch_shape = expected_batch_shape if expected_batch_shape is not None else self.batch_shape
 
         # Mocks
-        _wrapped_cholesky = MagicMock(
-            wraps=torch.linalg.cholesky if CHOLESKY_METHOD == "torch.linalg.cholesky" else torch.linalg.cholesky_ex
-        )
+        _wrapped_cholesky = MagicMock(wraps=torch.linalg.cholesky_ex)
         _wrapped_cg = MagicMock(wraps=gpytorch.utils.linear_cg)
         _wrapped_ciq = MagicMock(wraps=gpytorch.utils.contour_integral_quad)
-        _cholesky_mock = patch(CHOLESKY_METHOD, new=_wrapped_cholesky)
+        _cholesky_mock = patch("torch.linalg.cholesky_ex", new=_wrapped_cholesky)
         _cg_mock = patch("gpytorch.utils.linear_cg", new=_wrapped_cg)
         _ciq_mock = patch("gpytorch.utils.contour_integral_quad", new=_wrapped_ciq)
 
@@ -216,11 +219,21 @@ class VariationalTestCase(BaseTestCase):
         with _cholesky_mock as cholesky_mock, _cg_mock as cg_mock, _ciq_mock as ciq_mock:
             # Iter 1
             self.assertEqual(model.variational_strategy.variational_params_initialized.item(), 0)
-            self._training_iter(model, likelihood, data_batch_shape, mll_cls=self.mll_cls, cuda=self.cuda)
+            self._training_iter(
+                model,
+                likelihood,
+                data_batch_shape,
+                mll_cls=self.mll_cls,
+                cuda=self.cuda,
+            )
             self.assertEqual(model.variational_strategy.variational_params_initialized.item(), 1)
             # Iter 2
             output, loss = self._training_iter(
-                model, likelihood, data_batch_shape, mll_cls=self.mll_cls, cuda=self.cuda
+                model,
+                likelihood,
+                data_batch_shape,
+                mll_cls=self.mll_cls,
+                cuda=self.cuda,
             )
             self.assertEqual(output.batch_shape, expected_batch_shape)
             self.assertEqual(output.event_shape, self.event_shape)
--- gpytorch/utils/cholesky.py
+++ gpytorch/utils/cholesky.py
@@ -8,86 +8,41 @@ from .. import settings
 from .errors import NanError, NotPSDError
 from .warnings import NumericalWarning
 
-try:
-    from torch.linalg import cholesky_ex  # noqa: F401
 
-    CHOLESKY_METHOD = "torch.linalg.cholesky_ex"  # used for counting mock calls
+def _psd_safe_cholesky(A, out=None, jitter=None, max_tries=3):
+    # Maybe log
+    if settings.verbose_linalg.on():
+        settings.verbose_linalg.logger.debug(f"Running Cholesky on a matrix of size {A.shape}.")
 
-    def _psd_safe_cholesky(A, out=None, jitter=None, max_tries=3):
-        # Maybe log
-        if settings.verbose_linalg.on():
-            settings.verbose_linalg.logger.debug(f"Running Cholesky on a matrix of size {A.shape}.")
+    if out is not None:
+        out = (out, torch.empty(A.shape[:-2], dtype=torch.int32, device=out.device))
 
-        if out is not None:
-            out = (out, torch.empty(A.shape[:-2], dtype=torch.int32, device=out.device))
+    L, info = torch.linalg.cholesky_ex(A, out=out)
+    if not torch.any(info):
+        return L
 
-        L, info = torch.linalg.cholesky_ex(A, out=out)
+    isnan = torch.isnan(A)
+    if isnan.any():
+        raise NanError(f"cholesky_cpu: {isnan.sum().item()} of {A.numel()} elements of the {A.shape} tensor are NaN.")
+
+    if jitter is None:
+        jitter = settings.cholesky_jitter.value(A.dtype)
+    Aprime = A.clone()
+    jitter_prev = 0
+    for i in range(max_tries):
+        jitter_new = jitter * (10 ** i)
+        # add jitter only where needed
+        diag_add = ((info > 0) * (jitter_new - jitter_prev)).unsqueeze(-1).expand(*Aprime.shape[:-1])
+        Aprime.diagonal(dim1=-1, dim2=-2).add_(diag_add)
+        jitter_prev = jitter_new
+        warnings.warn(
+            f"A not p.d., added jitter of {jitter_new:.1e} to the diagonal",
+            NumericalWarning,
+        )
+        L, info = torch.linalg.cholesky_ex(Aprime, out=out)
         if not torch.any(info):
             return L
-
-        isnan = torch.isnan(A)
-        if isnan.any():
-            raise NanError(
-                f"cholesky_cpu: {isnan.sum().item()} of {A.numel()} elements of the {A.shape} tensor are NaN."
-            )
-
-        if jitter is None:
-            jitter = settings.cholesky_jitter.value(A.dtype)
-        Aprime = A.clone()
-        jitter_prev = 0
-        for i in range(max_tries):
-            jitter_new = jitter * (10 ** i)
-            # add jitter only where needed
-            diag_add = ((info > 0) * (jitter_new - jitter_prev)).unsqueeze(-1).expand(*Aprime.shape[:-1])
-            Aprime.diagonal(dim1=-1, dim2=-2).add_(diag_add)
-            jitter_prev = jitter_new
-            warnings.warn(f"A not p.d., added jitter of {jitter_new:.1e} to the diagonal", NumericalWarning)
-            L, info = torch.linalg.cholesky_ex(Aprime, out=out)
-            if not torch.any(info):
-                return L
-        raise NotPSDError(f"Matrix not positive definite after repeatedly adding jitter up to {jitter_new:.1e}.")
-
-
-except ImportError:
-
-    # Fall back to torch.linalg.cholesky - this can be more than 3 orders of magnitude slower!
-    # TODO: Remove once PyTorch req. is >= 1.9
-
-    CHOLESKY_METHOD = "torch.linalg.cholesky"  # used for counting mock calls
-
-    def _psd_safe_cholesky(A, out=None, jitter=None, max_tries=3):
-        # Maybe log
-        if settings.verbose_linalg.on():
-            settings.verbose_linalg.logger.debug(f"Running Cholesky on a matrix of size {A.shape}.")
-
-        try:
-            L = torch.linalg.cholesky(A, out=out)
-            return L
-        except RuntimeError as e:
-            isnan = torch.isnan(A)
-            if isnan.any():
-                raise NanError(
-                    f"cholesky_cpu: {isnan.sum().item()} of {A.numel()} elements of the {A.shape} tensor are NaN."
-                )
-
-            if jitter is None:
-                jitter = settings.cholesky_jitter.value(A.dtype)
-            Aprime = A.clone()
-            jitter_prev = 0
-            for i in range(max_tries):
-                jitter_new = jitter * (10 ** i)
-                Aprime.diagonal(dim1=-2, dim2=-1).add_(jitter_new - jitter_prev)
-                jitter_prev = jitter_new
-                try:
-                    L = torch.linalg.cholesky(Aprime, out=out)
-                    warnings.warn(f"A not p.d., added jitter of {jitter_new:.1e} to the diagonal", NumericalWarning)
-                    return L
-                except RuntimeError:
-                    continue
-            raise NotPSDError(
-                f"Matrix not positive definite after repeatedly adding jitter up to {jitter_new:.1e}. "
-                f"Original error on first attempt: {e}"
-            )
+    raise NotPSDError(f"Matrix not positive definite after repeatedly adding jitter up to {jitter_new:.1e}.")
 
 
 def psd_safe_cholesky(A, upper=False, out=None, jitter=None, max_tries=3):
