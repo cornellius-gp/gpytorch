efb14ad58e28ebd33aa2ac694cfd5c62de8ea379
Wesley Maddox <wjm78@case.edu>
2022-03-03 09:16:31 -0500

    Update test_metrics.py

--- test/metrics/test_metrics.py
+++ test/metrics/test_metrics.py
@@ -1,11 +1,14 @@
 import math
 import unittest
-
+from gpytorch.likelihoods import GaussianLikelihood, MultitaskGaussianLikelihood
+from gpytorch.likelihoods.multitask_gaussian_likelihood import MultitaskGaussianLikelihood
+from gpytorch.means import ConstantMean, MultitaskMean
+from gpytorch.kernels import ScaleKernel, RBFKernel, MultitaskKernel
+from gpytorch.distributions import MultivariateNormal, MultitaskMultivariateNormal
 import torch
-
 import gpytorch
 from gpytorch.metrics import (
-    average_coverage_error,
+    # average_coverage_error,
     mean_absolute_error,
     mean_squared_error,
     mean_standardized_log_loss,
@@ -14,117 +17,151 @@ from gpytorch.metrics import (
 )
 from gpytorch.models import ExactGP
 
-N_PTS = 10
-
-
 class ExactGPModel(ExactGP):
     def __init__(self, train_x, train_y, likelihood):
         super().__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.ConstantMean()
-        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
-
+        self.mean_module = ConstantMean()
+        self.covar_module = ScaleKernel(RBFKernel())
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+        return MultivariateNormal(mean_x, covar_x)
 
 
 class ExactMultiTaskGPModel(ExactGP):
     def __init__(self, train_x, train_y, likelihood):
         super().__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.MultitaskMean(gpytorch.means.ConstantMean(), num_tasks=2)
-        self.covar_module = gpytorch.kernels.MultitaskKernel(gpytorch.kernels.RBFKernel(), num_tasks=2, rank=1)
-
+        self.mean_module = MultitaskMean(ConstantMean(), num_tasks=2)
+        self.covar_module = MultitaskKernel(RBFKernel(), num_tasks=2, rank=1)
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultitaskMultivariateNormal(mean_x, covar_x)
-
-
+        return MultitaskMultivariateNormal(mean_x, covar_x)
+    
+    
 class TestMetricsSingleTask(unittest.TestCase):
-    def create_model(self, train_x, train_y, likelihood):
+    def setUp(self, batch_shape=None):
+        train_x, train_y = self.create_data_and_labels(batch_shape=batch_shape)
+        untrained_model = self.create_model(train_x, train_y)
+        self.trained_model = self.train_model(untrained_model, train_x, train_y)
+        self.untrained_model = self.create_model(train_x, train_y)
+        self.testing_pts = self.create_data_and_labels(batch_shape=batch_shape,is_training=False)
+        self.train_x = train_x
+        self.train_y = train_y
+        
+    def create_model(self, train_x, train_y):
+        likelihood = GaussianLikelihood(batch_shape=train_x.shape[:-2])
         model = ExactGPModel(train_x, train_y, likelihood)
+        # assume determinisim
         return model
-
-    def create_test_data(self):
-        return torch.linspace(0, 1, N_PTS)
-
-    def create_likelihood_and_labels(self, train_data):
-        likelihood = gpytorch.likelihoods.GaussianLikelihood()
-        labels = torch.sin(train_data * (2 * math.pi)) + torch.randn(train_data.size()) * 0.2
-        return likelihood, labels
-
+    
+    def create_data_and_labels(self, n=20, batch_shape=None, seed=0, is_training=True):
+        input_range = [0., 1.] if is_training else [1., 1.2]
+        torch.random.manual_seed(seed)
+        if batch_shape is None:
+            batch_shape = torch.Size()
+        inputs = torch.linspace(*input_range, n).view(-1, 1).contiguous()
+        if batch_shape != torch.Size():
+            inputs = inputs.unsqueeze(0)
+            inputs = inputs.repeat(*batch_shape, 1, 1)
+        labels = torch.sin(inputs * (2 * math.pi)) + torch.randn_like(inputs) * 0.1
+        return inputs, labels.squeeze()
+    
     def get_metric(self, model, likelihood, train_data, labels, metric, **kwargs):
         model.eval()
         likelihood.eval()
         pred_dist = likelihood(model(train_data))
         return metric(pred_dist, labels, **kwargs)
-
-    def check_metric(self, metric, **kwargs):
-        train_data = self.create_test_data()
-        likelihood, labels = self.create_likelihood_and_labels(train_data)
-        model = self.create_model(train_data, labels, likelihood)
-        mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+    
+    def train_model(self, model, train_x, train_y):
+        mll = gpytorch.mlls.ExactMarginalLogLikelihood(model.likelihood, model)
         optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
-
-        init_value = self.get_metric(model, likelihood, train_data, labels, metric, **kwargs)
-
         model.train()
-        likelihood.train()
-        for _ in range(5):
+        model.likelihood.train()
+        for _ in range(10):
             optimizer.zero_grad()
-            output = model(train_data)
-            loss = -mll(output, labels)
+            output = model(train_x)
+            loss = -mll(output, train_y).sum()
             loss.backward()
             optimizer.step()
-
-        final_value = self.get_metric(model, likelihood, train_data, labels, metric, **kwargs)
+        
+        return model
+    
+    def check_metric(self, metric, **kwargs):
+        init_value = self.get_metric(self.untrained_model, self.untrained_model.likelihood, *self.testing_pts, metric, **kwargs)
+        final_value = self.get_metric(self.trained_model, self.trained_model.likelihood, *self.testing_pts, metric, **kwargs)
         return init_value, final_value
+    
+    def _test_metric(self, metric, should_check_differentiable=True, **kwargs):
+        init_value, final_value = self.check_metric(metric, **kwargs)
+        self.assertEqual(final_value.shape, self.train_y.shape[:-1])
+        self.assertTrue(torch.all(final_value <= init_value))
+        if should_check_differentiable:
+            self.assertTrue(final_value.requires_grad)  # check that the metric is differentiable
+            
+    def test_mean_absolute_error(self):
+        self._test_metric(mean_absolute_error)
+        
+    def test_mean_squared_error(self):
+        self._test_metric(mean_squared_error)
+        
+    def test_negative_log_predictive_density(self):
+        self._test_metric(negative_log_predictive_density)
+        
+    def test_mean_standardized_log_loss(self):
+        self._test_metric(mean_standardized_log_loss)
+        
+    def test_quantile_coverage_error(self):
+        self._test_metric(quantile_coverage_error, should_check_differentiable=False, quantile=95.)
 
-    def test_mae(self):
-        init_value, final_value = self.check_metric(mean_absolute_error)
-        self.assertTrue(torch.all(final_value < init_value))
-        self.assertTrue(final_value.requires_grad)  # check that the metric is differentiable
-
-    def test_mse(self):
-        init_value, final_value = self.check_metric(mean_squared_error)
-        self.assertTrue(torch.all(final_value < init_value))
-        self.assertTrue(final_value.requires_grad)  # check that the metric is differentiable
-
-    def test_nlpd(self):
-        init_value, final_value = self.check_metric(negative_log_predictive_density)
-        self.assertLess(final_value, init_value)  # works for a scaler, for vector, use torch.all
-        self.assertTrue(final_value.requires_grad)  # check that the metric is differentiable
-
-    def test_msll(self):
-        init_value, final_value = self.check_metric(mean_standardized_log_loss)
-        self.assertTrue(torch.all(final_value < init_value))
-        self.assertTrue(final_value.requires_grad)  # check that the metric is differentiable
-
-    def test_qce(self):
-        init_value, final_value = self.check_metric(quantile_coverage_error, quantile=95)
-        self.assertTrue(torch.all(final_value <= init_value))  # strictly less may be unnecessary here
-
-    def test_ace(self):
-        init_value, final_value = self.check_metric(average_coverage_error)
-        self.assertTrue(torch.all(final_value < init_value))
-
-
+        
+class TestMetricsBatchedSingleTask(TestMetricsSingleTask):
+    def setUp(self):
+        batch_shape = torch.Size((4,))
+        super().setUp(batch_shape=batch_shape)
+        
+        
 class TestMetricsMultiTask(TestMetricsSingleTask):
-    def create_model(self, train_x, train_y, likelihood):
+    def create_data_and_labels(self, n=20, batch_shape=None, seed=0, is_training=True):
+        input_range = [0., 1.] if is_training else [1., 1.2]
+        torch.random.manual_seed(seed)
+        if batch_shape is None:
+            batch_shape = torch.Size()
+        inputs = torch.linspace(*input_range, n).view(-1, 1).contiguous()
+        if batch_shape != torch.Size():
+            inputs = inputs.unsqueeze(0)
+            inputs = inputs.repeat(*batch_shape, 1, 1)
+        labels = torch.cat(
+            [
+                torch.sin(inputs * (2 * math.pi)) + torch.randn_like(inputs) * 0.1,
+                torch.cos(inputs * (2 * math.pi)) + torch.randn_like(inputs) * 0.1,
+            ],
+            -1
+        )
+        return inputs, labels.squeeze().contiguous()
+    
+    def create_model(self, train_x, train_y):
+        likelihood = MultitaskGaussianLikelihood(num_tasks=2)
         model = ExactMultiTaskGPModel(train_x, train_y, likelihood)
         return model
-
-    def create_test_data(self):
-        return torch.linspace(0, 1, N_PTS)
-
-    def create_likelihood_and_labels(self, train_data):
-        likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=2)
-        labels = torch.stack(
-            [
-                torch.sin(train_data * (2 * math.pi)) + torch.randn(train_data.size()) * 0.2,
-                torch.cos(train_data * (2 * math.pi)) + torch.randn(train_data.size()) * 0.2,
-            ],
-            -1,
-        )
-        return likelihood, labels
+    
+    def _test_metric(self, metric, should_check_differentiable=True, **kwargs):
+        init_value, final_value = self.check_metric(metric, **kwargs)
+        print(init_value, final_value, init_value.mean(), final_value.mean())
+        # here the trailing dimension is the number of tasks
+        self.assertEqual(final_value.shape, torch.Size((*self.train_y.shape[:-2], self.train_y.shape[-1],)))
+        self.assertTrue(torch.all(final_value.mean() <= init_value.mean()))
+        if should_check_differentiable:
+            self.assertTrue(final_value.requires_grad)  # check that the metric is differentiable
+            
+    def test_negative_log_predictive_density(self):
+        init_value, final_value = self.check_metric(negative_log_predictive_density)
+        self.assertEqual(final_value.shape, self.train_y.shape[:-2])
+        self.assertTrue(torch.all(final_value <= init_value))
+        self.assertTrue(final_value.requires_grad)  # check that the metric is differentiable
+        
+        
+class TestMetricsBatchedMultiTask(TestMetricsMultiTask):
+    def setUp(self):
+        batch_shape = torch.Size((4,))
+        super().setUp(batch_shape=batch_shape
