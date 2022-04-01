8c9eb1381670395e575c35c0e837a41746252249
David Eriksson <dme65@cornell.edu>
2022-04-01 09:43:57 -0700

    Lint

--- gpytorch/priors/__init__.py
+++ gpytorch/priors/__init__.py
@@ -4,7 +4,14 @@ from .horseshoe_prior import HorseshoePrior
 from .lkj_prior import LKJCholeskyFactorPrior, LKJCovariancePrior, LKJPrior
 from .prior import Prior
 from .smoothed_box_prior import SmoothedBoxPrior
-from .torch_priors import GammaPrior, HalfCauchyPrior, LogNormalPrior, MultivariateNormalPrior, NormalPrior, UniformPrior
+from .torch_priors import (
+    GammaPrior,
+    HalfCauchyPrior,
+    LogNormalPrior,
+    MultivariateNormalPrior,
+    NormalPrior,
+    UniformPrior,
+)
 
 # from .wishart_prior import InverseWishartPrior, WishartPrior
 
