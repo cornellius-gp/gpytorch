6b2471156ad7185c399fb4763b07dd73ed9b575c
AndPotap <AndPotap@github.com>
2022-06-03 11:54:17 -0400

    Reduced test

--- test/utils/test_linear_log_re_cg.py
+++ test/utils/test_linear_log_re_cg.py
@@ -2,8 +2,7 @@ import os
 import random
 import unittest
 import torch
-# from gpytorch.utils.linear_log_re_cg import linear_log_cg_re as linear_cg
-from gpytorch.utils.linear_alt import linear_log_cg_re as linear_cg
+from gpytorch.utils.linear_log_re_cg import linear_log_cg_re as linear_cg
 
 
 class TestLinearCG(unittest.TestCase):
@@ -36,31 +35,6 @@ class TestLinearCG(unittest.TestCase):
         actual = torch.cholesky_solve(rhs, matrix_chol)
         self.assertTrue(torch.allclose(solves, actual, atol=1e-3, rtol=1e-4))
 
-    def test_cg_with_tridiag(self):
-        size = 10
-        matrix = torch.randn(size, size, dtype=torch.float64)
-        matrix = matrix.matmul(matrix.transpose(-1, -2))
-        matrix.div_(matrix.norm())
-        matrix.add_(torch.eye(matrix.size(-1), dtype=torch.float64).mul_(1e-1))
-
-        rhs = torch.randn(size, 50, dtype=torch.float64)
-        solves, t_mats = linear_cg(
-            matrix.matmul, rhs=rhs, n_tridiag=5, max_tridiag_iter=10,
-            max_iter=size, tolerance=0, eps=1e-15,
-            initial_guess=torch.zeros_like(rhs),
-        )
-
-        # Check cg
-        matrix_chol = torch.linalg.cholesky(matrix)
-        actual = torch.cholesky_solve(rhs, matrix_chol)
-        self.assertTrue(torch.allclose(solves, actual, atol=1e-3, rtol=1e-4))
-
-        # Check tridiag
-        eigs = torch.linalg.eigvalsh(matrix)
-        for i in range(5):
-            approx_eigs = torch.linalg.eigvalsh(t_mats[i])
-            self.assertTrue(torch.allclose(eigs, approx_eigs, atol=1e-3, rtol=1e-4))
-
     def test_batch_cg(self):
         batch = 5
         size = 100
@@ -78,32 +52,6 @@ class TestLinearCG(unittest.TestCase):
         matrix_chol = torch.linalg.cholesky(matrix)
         actual = torch.cholesky_solve(rhs, matrix_chol)
         self.assertTrue(torch.allclose(solves, actual, atol=1e-3, rtol=1e-4))
-
-    def test_batch_cg_with_tridiag(self):
-        batch = 5
-        size = 10
-        matrix = torch.randn(batch, size, size, dtype=torch.float64)
-        matrix = matrix.matmul(matrix.transpose(-1, -2))
-        matrix.div_(matrix.norm())
-        matrix.add_(torch.eye(matrix.size(-1), dtype=torch.float64).mul_(1e-1))
-
-        rhs = torch.randn(batch, size, 10, dtype=torch.float64)
-        solves, t_mats = linear_cg(
-            matrix.matmul, rhs=rhs, n_tridiag=8, max_iter=size,
-            max_tridiag_iter=10, tolerance=0, eps=1e-30
-        )
-
-        # Check cg
-        matrix_chol = torch.linalg.cholesky(matrix)
-        actual = torch.cholesky_solve(rhs, matrix_chol)
-        self.assertTrue(torch.allclose(solves, actual, atol=1e-3, rtol=1e-4))
-
-        # Check tridiag
-        for i in range(5):
-            eigs = torch.linalg.eigvalsh(matrix[i])
-            for j in range(8):
-                approx_eigs = torch.linalg.eigvalsh(t_mats[j, i])
-                self.assertTrue(torch.allclose(eigs, approx_eigs, atol=1e-3, rtol=1e-4))
 
 
 if __name__ == "__main__":
