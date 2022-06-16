bfd85b1ed5378aa3b05ac229127c0c4b4802e7ea
David Eriksson <dme65@cornell.edu>
2022-06-16 09:21:34 -0600

    Update test_constant_mean.py

--- test/means/test_constant_mean.py
+++ test/means/test_constant_mean.py
@@ -20,13 +20,13 @@ class TestConstantMean(BaseMeanTestCase, unittest.TestCase):
         if self.__class__.batch_shape is None:
             prior = NormalPrior(0.0, 1.0)
         else:
-            prior = NormalPrior(torch.zeros(self.__class__.batch_shape), torch.ones(self.__class__.batch_shape))
+            prior = NormalPrior(torch.zeros(self.batch_shape), torch.ones(self.batch_shape))
         mean = self.create_mean(prior=prior)
         self.assertEqual(mean.mean_prior, prior)
         pickle.loads(pickle.dumps(mean))  # Should be able to pickle and unpickle with a prior
         value = prior.sample()
         mean._constant_closure(mean, value)
-        self.assertTrue((mean.constant.data == value.reshape(mean.constant.data.shape)).all())
+        self.assertTrue(torch.equal(mean.constant.data, value.reshape(mean.constant.data.shape)))
 
 
 class TestConstantMeanBatch(TestConstantMean, unittest.TestCase):
