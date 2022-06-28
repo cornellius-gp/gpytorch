fa4d1763bbdfffcb4fab364e956e305f9c1253a1
Luhuan Wu <lhwunju@outlook.com>
2022-06-28 15:24:42 -0400

    add sklearn support to nearest neighbor utils

--- gpytorch/utils/nearest_neighbors.py
+++ gpytorch/utils/nearest_neighbors.py
@@ -52,9 +52,11 @@ class NNUtil(Module):
                     ImportWarning
                 )
                 self.nnlib = "sklearn"
+                self.train_neighbors = None
 
         else:
             self.nnlib = "sklearn"
+            self.train_neighbors = None
 
         self.to(device)
 
@@ -93,22 +95,24 @@ class NNUtil(Module):
 
         test_x = self._expand_and_check_shape(test_x)
 
+        test_n = test_x.shape[-2]
+        test_x = test_x.view(-1, test_n, self.dim)
+        nn_idx = torch.empty(self.batch_shape.numel(), test_n, k, dtype=torch.int64)
+
         with torch.no_grad():
             if self.nnlib == 'sklearn':
-                # TODO: check this
-                if self.neighbors is None:
+                if self.train_neighbors is None:
                     raise RuntimeError("The nearest neighbor set has not been defined. First call `set_nn_idx`")
-                nn_idx = torch.from_numpy(self.train_neighbors.kneighbors(test_x.cpu().numpy())[1])
-                nn_idx = nn_idx.long().to(test_x.device)
-            else:
-                test_n = test_x.shape[-2]
-                test_x = test_x.view(-1, test_n, self.dim)
 
-                nn_idx = torch.empty(self.batch_shape.numel(), test_n, k, dtype=torch.int64)
+                for i in range(self.batch_shape.numel()):
+                    nn_idx_i = torch.from_numpy(self.train_neighbors[i].kneighbors(test_x[i].cpu().numpy())[1][...,:k])
+                    nn_idx[i] = nn_idx_i.long().to(test_x.device)
+            else:
 
                 for i in range(self.batch_shape.numel()):
                     nn_idx[i] = self.index[i].search(test_x[i], k)[1]
-                nn_idx = nn_idx.view(*self.batch_shape, test_n, k)
+
+        nn_idx = nn_idx.view(*self.batch_shape, test_n, k)
         return nn_idx
 
     def set_nn_idx(self, train_x):
@@ -123,10 +127,14 @@ class NNUtil(Module):
 
         with torch.no_grad():
             if self.nnlib == 'sklearn':
-                import sklearn.neighbors as NearestNeighbors
-                x = train_x.cpu().numpy()
-                # TODO: fix this
-                self.neighbors = NearestNeighbors(n_neighbors=self.k + 1, algorithm='auto').fit(x)
+                self.train_neighbors = []
+
+                from sklearn.neighbors import NearestNeighbors
+                train_x = train_x.view(-1, self.train_n, self.dim)
+
+                for i in range(self.batch_shape.numel()):
+                    x = train_x[i].cpu().numpy()
+                    self.train_neighbors.append(NearestNeighbors(n_neighbors=self.k, algorithm='auto').fit(x))
             elif self.nnlib == "faiss":
                 train_x = train_x.view(-1, self.train_n, self.dim)
                 for i in range(self.batch_shape.numel()):
@@ -135,8 +143,8 @@ class NNUtil(Module):
 
     def build_sequential_nn_idx(self, x):
         r"""
-        Build the sequential :math:`k` nearest neighbor structure within training data in the folloiwng way:
-        for the :math:`i`-th data point :math:`x_i`, find its :math:`k` nearest neighbors among preciding
+        Build the sequential :math:`k` nearest neighbor structure within training data in the following way:
+        for the :math:`i`-th data point :math:`x_i`, find its :math:`k` nearest neighbors among preceding
         training data :math:`x_1, \cdots, x_{i-1}`, for `i=k+1:N` where `N` is the size of training data.
         :param x: training data. Shape `(N, D)`
         :return: indices of nearest neighbors. Shape: `(N-k, k)`
@@ -144,6 +152,9 @@ class NNUtil(Module):
         x = self._expand_and_check_shape(x)
         N = x.shape[-2]
         assert self.k < N, f"k should be smaller than number of data, but got k = {self.k}, number of data = {N}."
+
+        nn_idx = torch.empty(self.batch_shape.numel(), N - self.k, self.k, dtype=torch.int64)
+        x_np = x.view(-1, N, self.dim).data.float().cpu().numpy()
 
         if self.nnlib == 'faiss':
             from faiss import IndexFlatL2
@@ -155,9 +166,6 @@ class NNUtil(Module):
                     from faiss import index_cpu_to_gpu
                     index = index_cpu_to_gpu(self.res, 0, index)
 
-                N = x.shape[-2]
-                nn_idx = torch.empty(self.batch_shape.numel(), N - self.k, self.k, dtype=torch.int64)
-                x_np = x.view(-1, N, self.dim).data.float().cpu().numpy()
 
                 for bi in range(self.batch_shape.numel()):
                     index.reset()
@@ -169,10 +177,19 @@ class NNUtil(Module):
                         )
                         index.add(row)
 
-                nn_idx = nn_idx.view(*self.batch_shape, N-self.k, self.k)
         else:
             assert self.nnlib == 'sklearn'
-            pass
+            from sklearn.neighbors import NearestNeighbors
+
+            for bi in range(self.batch_shape.numel()):
+                # finding k nearest neighbors in the first k
+                for i in range(self.k, N):
+
+                    train_neighbors = NearestNeighbors(n_neighbors=self.k, algorithm='auto').fit(x_np[bi][:i])
+                    nn_idx_i = torch.from_numpy(train_neighbors.kneighbors(x_np[bi][i][None,])[1]).squeeze()
+
+                    nn_idx[bi][i-self.k].copy_(nn_idx_i)
+        nn_idx = nn_idx.view(*self.batch_shape, N-self.k, self.k)
         return nn_idx
 
     def to(self, device=None):
@@ -188,7 +205,7 @@ class NNUtil(Module):
 
     def _expand_and_check_shape(self, x):
         if len(x.shape) == 1:
-            x = x.unsqueeze(-1)  # add dimension
+            x = x.unsqueeze(-1)
         assert x.shape[:-2] == self.batch_shape, f"x's batch shape must be equal to self.batch_shape, " \
                                                  f"but got x's batch shape={x.shape[:-2]}, self.batch_shape={self.batch_shape}."
         assert x.shape[-1] == self.dim, f"x's dim must be equal to self.dim, " \
