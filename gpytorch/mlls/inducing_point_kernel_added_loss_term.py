3714495b9a98af06457e9e10d109a9c5d55ed500
Geoff Pleiss <gpleiss@gmail.com>
2022-09-06 14:32:12 +0000

    Fix multitask/added_loss_term bugs in SGPR regression
    - Enable InducingPoint kernel to work with multitask regression
    - Enable broadcasting for SGPR added loss term
    
    [Fixes #2113]

--- gpytorch/mlls/inducing_point_kernel_added_loss_term.py
+++ gpytorch/mlls/inducing_point_kernel_added_loss_term.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import torch
+
+from ..likelihoods.multitask_gaussian_likelihood import MultitaskGaussianLikelihood
 from .added_loss_term import AddedLossTerm
 
 
@@ -14,5 +17,12 @@ class InducingPointKernelAddedLossTerm(AddedLossTerm):
         variational_covar = self.variational_dist.lazy_covariance_matrix
         diag = prior_covar.diagonal(dim1=-1, dim2=-2) - variational_covar.diagonal(dim1=-1, dim2=-2)
         shape = prior_covar.shape[:-1]
+        if isinstance(self.likelihood, MultitaskGaussianLikelihood):
+            shape = torch.Size([*shape, 1])
+            diag = diag.unsqueeze(-1)
         noise_diag = self.likelihood._shaped_noise_covar(shape, *params).diagonal(dim1=-1, dim2=-2)
-        return -0.5 * (diag / noise_diag).sum()
+        if isinstance(self.likelihood, MultitaskGaussianLikelihood):
+            noise_diag = noise_diag.reshape(*shape[:-1], -1)
+            return -0.5 * (diag / noise_diag).sum(dim=[-1, -2])
+        else:
+            return -0.5 * (diag / noise_diag).sum(dim=-1)
---
+++ test/examples/test_kronecker_multitask_sgpr_regression.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+from math import pi
+
+import os
+import random
+import torch
+import unittest
+
+import gpytorch
+from gpytorch.means import ConstantMean, MultitaskMean
+from gpytorch.likelihoods import MultitaskGaussianLikelihood
+from gpytorch.distributions import MultitaskMultivariateNormal
+
+
+# Simple training data: let's try to learn a sine function
+train_x = torch.linspace(0, 1, 100)
+
+# y1 function is sin(2*pi*x) with noise N(0, 0.04)
+train_y1 = torch.sin(train_x * (2 * pi)) + torch.randn(train_x.size()) * 0.1
+# y2 function is cos(2*pi*x) with noise N(0, 0.04)
+train_y2 = torch.cos(train_x * (2 * pi)) + torch.randn(train_x.size()) * 0.1
+
+# Create a train_y which interleaves the two
+train_y = torch.stack([train_y1, train_y2], -1)
+
+
+class MultitaskGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood):
+        super(MultitaskGPModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = MultitaskMean(ConstantMean(), num_tasks=2)
+        self.covar_module = gpytorch.kernels.MultitaskKernel(
+            gpytorch.kernels.InducingPointKernel(
+                gpytorch.kernels.RBFKernel(), inducing_points=torch.randn(50, 1), likelihood=likelihood
+            ), num_tasks=2, rank=2
+        )
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return MultitaskMultivariateNormal(mean_x, covar_x)
+
+
+class TestKroneckerMultiTaskSGPRRegression(unittest.TestCase):
+    def setUp(self):
+        if os.getenv("UNLOCK_SEED") is None or os.getenv("UNLOCK_SEED").lower() == "false":
+            self.rng_state = torch.get_rng_state()
+            torch.manual_seed(0)
+            if torch.cuda.is_available():
+                torch.cuda.manual_seed_all(0)
+            random.seed(0)
+
+    def tearDown(self):
+        if hasattr(self, "rng_state"):
+            torch.set_rng_state(self.rng_state)
+
+    def test_multitask_gp_mean_abs_error(self):
+        likelihood = MultitaskGaussianLikelihood(num_tasks=2)
+        model = MultitaskGPModel(train_x, train_y, likelihood)
+        # Find optimal model hyperparameters
+        model.train()
+        likelihood.train()
+
+        # Use the adam optimizer
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.1)  # Includes GaussianLikelihood parameters
+
+        # "Loss" for GPs - the marginal log likelihood
+        mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+
+        n_iter = 50
+        for _ in range(n_iter):
+            # Zero prev backpropped gradients
+            optimizer.zero_grad()
+            # Make predictions from training data
+            # Again, note feeding duplicated x_data and indices indicating which task
+            output = model(train_x)
+            # TODO: Fix this view call!!
+            loss = -mll(output, train_y)
+            loss.backward()
+            optimizer.step()
+
+        # Test the model
+        model.eval()
+        likelihood.eval()
+        test_x = torch.linspace(0, 1, 51)
+        test_y1 = torch.sin(test_x * (2 * pi))
+        test_y2 = torch.cos(test_x * (2 * pi))
+        test_preds = likelihood(model(test_x)).mean
+        mean_abs_error_task_1 = torch.mean(torch.abs(test_y1 - test_preds[:, 0]))
+        mean_abs_error_task_2 = torch.mean(torch.abs(test_y2 - test_preds[:, 1]))
+
+        self.assertLess(mean_abs_error_task_1.squeeze().item(), 0.05)
+        self.assertLess(mean_abs_error_task_2.squeeze().item(), 0.05)
+
+
+if __name__ == "__main__":
+    unittest.main()
