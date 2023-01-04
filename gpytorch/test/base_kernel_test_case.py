1e5e387ecf350ef25bedbca7f42b977aa712beec
Max Balandat <balandat@meta.com>
2023-01-04 13:43:59 -0800

    Error on multiple dtypes/devices (if kernel has no lengthscale)

--- gpytorch/test/base_kernel_test_case.py
+++ gpytorch/test/base_kernel_test_case.py
@@ -182,11 +182,16 @@ class BaseKernelTestCase(BaseTestCase):
 
     def test_kernel_dtype_device(self):
         kernel = self.create_kernel_no_ard(batch_shape=torch.Size([]))
-        self.assertEqual(kernel.dtype, torch.get_default_dtype())
+        orig_type = kernel.dtype
+        self.assertEqual(orig_type, torch.get_default_dtype())
         self.assertEqual(kernel.device, torch.device("cpu"))
-        new_type = next(iter({torch.float32, torch.float64} - {torch.get_default_dtype()}))
+        new_type = ({torch.float32, torch.float64} - {torch.get_default_dtype()}).pop()
         kernel.to(dtype=new_type)
         self.assertEqual(kernel.dtype, new_type)
         if torch.cuda.is_available():
             kernel.to(device="cuda")
             self.assertNotEqual(kernel.device, torch.device("cpu"))
+        if not kernel.has_lengthscale:
+            kernel.register_parameter("foo", torch.nn.Parameter(torch.zeros(1, dtype=orig_type)))
+            with self.assertRaisesRegex(RuntimeError, "parameters have multiple dtypes"):
+                kernel.dtype
