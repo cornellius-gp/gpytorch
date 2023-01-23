1f7ab49d067815eb979c0e30ec14b33949eb078f
Ankush Aggarwal <ankush_aggarwal2@yahoo.com>
2023-01-23 09:37:34 +0000

    Fix formatting with pre-commit

--- gpytorch/means/__init__.py
+++ gpytorch/means/__init__.py
@@ -10,4 +10,14 @@ from .mean import Mean
 from .multitask_mean import MultitaskMean
 from .zero_mean import ZeroMean
 
-__all__ = ["Mean", "ConstantMean", "ConstantMeanGrad", "ConstantMeanGradGrad", "LinearMean", "LinearMeanGrad", "LinearMeanGradGrad", "MultitaskMean", "ZeroMean"]
+__all__ = [
+    "Mean",
+    "ConstantMean",
+    "ConstantMeanGrad",
+    "ConstantMeanGradGrad",
+    "LinearMean",
+    "LinearMeanGrad",
+    "LinearMeanGradGrad",
+    "MultitaskMean",
+    "ZeroMean",
+]
