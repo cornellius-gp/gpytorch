a92b5963148f88b7de47dc02209d65fad8c194aa
m-julian <52214154+m-julian@users.noreply.github.com>
2023-03-09 22:06:18 +0000

    add more tests

--- test/kernels/keops/test_periodic_kernel.py
+++ test/kernels/keops/test_periodic_kernel.py
@@ -4,8 +4,11 @@ import unittest
 
 import torch
 
+import gpytorch
+
 from gpytorch.kernels import PeriodicKernel as GPeriodicKernel
 from gpytorch.kernels.keops import PeriodicKernel
+from gpytorch.priors import NormalPrior
 from gpytorch.test.base_kernel_test_case import BaseKernelTestCase
 
 try:
@@ -19,42 +22,106 @@ try:
             return PeriodicKernel(ard_num_dims=num_dims, **kwargs)
 
     class TestPeriodicKeOpsKernel(unittest.TestCase):
+
+        # tests the keops implementation
         def test_forward_x1_eq_x2(self):
 
-            x1 = torch.randn(100, 3)
+            with gpytorch.settings.max_cholesky_size(2):
 
-            kern1 = PeriodicKernel()
-            kern2 = GPeriodicKernel()
+                x1 = torch.randn(100, 3)
 
-            k1 = kern1(x1, x1).to_dense()
-            k2 = kern2(x1, x1).to_dense()
+                kern1 = PeriodicKernel()
+                kern2 = GPeriodicKernel()
 
-            self.assertLess(torch.norm(k1 - k2), 1e-4)
+                k1 = kern1(x1, x1).to_dense()
+                k2 = kern2(x1, x1).to_dense()
+
+                self.assertLess(torch.norm(k1 - k2), 1e-4)
 
         def test_forward_x1_neq_x2(self):
 
-            x1 = torch.randn(100, 3)
-            x2 = torch.randn(100, 3)
+            with gpytorch.settings.max_cholesky_size(2):
 
-            kern1 = PeriodicKernel()
-            kern2 = GPeriodicKernel()
+                x1 = torch.randn(100, 3)
+                x2 = torch.randn(100, 3)
 
-            k1 = kern1(x1, x2).to_dense()
-            k2 = kern2(x1, x2).to_dense()
+                kern1 = PeriodicKernel()
+                kern2 = GPeriodicKernel()
 
-            self.assertLess(torch.norm(k1 - k2), 1e-4)
+                k1 = kern1(x1, x2).to_dense()
+                k2 = kern2(x1, x2).to_dense()
+
+                self.assertLess(torch.norm(k1 - k2), 1e-4)
 
         def test_batch_matmul(self):
 
-            x1 = torch.randn(3, 2, 100, 3)
-            kern1 = PeriodicKernel()
-            kern2 = GPeriodicKernel()
+            with gpytorch.settings.max_cholesky_size(2):
 
-            rhs = torch.randn(3, 2, 100, 1)
-            res1 = kern1(x1, x1).matmul(rhs)
-            res2 = kern2(x1, x1).matmul(rhs)
+                x1 = torch.randn(3, 2, 100, 3)
+                kern1 = PeriodicKernel()
+                kern2 = GPeriodicKernel()
 
-            self.assertLess(torch.norm(res1 - res2), 1e-4)
+                rhs = torch.randn(3, 2, 100, 1)
+                res1 = kern1(x1, x1).matmul(rhs)
+                res2 = kern2(x1, x1).matmul(rhs)
+
+                self.assertLess(torch.norm(res1 - res2), 1e-4)
+
+        # tests the nonkeops implementation (_nonkeops_covar_func)
+        def test_forward_x1_eq_x2_nonkeops(self):
+
+            with gpytorch.settings.max_cholesky_size(800):
+
+                x1 = torch.randn(100, 3)
+
+                kern1 = PeriodicKernel()
+                kern2 = GPeriodicKernel()
+
+                k1 = kern1(x1, x1).to_dense()
+                k2 = kern2(x1, x1).to_dense()
+
+            self.assertLess(torch.norm(k1 - k2), 1e-4)
+
+        def test_forward_x1_neq_x2_nonkeops(self):
+
+            with gpytorch.settings.max_cholesky_size(800):
+
+                x1 = torch.randn(100, 3)
+                x2 = torch.randn(100, 3)
+
+                kern1 = PeriodicKernel()
+                kern2 = GPeriodicKernel()
+
+                k1 = kern1(x1, x2).to_dense()
+                k2 = kern2(x1, x2).to_dense()
+
+                self.assertLess(torch.norm(k1 - k2), 1e-4)
+
+        def test_batch_matmul_nonkeops(self):
+
+            with gpytorch.settings.max_cholesky_size(800):
+
+                x1 = torch.randn(3, 2, 100, 3)
+                kern1 = PeriodicKernel()
+                kern2 = GPeriodicKernel()
+
+                rhs = torch.randn(3, 2, 100, 1)
+                res1 = kern1(x1, x1).matmul(rhs)
+                res2 = kern2(x1, x1).matmul(rhs)
+
+                self.assertLess(torch.norm(res1 - res2), 1e-4)
+
+    def create_kernel_with_prior(self, period_length_prior):
+        return PeriodicKernel(period_length_prior=period_length_prior)
+
+    def test_prior_type(self):
+        """
+        Raising TypeError if prior type is other than gpytorch.priors.Prior
+        """
+        kernel_fn = lambda prior: self.create_kernel_with_prior(prior)
+        kernel_fn(None)
+        kernel_fn(NormalPrior(0, 1))
+        self.assertRaises(TypeError, kernel_fn, 1)
 
 except ImportError:
     pass
