8fddebd86d2512feb81a1b36ac076f97c83d7448
m-julian <52214154+m-julian@users.noreply.github.com>
2023-03-24 15:43:51 +0000

    run keops tests on cpu

--- gpytorch/test/base_keops_test_case.py
+++ gpytorch/test/base_keops_test_case.py
@@ -16,15 +16,13 @@ class BaseKeOpsTestCase(ABC):
 
         # tests the keops implementation
     def test_forward_x1_eq_x2(self, **kwargs):
-        if not torch.cuda.is_available():
-            return
 
         with gpytorch.settings.max_cholesky_size(2):
 
-            x1 = torch.randn(100, 3).cuda()
+            x1 = torch.randn(100, 3)
 
-            kern1 = self.k1(**kwargs).cuda()
-            kern2 = self.k2(**kwargs).cuda()
+            kern1 = self.k1(**kwargs)
+            kern2 = self.k2(**kwargs)
 
             k1 = kern1(x1, x1).to_dense()
             k2 = kern2(x1, x1).to_dense()
@@ -32,16 +30,14 @@ class BaseKeOpsTestCase(ABC):
             self.assertLess(torch.norm(k1 - k2), 1e-4)
 
     def test_forward_x1_neq_x2(self, **kwargs):
-        if not torch.cuda.is_available():
-            return
 
         with gpytorch.settings.max_cholesky_size(2):
 
-            x1 = torch.randn(100, 3).cuda()
-            x2 = torch.randn(50, 3).cuda()
+            x1 = torch.randn(100, 3)
+            x2 = torch.randn(50, 3)
 
-            kern1 = self.k1(**kwargs).cuda()
-            kern2 = self.k2(**kwargs).cuda()
+            kern1 = self.k1(**kwargs)
+            kern2 = self.k2(**kwargs)
 
             k1 = kern1(x1, x2).to_dense()
             k2 = kern2(x1, x2).to_dense()
@@ -49,16 +45,14 @@ class BaseKeOpsTestCase(ABC):
             self.assertLess(torch.norm(k1 - k2), 1e-4)
 
     def test_batch_matmul(self, **kwargs):
-        if not torch.cuda.is_available():
-            return
 
         with gpytorch.settings.max_cholesky_size(2):
 
-            x1 = torch.randn(3, 2, 100, 3).cuda()
-            kern1 = self.k1(**kwargs).cuda()
-            kern2 = self.k2(**kwargs).cuda()
+            x1 = torch.randn(3, 2, 100, 3)
+            kern1 = self.k1(**kwargs)
+            kern2 = self.k2(**kwargs)
 
-            rhs = torch.randn(3, 2, 100, 1).cuda()
+            rhs = torch.randn(3, 2, 100, 1)
             res1 = kern1(x1, x1).matmul(rhs)
             res2 = kern2(x1, x1).matmul(rhs)
 
@@ -66,15 +60,13 @@ class BaseKeOpsTestCase(ABC):
 
     # tests the nonkeops implementation (_nonkeops_covar_func)
     def test_forward_x1_eq_x2_nonkeops(self, **kwargs):
-        if not torch.cuda.is_available():
-            return
 
         with gpytorch.settings.max_cholesky_size(800):
 
-            x1 = torch.randn(100, 3).cuda()
+            x1 = torch.randn(100, 3)
 
-            kern1 = self.k1(**kwargs).cuda()
-            kern2 = self.k2(**kwargs).cuda()
+            kern1 = self.k1(**kwargs)
+            kern2 = self.k2(**kwargs)
 
             k1 = kern1(x1, x1).to_dense()
             k2 = kern2(x1, x1).to_dense()
@@ -82,16 +74,14 @@ class BaseKeOpsTestCase(ABC):
             self.assertLess(torch.norm(k1 - k2), 1e-4)
 
     def test_forward_x1_neq_x2_nonkeops(self, **kwargs):
-        if not torch.cuda.is_available():
-            return
 
         with gpytorch.settings.max_cholesky_size(800):
 
-            x1 = torch.randn(100, 3).cuda()
-            x2 = torch.randn(50, 3).cuda()
+            x1 = torch.randn(100, 3)
+            x2 = torch.randn(50, 3)
 
-            kern1 = self.k1(**kwargs).cuda()
-            kern2 = self.k2(**kwargs).cuda()
+            kern1 = self.k1(**kwargs)
+            kern2 = self.k2(**kwargs)
 
             k1 = kern1(x1, x2).to_dense()
             k2 = kern2(x1, x2).to_dense()
@@ -99,16 +89,14 @@ class BaseKeOpsTestCase(ABC):
             self.assertLess(torch.norm(k1 - k2), 1e-4)
 
     def test_batch_matmul_nonkeops(self, **kwargs):
-        if not torch.cuda.is_available():
-            return
 
         with gpytorch.settings.max_cholesky_size(800):
 
-            x1 = torch.randn(3, 2, 100, 3).cuda()
-            kern1 = self.k1(**kwargs).cuda()
-            kern2 = self.k2(**kwargs).cuda()
+            x1 = torch.randn(3, 2, 100, 3)
+            kern1 = self.k1(**kwargs)
+            kern2 = self.k2(**kwargs)
 
-            rhs = torch.randn(3, 2, 100, 1).cuda()
+            rhs = torch.randn(3, 2, 100, 1)
             res1 = kern1(x1, x1).matmul(rhs)
             res2 = kern2(x1, x1).matmul(rhs)
 
