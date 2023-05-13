e34791b2de1d7afd726ae9ca2f32d68f76e8e154
m-julian <52214154+m-julian@users.noreply.github.com>
2023-05-13 18:51:24 +0100

    gradient and ard tests

--- gpytorch/test/base_keops_test_case.py
+++ gpytorch/test/base_keops_test_case.py
@@ -1,11 +1,15 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 
 import torch
 
 import gpytorch
 
+from .base_test_case import BaseTestCase
 
-class BaseKeOpsTestCase(ABC):
+CHOLESKY_SIZE_KEOPS, CHOLESKY_SIZE_NONKEOPS = 2, 800
+
+
+class BaseKeOpsTestCase(BaseTestCase):
     @abstractmethod
     def k1(self):
         """Returns first kernel class"""
@@ -16,40 +20,58 @@ class BaseKeOpsTestCase(ABC):
         """Returns second kernel class"""
         pass
 
-        # tests the keops implementation
+    # tests the keops implementation
 
-    def test_forward_x1_eq_x2(self, **kwargs):
+    def test_forward_x1_eq_x2(self, max_cholesky_size=2, ard=False, **kwargs):
 
-        with gpytorch.settings.max_cholesky_size(2):
+        with gpytorch.settings.max_cholesky_size(max_cholesky_size):
+
+            ndims = 3
 
             x1 = torch.randn(100, 3)
 
-            kern1 = self.k1(**kwargs)
-            kern2 = self.k2(**kwargs)
+            if ard:
+                kern1 = self.k1(ard_num_dims=ndims, **kwargs)
+                kern2 = self.k2(ard_num_dims=ndims, **kwargs)
+            else:
+                kern1 = self.k1(**kwargs)
+                kern2 = self.k2(**kwargs)
 
             k1 = kern1(x1, x1).to_dense()
             k2 = kern2(x1, x1).to_dense()
 
             self.assertLess(torch.norm(k1 - k2), 1e-4)
 
-    def test_forward_x1_neq_x2(self, **kwargs):
+    def test_forward_x1_eq_x2_ard(self):
+        return self.test_forward_x1_eq_x2(ard=True)
 
-        with gpytorch.settings.max_cholesky_size(2):
+    def test_forward_x1_neq_x2(self, max_cholesky_size=2, ard=False, **kwargs):
 
-            x1 = torch.randn(100, 3)
-            x2 = torch.randn(50, 3)
+        with gpytorch.settings.max_cholesky_size(max_cholesky_size):
 
-            kern1 = self.k1(**kwargs)
-            kern2 = self.k2(**kwargs)
+            ndims = 3
+
+            x1 = torch.randn(100, ndims)
+            x2 = torch.randn(50, ndims)
+
+            if ard:
+                kern1 = self.k1(ard_num_dims=ndims, **kwargs)
+                kern2 = self.k2(ard_num_dims=ndims, **kwargs)
+            else:
+                kern1 = self.k1(**kwargs)
+                kern2 = self.k2(**kwargs)
 
             k1 = kern1(x1, x2).to_dense()
             k2 = kern2(x1, x2).to_dense()
 
             self.assertLess(torch.norm(k1 - k2), 1e-4)
 
-    def test_batch_matmul(self, **kwargs):
+    def test_forward_x1_meq_x2_ard(self):
+        return self.test_forward_x1_neq_x2(ard=True)
 
-        with gpytorch.settings.max_cholesky_size(2):
+    def test_batch_matmul(self, max_cholesky_size=2, **kwargs):
+
+        with gpytorch.settings.max_cholesky_size(max_cholesky_size):
 
             x1 = torch.randn(3, 2, 100, 3)
             kern1 = self.k1(**kwargs)
@@ -60,47 +82,60 @@ class BaseKeOpsTestCase(ABC):
             res2 = kern2(x1, x1).matmul(rhs)
 
             self.assertLess(torch.norm(res1 - res2), 1e-4)
+
+    def test_gradient(self, max_cholesky_size=2, ard=False, **kwargs):
+
+        with gpytorch.settings.max_cholesky_size(max_cholesky_size):
+
+            ndims = 3
+
+            x1 = torch.randn(4, 100, ndims)
+
+            if ard:
+                kern1 = self.k1(ard_num_dims=ndims, **kwargs)
+                kern2 = self.k2(ard_num_dims=ndims, **kwargs)
+            else:
+                kern1 = self.k1(**kwargs)
+                kern2 = self.k2(**kwargs)
+
+            res1 = kern1(x1, x1)
+            res2 = kern2(x1, x1)
+
+            s1 = res1.sum()
+            s2 = res2.sum()
+
+            # stack all gradients into a tensor
+            grad_s1 = torch.vstack(torch.autograd.grad(s1, [*kern1.hyperparameters()]))
+            grad_s2 = torch.vstack(torch.autograd.grad(s2, [*kern2.hyperparameters()]))
+
+            self.assertAllClose(grad_s1, grad_s2, rtol=1e-4, atol=1e-5)
 
     # tests the nonkeops implementation (_nonkeops_covar_func)
-    def test_forward_x1_eq_x2_nonkeops(self, **kwargs):
 
-        with gpytorch.settings.max_cholesky_size(800):
+    def test_forward_x1_eq_x2_nonkeops(self):
 
-            x1 = torch.randn(100, 3)
+        self.test_forward_x1_eq_x2(max_cholesky_size=CHOLESKY_SIZE_NONKEOPS)
 
-            kern1 = self.k1(**kwargs)
-            kern2 = self.k2(**kwargs)
+    def test_forward_x1_eq_x2_nonkeops_ard(self):
 
-            k1 = kern1(x1, x1).to_dense()
-            k2 = kern2(x1, x1).to_dense()
+        self.test_forward_x1_eq_x2(max_cholesky_size=CHOLESKY_SIZE_NONKEOPS, ard=True)
 
-            self.assertLess(torch.norm(k1 - k2), 1e-4)
+    def test_forward_x1_neq_x2_nonkeops(self):
 
-    def test_forward_x1_neq_x2_nonkeops(self, **kwargs):
+        self.test_forward_x1_neq_x2(max_cholesky_size=CHOLESKY_SIZE_NONKEOPS)
 
-        with gpytorch.settings.max_cholesky_size(800):
+    def test_forward_x1_neq_x2_nonkeops_ard(self):
 
-            x1 = torch.randn(100, 3)
-            x2 = torch.randn(50, 3)
+        self.test_forward_x1_neq_x2(max_cholesky_size=CHOLESKY_SIZE_NONKEOPS, ard=True)
 
-            kern1 = self.k1(**kwargs)
-            kern2 = self.k2(**kwargs)
+    def test_batch_matmul_nonkeops(self):
 
-            k1 = kern1(x1, x2).to_dense()
-            k2 = kern2(x1, x2).to_dense()
+        self.test_batch_matmul(max_cholesky_size=CHOLESKY_SIZE_NONKEOPS)
 
-            self.assertLess(torch.norm(k1 - k2), 1e-4)
+    def test_gradient_nonkeops(self):
 
-    def test_batch_matmul_nonkeops(self, **kwargs):
+        self.test_gradient(max_cholesky_size=CHOLESKY_SIZE_NONKEOPS)
 
-        with gpytorch.settings.max_cholesky_size(800):
+    def test_gradient_nonkeops_ard(self):
 
-            x1 = torch.randn(3, 2, 100, 3)
-            kern1 = self.k1(**kwargs)
-            kern2 = self.k2(**kwargs)
-
-            rhs = torch.randn(3, 2, 100, 1)
-            res1 = kern1(x1, x1).matmul(rhs)
-            res2 = kern2(x1, x1).matmul(rhs)
-
-            self.assertLess(torch.norm(res1 - res2), 1e-4)
+        self.test_gradient(max_cholesky_size=CHOLESKY_SIZE_NONKEOPS, ard=True)
