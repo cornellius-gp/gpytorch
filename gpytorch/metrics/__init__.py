905e6bf460871f7346cd48c08607b523830e3dd3
Jonathan Wenger <jw4246@columbia.edu>
2023-12-11 14:15:00 -0500

    metrics: KL divergence and Wasserstein distance

--- gpytorch/metrics/__init__.py
+++ gpytorch/metrics/__init__.py
@@ -1,10 +1,12 @@
 from .metrics import (
+    kl_divergence,
     mean_absolute_error,
     mean_squared_error,
     mean_standardized_log_loss,
     negative_log_predictive_density,
     quantile_coverage_error,
     standardized_mean_squared_error,
+    wasserstein,
 )
 
 __all__ = [
@@ -14,4 +16,6 @@ __all__ = [
     "mean_standardized_log_loss",
     "negative_log_predictive_density",
     "quantile_coverage_error",
+    "kl_divergence",
+    "wasserstein",
 ]
--- gpytorch/metrics/metrics.py
+++ gpytorch/metrics/metrics.py
@@ -107,3 +107,95 @@ def quantile_coverage_error(
     n_samples_within_bounds = ((test_y > lower) * (test_y < upper)).sum(combine_dim)
     fraction = n_samples_within_bounds / test_y.shape[combine_dim]
     return torch.abs(fraction - quantile / 100)
+
+
+def kl_divergence(q: MultivariateNormal, p: MultivariateNormal):
+    """Kullback-Leibler Divergence.
+
+    See for example: https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence#Multivariate_normal_distributions
+
+    :param q: First distributional argument. Can be a stack of multivariate normals.
+    :param p: Second distributional argument. Can be a stack of multivariate normals.
+    """
+
+    # Error checking
+    if not q.event_shape == p.event_shape:
+        raise ValueError("Multivariate normal distributions must have same event shape.")
+
+    # Compute Cholesky decompositions
+    q_cov_chol = torch.linalg.cholesky(q.covariance_matrix, upper=False)
+    p_cov_chol = torch.linalg.cholesky(p.covariance_matrix, upper=False)
+
+    # Forward substitution
+    M = torch.linalg.solve_triangular(p_cov_chol, q_cov_chol, upper=False, left=True)
+    mean_diff = p.mean - q.mean
+    y = torch.squeeze(
+        torch.linalg.solve_triangular(p_cov_chol, torch.unsqueeze(mean_diff, dim=-1), upper=False, left=True),
+        dim=-1,
+    )
+
+    # Individual terms
+    trace_term = torch.sum(M**2, dim=(-2, -1))
+    mahanalobis_distance_term = torch.sum(y**2, dim=-1)
+    logdet_term = torch.sum(
+        torch.log(torch.diagonal(p_cov_chol, dim1=-2, dim2=-1))
+        - torch.log(torch.diagonal(q_cov_chol, dim1=-2, dim2=-1)),
+        dim=-1,
+    )
+
+    return 0.5 * (trace_term - q.event_shape[0] + mahanalobis_distance_term) + logdet_term
+
+
+def _symsqrt(matrix):
+    """Compute the square root of a positive definite matrix."""
+
+    # SVD
+    _, s, v = matrix.svd()
+
+    # Truncate small components
+    above_cutoff = s > s.max(-1, True).values * s.size(-1) * torch.finfo(s.dtype).eps
+    components = above_cutoff.sum(-1)
+    common = components.max()
+    unbalanced = common != components.min()
+    if common < s.size(-1):
+        s = s[..., :common]
+        v = v[..., :common]
+        if unbalanced:
+            above_cutoff = above_cutoff[..., :common]
+    if unbalanced:
+        s = s.where(above_cutoff, torch.zeros((), device=s.device, dtype=s.dtype))
+    return (v * s.sqrt().unsqueeze(-2)) @ v.transpose(-2, -1)
+
+
+def wasserstein(q: MultivariateNormal, p: MultivariateNormal, order=2):
+    """Wasserstein distance or earth mover's distance.
+
+    See for example: https://djalil.chafai.net/blog/2010/04/30/wasserstein-distance-between-two-gaussians/
+
+    :param q: First distributional argument. Can be a stack of multivariate normals.
+    :param p: Second distributional argument. Can be a stack of multivariate normals.
+    :param order: Order of the Wasserstein distance.
+    """
+
+    # Error checking
+    if not q.event_shape == p.event_shape:
+        raise ValueError("Multivariate normal distributions must have same event shape.")
+    if order != 2:
+        raise NotImplementedError("Only order p=2 is supported currently.")
+
+    # Compute individual terms
+    q_cov_sqrt = _symsqrt(q.covariance_matrix)
+    if p.batch_shape == torch.Size([]):
+        mixed_cov_matrix = q_cov_sqrt @ p.covariance_matrix @ q_cov_sqrt
+        mixed_cov_sqrt = _symsqrt(mixed_cov_matrix)
+        mixed_trace_term = torch.trace(mixed_cov_sqrt)
+    else:
+        mixed_cov_matrix = torch.bmm(q_cov_sqrt, torch.bmm(p.covariance_matrix, q_cov_sqrt))
+        mixed_cov_sqrt = _symsqrt(mixed_cov_matrix)
+        mixed_trace_term = torch.vmap(torch.trace)(mixed_cov_sqrt)
+
+    mean_sq_distance_term = torch.sum((q.mean - p.mean) ** 2, dim=(-1))
+
+    return (
+        mean_sq_distance_term + torch.sum(q.variance, dim=-1) + torch.sum(p.variance, dim=-1) - 2.0 * mixed_trace_term
+    )
