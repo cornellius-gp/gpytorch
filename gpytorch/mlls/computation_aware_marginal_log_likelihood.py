fixed missing constant in computation-aware eLBO

gpytorch/mlls/computation_aware_marginal_log_likelihood.py
@@ -496,13 +496,12 @@ class ComputationAwareELBO(MarginalLogLikelihood):
         )
 
         # KL divergence to prior
-        kl_prior_term = -0.5 * (
-            num_actions * torch.log(self.likelihood.noise)
-            + torch.logdet(StrS)
-            - 2 * torch.sum(torch.log(cholfac_gram.diagonal()))
-            - torch.inner(compressed_repr_weights, (gram_SKS @ compressed_repr_weights))
-            + torch.trace(torch.cholesky_solve(gram_SKS, cholfac_gram, upper=False))
-            + num_train_data
+        kl_prior_term = 0.5 * (
+            torch.inner(compressed_repr_weights, (gram_SKS @ compressed_repr_weights))
+            + 2 * torch.sum(torch.log(cholfac_gram.diagonal()))
+            - num_actions * torch.log(self.likelihood.noise)
+            - torch.logdet(StrS)
+            - torch.trace(torch.cholesky_solve(gram_SKS, cholfac_gram, upper=False))
         )
 
         elbo = torch.squeeze(expected_log_likelihood_term - kl_prior_term)

gpytorch/models/computation_aware_gp/preconditioners/preconditioner.py
@@ -50,30 +50,30 @@ class Preconditioner(nn.Module, abc.ABC):
         upper_bound_max_eigval_preconditioner_inv: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         # Ensure preconditioner validity
-        if upper_bound_max_eigval_preconditioner_inv is None:
-            upper_bound_max_eigval_preconditioner_inv = torch.max(
-                torch.sum(torch.abs(unnormalized_preconditioner_inv), dim=1)
-            )
-        if torch.abs(upper_bound_max_eigval_preconditioner_inv) < 1e-12:
-            return torch.zeros(())
+        # if upper_bound_max_eigval_preconditioner_inv is None:
+        #     upper_bound_max_eigval_preconditioner_inv = torch.max(
+        #         torch.sum(torch.abs(unnormalized_preconditioner_inv), dim=1)
+        #     )
+        # if torch.abs(upper_bound_max_eigval_preconditioner_inv) < 1e-12:
+        #     return torch.zeros(())
 
-        lower_bound_max_eigval_Khat = torch.sum(kernel(X)) / X.shape[0] + noise
-        upper_bound_max_eigval_Khat = torch.max(
-            torch.sum(
-                torch.abs(kernel(X).to_dense() + noise * torch.eye(X.shape[0])),
-                dim=1,
-            )
-        )
-        scalar_factor_precond_inv = (
-            torch.minimum(lower_bound_max_eigval_Khat, 1.0 / upper_bound_max_eigval_Khat)
-            / upper_bound_max_eigval_preconditioner_inv
-        )
-        # Pinvsqrt_Khat = self.sqrt_inv_matmul(
-        #     kernel(X).to_dense() + noise * torch.eye(X.shape[0]), kernel=kernel, noise=noise, X=X
+        # lower_bound_max_eigval_Khat = torch.sum(kernel(X)) / X.shape[0] + noise
+        # upper_bound_max_eigval_Khat = torch.max(
+        #     torch.sum(
+        #         torch.abs(kernel(X).to_dense() + noise * torch.eye(X.shape[0])),
+        #         dim=1,
+        #     )
         # )
-        # Pinvsqrt_Khat_Pinvsqrt = self.sqrt_inv_matmul(Pinvsqrt_Khat.mT, kernel=kernel, noise=noise, X=X)
-        # upper_bound_max_eigval_Pinvsqrt_Khat_Pinvsqrt = torch.max(torch.sum(torch.abs(Pinvsqrt_Khat_Pinvsqrt), dim=1))
-        # scalar_factor_precond_inv = 1 / upper_bound_max_eigval_Pinvsqrt_Khat_Pinvsqrt
+        # scalar_factor_precond_inv = (
+        #     torch.minimum(lower_bound_max_eigval_Khat, 1.0 / upper_bound_max_eigval_Khat)
+        #     / upper_bound_max_eigval_preconditioner_inv
+        # )
+        Pinvsqrt_Khat = self.sqrt_inv_matmul(
+            kernel(X).to_dense() + noise * torch.eye(X.shape[0]), kernel=kernel, noise=noise, X=X
+        )
+        Pinvsqrt_Khat_Pinvsqrt = self.sqrt_inv_matmul(Pinvsqrt_Khat.mT, kernel=kernel, noise=noise, X=X)
+        upper_bound_max_eigval_Pinvsqrt_Khat_Pinvsqrt = torch.max(torch.sum(torch.abs(Pinvsqrt_Khat_Pinvsqrt), dim=1))
+        scalar_factor_precond_inv = 1 / upper_bound_max_eigval_Pinvsqrt_Khat_Pinvsqrt
 
         # print(scalar_factor_precond_inv)
         return scalar_factor_precond_inv