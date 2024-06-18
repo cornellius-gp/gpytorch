7465c099949926ae919d52fa841d4c305c95c291
Jonathan Wenger <jw4246@columbia.edu>
2024-06-18 14:32:07 -0400

    removed batching from computation-aware ELBO and added informative comments

--- gpytorch/mlls/computation_aware_elbo.py
+++ gpytorch/mlls/computation_aware_elbo.py
@@ -6,7 +6,7 @@ import math
 import torch
 from linear_operator import utils
 
-from .. import kernels
+from .. import kernels, settings
 
 from ..likelihoods import _GaussianLikelihoodBase, GaussianLikelihood
 from .marginal_log_likelihood import MarginalLogLikelihood
@@ -24,32 +24,24 @@ class ComputationAwareELBO(MarginalLogLikelihood):
             raise NotImplementedError("Likelihood must be Gaussian for computation-aware inference.")
         super().__init__(likelihood, model)
 
-    def forward(self, outputs_batch: torch.Tensor, targets_batch: torch.Tensor, **kwargs):
-        # TODO: don't batch objective
-        # TODO: comment properly
+    def forward(self, outputs: torch.Tensor, targets: torch.Tensor, **kwargs):
 
-        num_actions = self.model.actions_op.shape[-2]
-
-        # Training data
+        # Initialize some useful variables
         train_inputs = self.model.train_inputs[0]
         train_targets = self.model.train_targets
         num_train_data = len(train_targets)
+        prior_evaluated_at_train_inputs = outputs[
+            0:num_train_data
+        ]  # Training data size might not exactly match self.model.num_non_zero * self.model.projection_dim
 
-        if targets_batch.shape[0] < num_train_data:
-            # Batched objective
-            is_batched_objective = True
-            train_inputs_batch = outputs_batch.lazy_covariance_matrix.x1
-            num_train_data_batch = targets_batch.shape[0]
-            prior_train_inputs = self.model(train_inputs)
-        else:
-            # Objective evaluated on entire training data
-            is_batched_objective = False
-            targets_batch = self.model.train_targets[
-                0:num_train_data
-            ]  # Account for block structure in sparse actions (N=I*K)
-            outputs_batch = outputs_batch[0:num_train_data]
-            prior_train_inputs = outputs_batch
-            num_train_data_batch = num_train_data
+        if settings.debug.on():
+            # Check whether training objective is evaluated at the training data
+            # Note that subsetting is needed here, since a block sparse projection with equal block size
+            # necessitates that num_train_data = self.model.num_non_zero * self.model.projection_dim
+            if (not torch.equal(train_inputs, outputs.lazy_covariance_matrix.x1[0:num_train_data])) or (
+                not torch.equal(train_targets, targets[0:num_train_data])
+            ):
+                raise RuntimeError("You must evaluate the objective on the training inputs!")
 
         # Kernel
         if isinstance(self.model.covar_module, kernels.ScaleKernel):
@@ -61,9 +53,10 @@ class ComputationAwareELBO(MarginalLogLikelihood):
             lengthscale = self.model.covar_module.lengthscale
             kernel_forward_fn = self.model.covar_module._forward_no_kernel_linop
 
-        # Gramian S'KS
-        del self.model.cholfac_gram_SKhatS  # Explicitly free up memory from prediction
+        # Explicitly free up memory from prediction to avoid unnecessary memory overhead
+        del self.model.cholfac_gram_SKhatS
 
+        # Lazily evaluate kernel at training inputs as a 4D tensor with shape (PROJ_DIM, PROJ_DIM, NNZ, NNZ)
         K_lazy = kernel_forward_fn(
             train_inputs.div(lengthscale).view(
                 self.model.projection_dim, self.model.num_non_zero, train_inputs.shape[-1]
@@ -72,70 +65,63 @@ class ComputationAwareELBO(MarginalLogLikelihood):
                 self.model.projection_dim, 1, self.model.num_non_zero, train_inputs.shape[-1]
             ),
         )
+
+        # Compute S'K in block shape (PROJ_DIM, PROJ_DIM, NNZ)
         StK_block_shape = (
             K_lazy @ self.model.actions_op.blocks.view(self.model.projection_dim, 1, self.model.num_non_zero, 1)
         ).squeeze(-1)
-        gram_SKS = (StK_block_shape * self.model.actions_op.blocks).sum(-1).mul(outputscale)
-        StK = StK_block_shape.view(num_actions, num_actions * self.model.num_non_zero).mul(outputscale)
+        covar_x_batch_X_train_actions = (
+            StK_block_shape.view(self.model.projection_dim, self.model.projection_dim * self.model.num_non_zero)
+            .mul(outputscale)
+            .mT
+        )
 
+        # Projected Gramians S'KS and S'(K + noise)S
+        gram_SKS = (StK_block_shape * self.model.actions_op.blocks).sum(-1).mul(outputscale)
         StrS_diag = (self.model.actions_op.blocks**2).sum(-1)  # NOTE: Assumes orthogonal actions.
         gram_SKhatS = gram_SKS + torch.diag(self.likelihood.noise * StrS_diag)
 
+        # Cholesky factor of Gramian
         cholfac_gram_SKhatS = utils.cholesky.psd_safe_cholesky(gram_SKhatS.to(dtype=torch.float64), upper=False)
 
         # Save Cholesky factor for prediction
         self.model.cholfac_gram_SKhatS = cholfac_gram_SKhatS.clone().detach()
 
-        if is_batched_objective:
-            covar_x_batch_X_train_actions = (
-                (
-                    kernel_forward_fn(
-                        train_inputs_batch.div(lengthscale),
-                        train_inputs.div(lengthscale).view(
-                            self.model.projection_dim, self.model.num_non_zero, train_inputs.shape[-1]
-                        ),
-                    )
-                    @ self.model.actions_op.blocks.view(self.model.projection_dim, self.model.num_non_zero, 1)
-                )
-                .squeeze(-1)
-                .mT.mul(outputscale)
-            )
-        else:
-            covar_x_batch_X_train_actions = StK.mT
+        # "Projected" training data (with mean correction)
+        actions_targets = self.model.actions_op._matmul(
+            torch.atleast_2d(train_targets - prior_evaluated_at_train_inputs.mean).mT
+        ).squeeze(-1)
 
         # Compressed representer weights
-        actions_targets = self.model.actions_op._matmul(
-            torch.atleast_2d(train_targets - prior_train_inputs.mean).mT
-        ).squeeze(-1)
         compressed_repr_weights = torch.cholesky_solve(
             actions_targets.unsqueeze(1).to(dtype=torch.float64), cholfac_gram_SKhatS, upper=False
         ).squeeze(-1)
 
         # Expected log-likelihood term
-        f_pred_mean_batch = outputs_batch.mean + covar_x_batch_X_train_actions @ torch.atleast_1d(
+        f_pred_mean_batch = prior_evaluated_at_train_inputs.mean + covar_x_batch_X_train_actions @ torch.atleast_1d(
             compressed_repr_weights
-        ).to(dtype=targets_batch.dtype)
+        ).to(dtype=targets.dtype)
         sqrt_downdate = torch.linalg.solve_triangular(
             cholfac_gram_SKhatS, covar_x_batch_X_train_actions.mT, upper=False
         )
         trace_downdate = torch.sum(sqrt_downdate**2, dim=-1)
-        f_pred_var_batch = torch.sum(outputs_batch.variance) - torch.sum(trace_downdate)
+        f_pred_var_batch = torch.sum(prior_evaluated_at_train_inputs.variance) - torch.sum(trace_downdate)
         expected_log_likelihood_term = -0.5 * (
-            num_train_data_batch * torch.log(self.likelihood.noise)
+            num_train_data * torch.log(self.likelihood.noise)
             + 1
             / self.likelihood.noise
-            * (torch.linalg.vector_norm(targets_batch - f_pred_mean_batch) ** 2 + f_pred_var_batch)
-            + num_train_data_batch * torch.log(torch.as_tensor(2 * math.pi))
-        ).div(num_train_data_batch)
+            * (torch.linalg.vector_norm(train_targets - f_pred_mean_batch) ** 2 + f_pred_var_batch)
+            + num_train_data * torch.log(torch.as_tensor(2 * math.pi))
+        ).div(num_train_data)
 
         # KL divergence to prior
         kl_prior_term = 0.5 * (
             torch.inner(compressed_repr_weights, (gram_SKS.to(dtype=torch.float64) @ compressed_repr_weights))
             + 2 * torch.sum(torch.log(cholfac_gram_SKhatS.diagonal()))
-            - num_actions * torch.log(self.likelihood.noise).to(dtype=torch.float64)
+            - self.model.projection_dim * torch.log(self.likelihood.noise).to(dtype=torch.float64)
             - torch.log(StrS_diag.to(dtype=torch.float64).sum())
             - torch.trace(torch.cholesky_solve(gram_SKS.to(dtype=torch.float64), cholfac_gram_SKhatS, upper=False))
         ).div(num_train_data)
 
-        elbo = torch.squeeze(expected_log_likelihood_term - kl_prior_term.to(dtype=targets_batch.dtype))
+        elbo = torch.squeeze(expected_log_likelihood_term - kl_prior_term.to(dtype=targets.dtype))
         return elbo
