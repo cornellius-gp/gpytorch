41cab30ee64c23c152a360ca33a7916b34e934b4
benediktjohannes <benedikt.johannes.hofer@gmail.com>
2026-01-25 21:52:26 +0100

    Fix notImplementedError message

--- gpytorch/variational/unwhitened_variational_strategy.py
+++ gpytorch/variational/unwhitened_variational_strategy.py
@@ -72,10 +72,10 @@ class UnwhitenedVariationalStrategy(_VariationalStrategy):
         # retrieve the variational mean, m and covariance matrix, S.
         if not isinstance(self._variational_distribution, CholeskyVariationalDistribution):
             raise NotImplementedError(
-                "Only CholeskyVariationalDistribution has pseudo-point support currently, ",
-                "but your _variational_distribution is a ",
-                self._variational_distribution.__name__,
+                f"Only CholeskyVariationalDistribution has pseudo-point support currently, "
+                f"but your _variational_distribution is a {type(self._variational_distribution).__name__}"
             )
+
 
         # retrieve the variational mean, m and covariance matrix, S.
         var_cov_root = TriangularLinearOperator(self._variational_distribution.chol_variational_covar)
